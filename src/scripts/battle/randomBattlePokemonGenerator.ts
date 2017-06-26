956d2e6c42f81e2322e9748e107d2985a0692b15
Dennis Mouwen <dennismouwen@gmail.com>
2017-06-26 14:21:07 +0200

    update generator to new pokemonmap style

--- src/scripts/battle/randomBattlePokemonGenerator.ts
+++ src/scripts/battle/randomBattlePokemonGenerator.ts
@@ -12,23 +12,23 @@ class randomBattlePokemonGenerator {
         let rand: number = Math.floor(Math.random() * pokemonList.length);
         let name: string = pokemonList[rand];
         let basePokemon = PokemonHelper.findPokemonByName(name);
+        let id = basePokemon["id"];
 
-
-        let type1: PokemonTypes = PokemonHelper.typeStringToId(basePokemon.type[0]);
-        let type2: PokemonTypes = PokemonHelper.typeStringToId(basePokemon.type[1]);
+        let type1: PokemonTypes = PokemonHelper.typeStringToId(basePokemon["type"][0]);
+        let type2: PokemonTypes = PokemonHelper.typeStringToId(basePokemon["type"][1]);
 
         // TODO this monster formula needs to be improved. Preferably with graphs :D
         let maxHealth: number = Math.max(Math.floor(Math.pow((100 * Math.pow(route, 2.2) * Math.pow(player.caughtPokemonList.length - 1, 1.2) / 12), 1.15)), 20) || 20;
 
         let catchVariation = Math.floor(Math.random() * 7 - 3);
 
-        let catchRate: number = Math.floor(Math.pow(basePokemon.catchRate, 0.75)) + catchVariation;
-        let exp: number = basePokemon.exp;
+        let catchRate: number = Math.floor(Math.pow(basePokemon["catchrate"], 0.75)) + catchVariation;
+        let exp: number = basePokemon["exp"];
 
         let deviation = Math.floor(Math.random() * 51) - 25;
         let money: number = Math.max(10, 3 * route + 5 * Math.pow(route, 1.15) + deviation);
-        let shiny:boolean = this.generateShiny();
-        return new battlePokemon(name, type1, type2, maxHealth, catchRate, exp, money, shiny);
+        let shiny: boolean = this.generateShiny();
+        return new battlePokemon(name, id, type1, type2, maxHealth, catchRate, exp, money, shiny);
     }
 
     public static generateShiny(): boolean {
