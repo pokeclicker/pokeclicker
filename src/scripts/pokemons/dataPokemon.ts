51ee949b10692a83ef5adca0b8e234c1edefb058
Dennis Mouwen <dennismouwen@gmail.com>
2017-06-26 14:48:08 +0200

    add constructor

--- src/scripts/pokemons/dataPokemon.ts
+++ src/scripts/pokemons/dataPokemon.ts
@@ -14,4 +14,20 @@ class dataPokemon implements pokemonInterface {
     exp: number;
     eggCycles: number;
     shiny: boolean;
+
+
+    constructor(id: number, name: string, catchRate: number, evolution: string, evoLevel: number, type1: gameConstants.PokemonType, type2: gameConstants.PokemonType, attack: number, levelType: gameConstants.LevelType, exp: number, eggCycles: number, shiny: boolean) {
+        this.id = id;
+        this.name = name;
+        this.catchRate = catchRate;
+        this.evolution = evolution;
+        this.evoLevel = evoLevel;
+        this.type1 = type1;
+        this.type2 = type2;
+        this.attack = attack;
+        this.levelType = levelType;
+        this.exp = exp;
+        this.eggCycles = eggCycles;
+        this.shiny = shiny;
+    }
 }
