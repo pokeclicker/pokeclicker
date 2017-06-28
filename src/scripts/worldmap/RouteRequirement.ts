40e8d0185227301b4749b3fb619fd3104dde7018
IshaD <ishadijcks@gmail.com>
2017-06-28 22:50:35 +0200

    Place RouteRequirement functions into a class.

--- src/scripts/worldmap/RouteRequirement.ts
+++ src/scripts/worldmap/RouteRequirement.ts
@@ -1,30 +1,34 @@
-let moveToRoute = function (route: number, region: GameConstants.Region) {
-    if (!isNaN(route) && !(route == Player.route)) {
-        if (accessToRoute(route, region)) {
-            Player.route = route;
-        }
-        else {
-            console.log("You don't have access to that route yet.");
-        }
-    }
-};
+class RouteRequirement {
 
-let accessToRoute = function (route: number, region: GameConstants.Region) {
-    if (!Player.hasBadge(GameConstants.routeGymRequirements[region][route])) {
-        console.log("Missing a badge ");
-        return false;
-    }
-    let reqList = GameConstants.routeRequirements[region][route];
-    if (reqList == undefined) {
-        console.log("No route requirements");
-        return true;
-    }
-    for (let i = 0; i < reqList.length; i++) {
-        let route: number = reqList[i];
-        if (Player.routeKills[route] < Player.routeKillsNeeded) {
-            console.log("Not enough kills on route: " + route);
-            return false
+    public static moveToRoute = function (route: number, region: GameConstants.Region) {
+        if (!isNaN(route) && !(route == Player.route)) {
+            if (this.accessToRoute(route, region)) {
+                Player.route = route;
+            }
+            else {
+                console.log("You don't have access to that route yet.");
+            }
         }
-    }
-    return true;
-};
+    };
+
+    public static accessToRoute = function (route: number, region: GameConstants.Region) {
+        if (!Player.hasBadge(GameConstants.routeGymRequirements[region][route])) {
+            console.log("Missing a badge ");
+            return false;
+        }
+        let reqList = GameConstants.routeRequirements[region][route];
+        if (reqList == undefined) {
+            console.log("No route requirements");
+            return true;
+        }
+        for (let i = 0; i < reqList.length; i++) {
+            let route: number = reqList[i];
+            if (Player.routeKills[route] < Player.routeKillsNeeded) {
+                console.log("Not enough kills on route: " + route);
+                return false
+            }
+        }
+        return true;
+    };
+
+}
