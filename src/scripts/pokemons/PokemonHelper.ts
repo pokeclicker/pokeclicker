a2d32e90a0ed57411d56ac35dc1e8800d59579f7
IshaD <ishadijcks@gmail.com>
2017-06-30 21:25:13 +0200

    Add calculateLevel observable to CaughtPokemon

--- src/scripts/pokemons/PokemonHelper.ts
+++ src/scripts/pokemons/PokemonHelper.ts
@@ -3,15 +3,19 @@
 
 class PokemonHelper {
 
-    public static getPokemonByName(name:string){
-        return pokemonMap[name];
+    public static getPokemonByName(name: string) {
+        let basePokemon = pokemonMap[name];
+        let type2: GameConstants.PokemonType = basePokemon["type"][0] || GameConstants.PokemonType.None;
+        let evoLevel = basePokemon["evoLevel"] || 101;
+        let eggCycles: number = basePokemon["eggCycles"] || 20;
+        return new DataPokemon(basePokemon["id"], basePokemon["name"], basePokemon["catchRate"], basePokemon["evolution"], evoLevel, basePokemon["type"][0], type2, basePokemon["attack"], basePokemon["levelType"], basePokemon["exp"], eggCycles);
     }
 
-    public static typeStringToId(id:string){
+    public static typeStringToId(id: string) {
         return GameConstants.PokemonType[id];
     }
 
-    public static typeIdToString(id:number){
+    public static typeIdToString(id: number) {
         return GameConstants.PokemonType[id];
     }
 
