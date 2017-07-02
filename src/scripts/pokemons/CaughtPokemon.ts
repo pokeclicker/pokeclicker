db2ce7c617206c525140ba1c416252e8feeec849
Aegyo <ae.jessicat@gmail.com>
2017-07-02 17:40:06 +0100

    Implement evolution

--- src/scripts/pokemons/CaughtPokemon.ts
+++ src/scripts/pokemons/CaughtPokemon.ts
@@ -10,6 +10,7 @@ class CaughtPokemon {
     attackBonus: KnockoutObservable<number>;
     exp: KnockoutObservable<number>;
     levelObservable: KnockoutComputed<number>;
+    evolver: KnockoutSubscription
 
     constructor(pokemonData: DataPokemon, ev: boolean, atBo: number, xp: number) {
         this.id = pokemonData.id;
@@ -20,5 +21,14 @@ class CaughtPokemon {
         this.levelObservable = ko.computed(() => {return PokemonHelper.calculateLevel(this)});
         this.baseAttack = pokemonData.attack
         this.attack = ko.computed(() => {return PokemonHelper.calculateAttack(this.baseAttack, this.attackBonus(), this.levelObservable())})
+
+        if (pokemonData.evoLevel) {
+            this.evolver = this.levelObservable.subscribe(() => {
+                if (this.levelObservable() >= pokemonData.evoLevel) {
+                    Player.capturePokemon(pokemonData.evolution, false);
+                    this.evolver.dispose();
+                }
+            })
+        }
     }
 }
