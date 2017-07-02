c9323910a61460aa4b00d228208d8041643e77a4
Aegyo <ae.jessicat@gmail.com>
2017-07-02 13:43:10 +0100

    Make CaughtPokemon id a regular number
    id never updates, so making it observable is unnecessary

--- src/scripts/pokemons/CaughtPokemon.ts
+++ src/scripts/pokemons/CaughtPokemon.ts
@@ -2,7 +2,7 @@
  * Created by dennis on 26-06-17.
  */
 class CaughtPokemon {
-    id: KnockoutObservable<number>;
+    id: number;
     name: string;
     evolved: boolean;
     attackBonus: KnockoutObservable<number>;
@@ -10,7 +10,7 @@ class CaughtPokemon {
     levelObservable: KnockoutComputed<number>;
 
     constructor(id : number, nm: string, ev: boolean, atBo: number, xp: number) {
-        this.id = ko.observable(id);
+        this.id = id;
         this.name = nm;
         this.evolved = ev;
         this.attackBonus = ko.observable(atBo);
