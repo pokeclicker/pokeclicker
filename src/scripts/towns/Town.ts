c7ecee2d8f37ecdef12dfed63d72fbe9cc5726d3
IshaD <ishadijcks@gmail.com>
2017-07-09 15:57:13 +0200

    Add viridian forest dungeon

--- src/scripts/towns/Town.ts
+++ src/scripts/towns/Town.ts
@@ -39,3 +39,7 @@ const TownList: { [name: string]: Town } = {};
 
 //TODO Add all towns
 TownList["Pewter City"] = new Town("Pewter City", [2]);
+
+
+//TODO Add all dungeons
+TownList["Viridian Forest"] = new Town("Viridian Forest", [1], null, dungeonList["Viridian Forest"]);
