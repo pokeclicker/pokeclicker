33342328303ec3960bbc5d5c6c9b162aa7ee242b
Dennis Mouwen <dennismouwen@gmail.com>
2017-07-10 14:48:01 +0200

    Start on start sequence modal

--- src/scripts/StartSequenceRunner.ts
+++ src/scripts/StartSequenceRunner.ts
@@ -3,5 +3,19 @@ class StartSequenceRunner {
     public static start() {
         Game.gameState(GameConstants.GameState.idle);
         $('#startSequenceModal').modal('show');
+
     }
 }
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    $('#startSequenceModal').on('hidden.bs.modal', function () {
+        $('#pickStarterModal').modal('show');
+
+    });
+
+    $('#pickStarterModal').on('hidden.bs.modal', function () {
+        Game.gameState(GameConstants.GameState.fighting);
+
+    });
+});
