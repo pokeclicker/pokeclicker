7511c72fcff2b349d4c1ebf208d5b6e8703140ca
Dennis Mouwen <dennismouwen@gmail.com>
2017-07-13 16:43:06 +0200

    Fix notification style and centering in modals

--- src/scripts/Notifier.ts
+++ src/scripts/Notifier.ts
@@ -41,6 +41,7 @@ class Notifier {
             '<span data-notify="icon"></span> ' +
             '<span data-notify="title">{1}</span> ' +
             '<span data-notify="message">{2}</span>' +
+            '<span style="padding: 5px;"></span>' +
             '<div class="progress" data-notify="progressbar">' +
             '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
             '</div>' +
