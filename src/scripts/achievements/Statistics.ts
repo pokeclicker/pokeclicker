590017d0d3a0bbf4ff040cefa75860d123e1faac
tklaxbro <tkatchen7@gmail.com>
2017-07-28 11:01:50 -0700

    Created Statistics Class

---
+++ src/scripts/achievements/Statistics.ts
@@ -0,0 +1,12 @@
+class Statistics {
+
+    public clicks: KnockoutObservable<number>;
+    public hatchedEggs: KnockoutObservable<number>;
+    public pokemonCaptured: KnockoutObservable<number>;
+    public pokemonDefeated: KnockoutObservable<number>;
+    public gymsDefeated: KnockoutObservableArray<number>;
+    public dungeonsCleared: KnockoutObservableArray<number>
+    public digItems: KnockoutObservable<number>;
+    public digDeeper: KnockoutObservable<number>;
+
+}
