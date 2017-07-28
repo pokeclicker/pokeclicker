eb7e336b560e8e45877c22e1060936ec9cf52bde
tklaxbro <tkatchen7@gmail.com>
2017-07-28 11:47:16 -0700

    Added Defeated and Captured Requirements

---
+++ src/scripts/achievements/CapturedRequirement.ts
@@ -0,0 +1,11 @@
+///<reference path="Requirement.ts"/>
+
+class CapturedRequirement extends Requirement{
+    constructor( value:number, type: GameConstants.AchievementOption = GameConstants.AchievementOption.more) {
+        super(value, type);
+    }
+
+    public getProgress(){
+        return Math.min(Statistics.pokemonCaptured, this.value);
+    }
+}
