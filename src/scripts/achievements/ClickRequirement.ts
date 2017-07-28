74983a96d66e2dc582d5d4a1361089905240ebc4
tklaxbro <tkatchen7@gmail.com>
2017-07-28 11:10:50 -0700

    Clicks

---
+++ src/scripts/achievements/ClickRequirement.ts
@@ -0,0 +1,11 @@
+///<reference path="Requirement.ts"/>
+
+class ClickRequirement extends Requirement{
+    constructor( value:number, type: GameConstants.AchievementOption = GameConstants.AchievementOption.more) {
+        super(value, type);
+    }
+
+    public getProgress(){
+        return Math.min(statistics.clicks, this.value);
+    }
+}
