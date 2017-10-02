20d2ae18a3a8a8a8f45b75c8babb56f8c17b207c
BaineGames <noah.baine@gmail.com>
2017-10-02 14:22:15 -0700

    fixed buggy mcbugface with shophandler - apparently was existing. Thanks @aegyo

--- src/scripts/shop/ShopHandler.ts
+++ src/scripts/shop/ShopHandler.ts
@@ -87,7 +87,7 @@ class ShopHandler {
     public static calculateButtonCss(): string {
         let item: Item = this.shopObservable().items()[ShopHandler.selected()];
 
-        if (!player.hasMoney(item.totalPrice()) || this.amount() < 1) {
+        if (item && !player.hasMoney(item.totalPrice()) || this.amount() < 1) {
             return "btn btn-danger smallButton smallFont"
         } else {
             return "btn btn-success smallButton smallFont"
