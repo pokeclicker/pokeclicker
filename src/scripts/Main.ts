5badc7de55c07757fc414e266982571d9f4ba8ba
Noah Ross <noah.baine@gmail.com>
2018-06-05 08:45:38 -0700

    removed map function

--- src/scripts/Main.ts
+++ src/scripts/Main.ts
@@ -233,10 +233,4 @@ class Game {
         $(this).remove();
         });
     }
-
-    static map (num, in_min, in_max, out_min, out_max) {
-        return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-      }
-
 }
-
