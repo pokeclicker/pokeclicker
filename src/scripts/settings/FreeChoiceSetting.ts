cc45ae3ff16d5e2fe8416878c653c547785a2ade
Isha Dijcks <ishadijcks@gmail.com>
2019-06-01 16:55:31 +0200

    Remove FreeChoiceSetting

--- src/scripts/settings/FreeChoiceSetting.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="Setting.ts" />
-
-class FreeChoiceSetting extends Setting {
-    constructor(name: string, displayName: string, defaultValue: any) {
-        super(name, displayName, [], defaultValue);
-    }
-}
