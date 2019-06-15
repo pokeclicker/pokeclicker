7512856381a3c3b62abc3a809b96b9a70282d98e
fuws <36806183+fuws@users.noreply.github.com>
2019-06-15 14:56:17 -0400

    Make additional evolution items available for purchase

--- src/scripts/towns/Town.ts
+++ src/scripts/towns/Town.ts
@@ -133,20 +133,22 @@ TownList["Cherrygrove City"] = new Town("Cherrygrove City", [29]);
 let VioletCityShop = new Shop(["MediumRestore", "Togepi"]);
 TownList["Violet City"] = new Town("Violet City", [31], VioletCityShop, dungeonList["Sprout Tower"]);
 
-TownList["Azalea Town"] = new Town("Azalea Town", [33], null, dungeonList["Slowpoke Well"]);
+let AzaleaTownShop = new Shop(["Kings_rock"]);
+TownList["Azalea Town"] = new Town("Azalea Town", [33], AzaleaTownShop, dungeonList["Slowpoke Well"]);
 
-let GoldenrodCityShop = new Shop(["Sun_stone"]);
+let GoldenrodCityShop = new Shop(["Sun_stone", "Upgrade"]);
 TownList["Goldenrod City"] = new Town("Goldenrod City", [34], GoldenrodCityShop);
 
 TownList["Ecruteak City"] = new Town("Ecruteak City", [37]);
 
-TownList["Olivine City"] = new Town("Olivine City", [39]);
+let OlivineCityShop = new Shop(["Metal_coat"]);
+TownList["Olivine City"] = new Town("Olivine City", [39], OlivineCityShop);
 
 TownList["Cianwood City"] = new Town("Cianwood City", [41]);
 
 TownList["Mahogany Town"] = new Town("Mahogany Town", [42], null, null, "Mt Mortar");
 
-let BlackthornCityShop = new Shop(["LargeRestore"]);
+let BlackthornCityShop = new Shop(["LargeRestore", "Dragon_scale"]);
 TownList["Blackthorn City"] = new Town("Blackthorn City", [44], BlackthornCityShop, null, "Ice Path");
 
 //Johto Dungeons
