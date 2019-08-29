42a16014396f2a3850cf7b587e7ba80530c8b909
RedSparr0w <admin@redsparr0w.com>
2019-08-29 16:52:25 +1200

    fixup dungeon

--- src/scripts/dungeons/DungeonRunner.ts
+++ src/scripts/dungeons/DungeonRunner.ts
@@ -80,7 +80,7 @@ class DungeonRunner {
 
     public static dungeonWon() {
         GameHelper.incrementObservable(player.statistics.dungeonsCleared[Statistics.getDungeonIndex(DungeonRunner.dungeon.name())]);
-        player.dungeonsCleared[Statistics.getDungeonIndex(DungeonRunner.dungeon.name())] += 1;
+        GameHelper.incrementObservable(player.dungeonsCleared[Statistics.getDungeonIndex(DungeonRunner.dungeon.name())]);
         Game.gameState(GameConstants.GameState.town);
         // TODO award loot with a special screen
         Notifier.notify("You have successfully completed the dungeon", GameConstants.NotificationOption.success);
