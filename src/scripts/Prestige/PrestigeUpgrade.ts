1c879c4a5f358db336bc10f6bb6aaca3f43dbdbe
RedSparr0w <admin@redsparr0w.com>
2019-08-29 13:56:30 +1200

    update tooltips

--- src/scripts/Prestige/PrestigeUpgrade.ts
+++ src/scripts/Prestige/PrestigeUpgrade.ts
@@ -9,6 +9,9 @@ class PrestigeUpgrade {
     constructor(id: number, description: string, cost: number, costType: GameConstants.PrestigeType, bonus = 0) {
         this.id = id;
         this.description = description;
+        this.tooltip = description + `<br/>`;
+        for (let i = 0; i < cost; i++)
+            this.tooltip += `<img src="assets/images/currency/money.png" class="prestige-coins prestige-coin-${costType}" />`;
         this.cost = cost;
         this.costType = costType;
         this.bonus = bonus;
