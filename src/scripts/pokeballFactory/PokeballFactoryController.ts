4f207b397637ef4aea664ea27d02b5359e9e2d6e
Isha Dijcks <ishadijcks@gmail.com>
2019-12-24 18:15:23 +0100

    No longer pause the game when viewing the PokeballFactory

--- src/scripts/pokeballFactory/PokeballFactoryController.ts
+++ src/scripts/pokeballFactory/PokeballFactoryController.ts
@@ -2,7 +2,6 @@ class PokeballFactoryController {
 
     public static openFactoryModal() {
         if (PokeballFactory.canAccess()) {
-            Game.gameState(GameConstants.GameState.paused);
             $('#pokeballFactoryModal').modal('show');
         } else {
             Notifier.notify("You need the <b>Factory Key</b> to enter the Pokéball Factory", GameConstants.NotificationOption.warning);
