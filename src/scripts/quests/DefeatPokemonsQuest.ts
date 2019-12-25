0463b6cf2abe71daca381c70b4f0a524639c047e
RedSparr0w <admin@redsparr0w.com>
2019-12-26 11:52:31 +1300

    Fixup semantic errors

--- src/scripts/quests/DefeatPokemonsQuest.ts
+++ src/scripts/quests/DefeatPokemonsQuest.ts
@@ -3,15 +3,15 @@
 class DefeatPokemonsQuest extends Quest implements QuestInterface {
     private route: number;
 
-    constructor(route: number, killsNeeded: number) {
+    constructor(route: number, region: number, killsNeeded: number) {
         super(killsNeeded, DefeatPokemonsQuest.calcReward(route, killsNeeded));
         this.description = `Defeat ${killsNeeded} pokemon on route ${route}.`;
         this.route = route;
         this.questFocus = player.routeKills[this.route];
     }
 
-    private static calcReward(route: number, killsNeeded: number): number {
-        let attacksPerPokemon = Math.ceil( Math.min( 4, PokemonFactory.routeHealth(route) / Math.max(1, player.pokemonAttackObservable()) ) )
+    private static calcReward(route: number, region: number, killsNeeded: number): number {
+        let attacksPerPokemon = Math.ceil(Math.min(4, PokemonFactory.routeHealth(route, region) / Math.max(1, player.pokemonAttackObservable())))
         return Math.ceil(GameConstants.DEFEAT_POKEMONS_BASE_REWARD * attacksPerPokemon * killsNeeded);
     }
 }
--- src/scripts/quests/QuestHelper.ts
+++ src/scripts/quests/QuestHelper.ts
@@ -21,7 +21,7 @@ class QuestHelper{
                 region = SeededRand.intBetween(0, player.highestRegion());
                 route = SeededRand.intBetween(GameConstants.RegionRoute[region][0], GameConstants.RegionRoute[region][1]);
                 amount = SeededRand.intBetween(100, 500);
-                return new DefeatPokemonsQuest(route, amount);
+                return new DefeatPokemonsQuest(route, region, amount);
             case "CapturePokemons":
                 amount = SeededRand.intBetween(100, 500);
                 return new CapturePokemonsQuest(amount);
