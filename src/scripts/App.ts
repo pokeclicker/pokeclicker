3f30cc57d15f93205995924bc9314a1c7f9a7561
Isha Dijcks <ishadijcks@gmail.com>
2019-12-27 13:52:31 +0100

    Move more methods to GameController.ts

--- src/scripts/App.ts
+++ src/scripts/App.ts
@@ -11,41 +11,9 @@ class App {
             UndergroundItem.initialize();
             game = new Game();
 
-            $(document).ready(function () {
-                $('[data-toggle="popover"]').popover();
-                $('[data-toggle="tooltip"]').tooltip();
-            });
-
             Notifier.notify("Game loaded", GameConstants.NotificationOption.info);
 
-            (ko as any).bindingHandlers.tooltip = {
-                init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
-                    let local = ko.utils.unwrapObservable(valueAccessor()),
-                        options = {};
-
-                    ko.utils.extend(options, ko.bindingHandlers.tooltip.options);
-                    ko.utils.extend(options, local);
-
-                    $(element).tooltip(options);
-
-                    if (bindingContext.$data instanceof Plot) {
-                        $(element).hover(function () {
-                            $(this).data('to', setInterval(function () {
-                                $(element).tooltip('hide')
-                                    .attr('data-original-title', FarmRunner.getTooltipLabel(bindingContext.$index()))
-                                    .tooltip('show');
-                            }, 100));
-                        }, function () {
-                            clearInterval($(this).data('to'));
-                        });
-                    }
-
-                },
-                options: {
-                    placement: "bottom",
-                    trigger: "click"
-                }
-            };
+            GameController.bindToolTips();
 
             PokedexHelper.populateTypeFilters();
             PokedexHelper.updateList();
