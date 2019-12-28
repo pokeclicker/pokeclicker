529c2cca31addb99732651ee1a04776a7f00db32
Isha Dijcks <ishadijcks@gmail.com>
2019-12-28 14:10:56 +0100

    Move KeyListeners and KeyBinds logic to GameController file

--- src/scripts/GameController.ts
+++ src/scripts/GameController.ts
@@ -83,3 +83,55 @@ class GameController {
         };
     }
 }
+
+$(document).on("keydown", function (e) {
+    let keyCode = e.keyCode;
+
+    if (App.game.gameState === GameConstants.GameState.dungeon) {
+        if (keyCode == 38 || keyCode == 87) {
+            DungeonRunner.map.moveUp();
+        } else if (keyCode == 39 || keyCode == 68) {
+            DungeonRunner.map.moveRight();
+        } else if (keyCode == 37 || keyCode == 65) {
+            DungeonRunner.map.moveLeft();
+        } else if (keyCode == 40 || keyCode == 83) {
+            DungeonRunner.map.moveDown();
+        } else if (keyCode == 32) {
+            DungeonRunner.openChest();
+            DungeonRunner.startBossFight();
+        }
+        e.preventDefault();
+    }
+
+});
+
+$(document).ready(function () {
+    $(document).on("keydown", function (e) {
+        let keyCode = e.keyCode;
+        if (App.game.gameState === GameConstants.GameState.safari) {
+            let dir = GameConstants.KeyToDirection[keyCode];
+            if (dir) {
+                e.preventDefault();
+                Safari.move(dir);
+            }
+            if (keyCode == 32) { // space
+                e.preventDefault();
+            }
+        }
+    });
+
+    $(document).on("keyup", function (e) {
+        let keyCode = e.keyCode;
+        if (App.game.gameState === GameConstants.GameState.safari) {
+            let dir = GameConstants.KeyToDirection[keyCode];
+            if (dir) {
+                e.preventDefault();
+                Safari.stop(dir);
+            } else if (keyCode == 32) { // space
+                e.preventDefault();
+            }
+        }
+    });
+
+    $("#pokedexModal").on("show.bs.modal", PokedexHelper.updateList)
+});
