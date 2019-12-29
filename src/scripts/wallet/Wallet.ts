be72e178ba422a1268618012ef031117fe3f8585
Isha Dijcks <ishadijcks@gmail.com>
2019-12-29 23:01:36 +0100

    Implement basic wallet class

--- src/scripts/wallet/Wallet.ts
+++ src/scripts/wallet/Wallet.ts
@@ -1,8 +1,42 @@
 class Wallet implements Feature {
+    name: string = "Wallet";
     saveKey: string;
+    currencies: ArrayOfObservables<number>;
 
     defaults = {
+        currencies: [0, 0, 0, 0, 0]
+    };
 
+
+    constructor() {
+        this.currencies = new ArrayOfObservables(this.defaults.currencies);
+    }
+
+    public gainMoney(amount: number, origin?: string) {
+
+        // Good
+        // let eventBonus = App.game.eventCalendar.getMoneyMultiplier(origin);
+
+        // Good
+        // let xMultiplier = App.game.getSomething();
+        // let yMultiplier = App.game.getSomethingElse();
+        //
+        // amount *= eventBonus * xMultiplier * yMultiplier;
+        //
+        // Statistics.addTotalMoney(origin);
+        this.addAmount(new Amount(amount, Currency.money))
+    }
+
+    private addAmount(amount: Amount) {
+        this.currencies[amount.currency] += amount.amount;
+    };
+
+    public hasAmount(amount: Amount) {
+        return this.currencies[amount.currency] >= amount.amount;
+    };
+
+    public loseAmount(amount: Amount) {
+        this.currencies[amount.currency] -= amount.amount;
     };
 
 
