45dcfd00bcef65687422a2e1d162e879d916c8c6
Isha Dijcks <ishadijcks@gmail.com>
2020-01-01 22:17:24 +0100

    Fix game initializing twice

--- src/scripts/App.ts
+++ src/scripts/App.ts
@@ -35,7 +35,6 @@ class App {
             GameController.applyRouteBindings();
             Preload.hideSplashScreen();
 
-            App.game.initialize();
             App.game.start();
 
         });
