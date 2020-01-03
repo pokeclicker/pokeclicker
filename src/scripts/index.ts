3f0b1fddd502616bb15b64a884eb807421861dc5
Isha Dijcks <ishadijcks@gmail.com>
2020-01-03 19:55:52 +0100

    Remove type from player

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -2,7 +2,7 @@
  * TODO(@Isha) refactor this to no longer be global but App properties.
  * Will be done after the major player refactor.
  */
-let player: Player;
+let player;
 
 /**
  * Start the application when all html elements are loaded.
