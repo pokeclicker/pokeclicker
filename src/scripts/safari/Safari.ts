ca6c77d840dbd058a4ea49dcf67da1cd3f976c9a
Aegyo <jessica.scoltock@protonmail.com>
2020-01-02 20:27:50 -0500

    Update Safari Entrance

--- src/scripts/safari/Safari.ts
+++ src/scripts/safari/Safari.ts
@@ -10,20 +10,22 @@ class Safari {
     static queue: Array<string> = [];
     private static playerXY = {"x": 0, "y": 0};
     private static origin;
+    static inProgress: KnockoutObservable<boolean> = ko.observable(false);
     static inBattle: KnockoutObservable<boolean> = ko.observable(false);
     static balls: KnockoutObservable<number> = ko.observable();
     static sprite: Motio;
 
     public static load() {
-        this.grid = [];
-        this.playerXY.x = 0;
-        this.playerXY.y = 0;
+        Safari.grid = [];
+        Safari.playerXY.x = 0;
+        Safari.playerXY.y = 0;
         Safari.lastDirection = "up";
         Safari.inBattle(false);
+        Safari.inProgress(true);
         Safari.balls(this.calculateStartPokeballs());
         for( let i = 0; i<GameConstants.Safari.SizeY; i++){
             let row = Array.apply(null, Array(GameConstants.Safari.SizeX)).map(Number.prototype.valueOf, 0);
-            this.grid.push(row);
+            Safari.grid.push(row);
         }
 
         Safari.addRandomBody(new FenceBody());
@@ -110,28 +112,60 @@ class Safari {
         }
     }
 
+    public static openModal() {
+        if (this.canAccess()) {
+            //App.game.gameState = GameConstants.GameState.safari;
+            //Safari.load();
+            $('#safariModal').modal({backdrop: 'static', keyboard: false});
+        } else {
+            Notifier.notify("You do not have access to that location", GameConstants.NotificationOption.warning);
+        }
+    }
+
+    private static canPay() {
+        return true;
+    }
+
+    private static cost() {
+        return 100;
+    }
+
+    private static payEntranceFee() {
+        if (Safari.canPay()) {
+            // TODO add increasing cost back
+            //typeof player.safariCostModifier == undefined ? 1 : player.safariCostModifier++;
+            
+            App.game.wallet.loseAmount(new Amount(Safari.cost(),GameConstants.Currency.questPoint))
+            Safari.load()
+        }
+    }
+
+    private static canAccess() {
+        return App.game.keyItems.hasKeyItem(KeyItems.KeyItem.Safari_ticket);
+    }
+
     static show() {
         let html = "";
 
-        for (let i=0; i<this.grid.length; i++) {
-            html += "<div class='row m-0'>";
-            for (let j=0; j<this.grid[0].length; j++) {
+        for (let i=0; i<Safari.grid.length; i++) {
+            html += "<div class='row'>";
+            for (let j=0; j<Safari.grid[0].length; j++) {
                 html += Safari.square(i, j);
             }
             html += "</div>";
         }
 
-        $("#safariBattleBody").hide();
-        $("#safariBody").html(html).show();
+        $("#safariBody").html(html);
 
         Safari.addPlayer(Math.floor(GameConstants.Safari.SizeX - 1)/2, GameConstants.Safari.SizeY - 1);
+
     }
 
     private static square(i: number, j: number): string {
         let img = 'assets/images/safari/' + this.grid[i][j] + '.png';
         let divId = "safari-" + j + "-" + i;
 
-        return "<div id='" + divId + "' style=background-image:url('" + img + "') class='col-sm-1 safariSquare'></div>"
+        return "<div id='" + divId + "' style=background-image:url('" + img + "') class='safariSquare'></div>"
     }
 
     private static addPlayer(i: number, j: number) {
@@ -260,20 +294,6 @@ class Safari {
         if (!Safari.queue[0]){ Safari.walking = false };
     }
 
-    public static openModal() {
-        if (this.canAccess()) {
-            App.game.gameState = GameConstants.GameState.safari;
-            Safari.load();
-            $('#safariModal').modal({backdrop: 'static', keyboard: false});
-        } else {
-            Notifier.notify("You do not have access to that location", GameConstants.NotificationOption.warning);
-        }
-    }
-
-    private static canAccess() {
-        return App.game.keyItems.hasKeyItem(KeyItems.KeyItem.Safari_ticket);
-    }
-
     private static checkBattle(): boolean {
         let battle = false;
         if (Safari.grid[Safari.playerXY.y][Safari.playerXY.x] === 10) {
@@ -297,6 +317,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
         MapHelper.moveToTown("Fuchsia City");
     });
 
+    /*
     $('#safariModal').on('shown.bs.modal', function () {
         let element = document.querySelector('#sprite');
         Safari.sprite = new Motio(element, {
@@ -308,4 +329,5 @@ document.addEventListener("DOMContentLoaded", function (event) {
             }
         });
     });
+    */
 });
