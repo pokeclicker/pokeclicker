c3519c96f50f53dd145828cbe22260e3707e95c0
Isha Dijcks <ishadijcks@gmail.com>
2020-01-04 15:41:19 +0100

    Fix breeding display

--- src/scripts/party/PartyController.ts
+++ src/scripts/party/PartyController.ts
@@ -1,5 +1,9 @@
 class PartyController {
 
+    static getCaughtStatusByName(name: string): CaughtStatus {
+        return this.getCaughtStatus(PokemonHelper.getPokemonByName(name).id)
+    }
+
     static getCaughtStatus(id: number): CaughtStatus {
         if (App.game.party.alreadyCaughtPokemon(id, true)) {
             return CaughtStatus.CaughtShiny;
@@ -12,19 +16,56 @@ class PartyController {
         return CaughtStatus.NotCaught
     }
 
+    public static getMaxLevelPokemonList() {
+        return App.game.party.caughtPokemon.filter((partyPokemon: PartyPokemon) => {
+            return !partyPokemon.breeding && partyPokemon.levelObservable() >= 100;
+        });
+    }
+
     // TODO implement
-    // public static sortedPokemonList(): KnockoutComputed<Array<PartyPokemon>> {
-    //     return ko.pureComputed(function () {
-    //         return this._caughtPokemonList().sort(PokemonHelper.compareBy(GameConstants.SortOptionsEnum[player._sortOption()], player._sortDescending()));
-    //     }, this).extend({rateLimit: 1000})
-    // }
-    //
-    // public maxLevelPokemonList(): KnockoutComputed<Array<PartyPokemon>> {
-    //     return ko.pureComputed(function () {
-    //         return this._caughtPokemonList().filter((pokemon) => {
-    //             return pokemon.levelObservable() == 100 && !pokemon.breeding();
-    //         })
-    //     }, this)
-    // }
+    static sortedPokemonList() {
+            // return App.game.party.caughtPokemon.sort(this.compareBy(SortOptionsEnum[],));
+        // }, this)
+    }
+
+
+    public static compareBy(property: string, direction: boolean): (a: PartyPokemon, b: PartyPokemon) => number {
+        return function (a, b) {
+            let _a, _b, res, dir = (direction) ? -1 : 1;
+
+            //Convert to plain JS so that observables don't need to be accessed with brackets
+            _a = ko.toJS(a);
+            _b = ko.toJS(b);
+
+            //CaughtPokemon doesn't have shiny property, create one for comparison if needed
+            if (property == "shiny") {
+                _a.shiny = Number(App.game.party.alreadyCaughtPokemon(PokemonHelper.getPokemonByName(a.name).id, true));
+                _b.shiny = Number(App.game.party.alreadyCaughtPokemon(PokemonHelper.getPokemonByName(b.name).id, true));
+            }
+
+            if (property == "attack" || property == "levelObservable" || property == "shiny") {
+                dir *= -1;
+            }
+
+            //Compare by provided property
+            if (_a[property] == _b[property]) {
+                //If they are equal according to provided property, sort by id
+                if (_a.id < _b.id) {
+                    return -1;
+                } else if (_a.id > _b.id) {
+                    return 1;
+                }
+            } else if (_a[property] < _b[property]) {
+                res = -1;
+            } else if (_a[property] > _b[property]) {
+                res = 1;
+            } else {
+                res = 0
+            }
+
+            return res * dir;
+        }
+    }
+
 
 }
--- src/scripts/pokemons/PokemonHelper.ts
+++ src/scripts/pokemons/PokemonHelper.ts
@@ -17,9 +17,9 @@ class PokemonHelper {
         });
     }
 
-    public static getEvolution(base: string, evoType: GameConstants.StoneType) {
+    public static getEvolution(id: number, evoType: GameConstants.StoneType) {
         for (let pokemon of App.game.party.caughtPokemon){
-            if (pokemon.name == base){
+            if (pokemon.id == id){
                 for (let evolution of pokemon.evolutions){
                     if (evolution instanceof StoneEvolution && evolution.stone == evoType) {
                         return evolution.evolvedPokemon;
@@ -27,7 +27,7 @@ class PokemonHelper {
                 }
             }
         }
-        return ""
+        return "";
     }
 
     public static getPokemonById(id: number): DataPokemon {
@@ -72,43 +72,6 @@ class PokemonHelper {
         return src;
     }
 
-    public static compareBy(property: string, direction: boolean): (a: PartyPokemon, b: PartyPokemon) => number {
-        return function (a, b) {
-            let _a, _b, res, dir = (direction) ? -1 : 1;
-
-            //Convert to plain JS so that observables don't need to be accessed with brackets
-            _a = ko.toJS(a);
-            _b = ko.toJS(b);
-
-            //CaughtPokemon doesn't have shiny property, create one for comparison if needed
-            if (property == "shiny") {
-                _a.shiny = Number(App.game.party.alreadyCaughtPokemon(PokemonHelper.getPokemonByName(a.name).id, true));
-                _b.shiny = Number(App.game.party.alreadyCaughtPokemon(PokemonHelper.getPokemonByName(b.name).id, true));
-            }
-
-            if (property == "attack" || property == "levelObservable" || property == "shiny") {
-                dir *= -1;
-            }
-
-            //Compare by provided property
-            if (_a[property] == _b[property]) {
-                //If they are equal according to provided property, sort by id
-                if (_a.id < _b.id) {
-                    return -1;
-                } else if (_a.id > _b.id) {
-                    return 1;
-                }
-            } else if (_a[property] < _b[property]) {
-                res = -1;
-            } else if (_a[property] > _b[property]) {
-                res = 1;
-            } else {
-                res = 0
-            }
-
-            return res * dir;
-        }
-    }
 
     public static calcNativeRegion(pokemonName: string) {
         let id = PokemonHelper.getPokemonByName(pokemonName).id;
