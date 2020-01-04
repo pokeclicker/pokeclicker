37697b103f961ac83200decdd7aea2c3286c2177
RedSparr0w <admin@redsparr0w.com>
2020-01-04 21:47:27 +1300

    clear all warnings for now

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,12 @@ module.exports = {
   },
   'extends': ['plugin:@typescript-eslint/recommended'],
   'rules': {
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/triple-slash-reference': 'off',
+    '@typescript-eslint/consistent-type-assertions': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off'
   }
 }
