338c13058f44ee97b26d50aae338031b01c2ffc2
RedSparr0w <admin@redsparr0w.com>
2020-01-06 13:44:56 +1300

    Add gen 4+ evolutions back

--- src/scripts/pokemons/PokemonList.ts
+++ src/scripts/pokemons/PokemonList.ts
@@ -1225,6 +1225,7 @@ const pokemonList: {
             'levelType': LevelType.mediumfast,
             'exp': 172,
             'eggCycles': 25,
+            'evolutions': [new StoneEvolution('Electabuzz', 'Electivire', GameConstants.StoneType.Trade_stone)],
         }, {
             'id': 126,
             'name': 'Magmar',
@@ -1234,6 +1235,7 @@ const pokemonList: {
             'levelType': LevelType.mediumfast,
             'exp': 173,
             'eggCycles': 25,
+            'evolutions': [new StoneEvolution('Magmar', 'Magmortar', GameConstants.StoneType.Trade_stone)],
         }, {
             'id': 127,
             'name': 'Pinsir',
