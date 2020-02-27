ce4f34da300350afe1334e84b3935cc7384f4246
RedSparr0w <admin@redsparr0w.com>
2020-02-28 08:27:56 +1300

    live-website [testing]

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const gulp = require('gulp');
 const changed = require('gulp-changed');
 const minifyHtml = require('gulp-minify-html');
@@ -9,15 +10,15 @@ const browserSync = require('browser-sync');
 const del = require('del');
 const less = require('gulp-less');
 const gulpImport = require('gulp-html-import');
-const ejs = require("gulp-ejs");
-const plumber = require("gulp-plumber");
+const ejs = require('gulp-ejs');
+const plumber = require('gulp-plumber');
 const replace = require('gulp-replace');
 const connect = require('gulp-connect');
 
 /**
  * Push build to gh-pages
  */
-gulp.task('deploy', () => gulp.src("./dist/**/*")
+gulp.task('deploy', () => gulp.src('./dist/**/*')
     .pipe(deploy()));
 
 const srcs = {
@@ -27,7 +28,8 @@ const srcs = {
     ejsTemplates: ['src/templates/*.ejs'],
     styles: 'src/styles/**/*.less',
     assets: 'src/assets/**/*',
-    libs: ['node_modules/bootstrap/dist/js/bootstrap.min.js',
+    libs: [
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
         'node_modules/bootstrap/dist/css/bootstrap.min.css',
         'node_modules/jquery/dist/jquery.min.js',
         'node_modules/popper.js/dist/umd/popper.min.js',
@@ -72,17 +74,15 @@ gulp.task('browserSync', () => {
 gulp.task('compile-html', (done) => {
     const htmlDest = './build';
 
-    let stream = gulp.src('./src/index.html');
-    if (process.env.HEROKU) {
-        stream.pipe(replace("<!--$DEV_BANNER-->", "@import \"developmentBanner.html\""))
-    }
-    stream.pipe(replace("$INIT_SENTRY", process.env.HEROKU !== undefined));
-    stream.pipe(replace("$INIT_GOOGLE_ANALYTICS", process.env.HEROKU !== undefined));
+    const stream = gulp.src('./src/index.html');
+    stream.pipe(replace('<!--$DEV_BANNER-->', '@import "developmentBanner.html"'));
+    stream.pipe(replace('$INIT_SENTRY', process.env.HEROKU !== undefined));
+    stream.pipe(replace('$INIT_GOOGLE_ANALYTICS', process.env.HEROKU !== undefined));
 
     stream.pipe(plumber())
         .pipe(gulpImport('./src/components/'))
-        .pipe(replace("$GIT_BRANCH", process.env.GIT_BRANCH))
-        .pipe(replace("$DEV_DESCRIPTION", process.env.DEV_DESCRIPTION !== undefined ? process.env.DEV_DESCRIPTION : ""))
+        .pipe(replace('$GIT_BRANCH', process.env.GIT_BRANCH !== undefined ? process.env.GIT_BRANCH : ''))
+        .pipe(replace('$DEV_DESCRIPTION', process.env.DEV_DESCRIPTION !== undefined ? process.env.DEV_DESCRIPTION : ''))
         .pipe(ejs())
         .pipe(gulp.dest(htmlDest))
         .pipe(browserSync.reload({stream: true}));
@@ -100,7 +100,7 @@ gulp.task('html', () => {
 });
 
 gulp.task('scripts', () => {
-    let tsProject = typescript.createProject('tsconfig.json');
+    const tsProject = typescript.createProject('tsconfig.json');
     return tsProject.src()
         .pipe(tsProject())
         .pipe(gulp.dest(dests.scripts))
@@ -135,7 +135,7 @@ gulp.task('serveProd', function () {
     connect.server({
         root: ['build'],
         port: process.env.PORT || 3000,
-        host: "0.0.0.0",
+        host: '0.0.0.0',
         livereload: false
     });
 });
