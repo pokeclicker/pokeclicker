c91746e1357bdd64610885348625a4476731c97a
RedSparr0w <admin@redsparr0w.com>
2020-03-11 14:41:59 +1300

    Create TownName.ts

---
+++ src/scripts/towns/TownName.ts
@@ -0,0 +1,28 @@
+enum TownName {
+    'None' = -1,
+    //Kanto
+    'Pewter_City',
+    'Cerulean_City',
+    'Vermillion_City',
+    'Celadon_City',
+    'Saffron_City',
+    'Fuchsia_City',
+    'Cinnabar_Island',
+    'Viridian_City',
+    'Pallet_Town',
+    'Lavender_Town',
+    'Indigo_Plateau_Kanto',
+    // Johto
+    'New_Bark_Town',
+    'Cherrygrove_City',
+    'Violet_City',
+    'Azalea_Town',
+    'Goldenrod_City',
+    'Ecruteak_City',
+    'Olivine_City',
+    'Cianwood_City',
+    'Mahogany_Town',
+    'Blackthorn_City',
+    // Hoenn
+    'Slateport_City',
+}
