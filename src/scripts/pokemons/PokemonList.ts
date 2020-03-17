11a5236e1d23e28a05f516e1686ef59b6df4dbb6
anapple96 <62083669+anapple96@users.noreply.github.com>
2020-03-17 17:25:24 +0700

    Update PokemonList.ts

--- src/scripts/pokemons/PokemonList.ts
+++ src/scripts/pokemons/PokemonList.ts
@@ -14211,7 +14211,7 @@ const pokemonList: {
 
 // TODO move to its own initialize method that gets called on game start.
 pokemonList.forEach(p => {
-    p.attack = Math.round(Math.max(Math.round(Math.max(1, Math.floor(Math.sqrt((2 * p.base.hitpoints)) * (2 * Math.round(Math.sqrt(p.base.attack * specialAttack) + Math.sqrt(p.base.speed)) * Math.sqrt(2 * Math.round(Math.sqrt(p.defense * p.base.specialDefense) + Math.sqrt(p.base.speed))))))) / 300));
+    p.attack = Math.round(Math.max(Math.round(Math.max(1, Math.floor(Math.sqrt((2 * p.base.hitpoints)) * (2 * Math.round(Math.sqrt(p.base.attack * p.base.specialAttack) + Math.sqrt(p.base.speed)) * Math.sqrt(2 * Math.round(Math.sqrt(p.base.defense * p.base.specialDefense) + Math.sqrt(p.base.speed))))))) / 300));
     if (p.baby) {
         p.evolutions?.forEach(evo => pokemonDevolutionMap[evo.evolvedPokemon] = evo.basePokemon);
     }
