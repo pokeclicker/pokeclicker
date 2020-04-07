952a0db1d4f660503ca3a712e77b3bf3984027ce
anapple96 <62083669+anapple96@users.noreply.github.com>
2020-04-07 21:36:43 +0700

    Update Breeding.ts

--- src/scripts/breeding/Breeding.ts
+++ src/scripts/breeding/Breeding.ts
@@ -233,11 +233,6 @@ class Breeding implements Feature {
     }
 
     public gainEggSlot() {
-        if (this.eggSlots < this.eggList.length) {
-            this.eggSlots += 1;
-        } else {
-            console.error(`Error : ${this.eggSlots} is larger than ${this.eggList.length}.`);
-        }
     }
 
     get eggList(): Array<KnockoutObservable<Egg>> {
