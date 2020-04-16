447016cc95f96d48f9d404c6b82479b53e4af042
RedSparr0w <admin@redsparr0w.com>
2020-04-17 11:28:18 +1200

    Nerf shiny quest gains

--- src/scripts/GameConstants.ts
+++ src/scripts/GameConstants.ts
@@ -134,7 +134,7 @@ namespace GameConstants {
     export const GAIN_TOKENS_BASE_REWARD      = CAPTURE_POKEMONS_BASE_REWARD / 13; // <route number> tokens gained for every capture
     export const HATCH_EGGS_BASE_REWARD       = questBase * 33;      // Dimava
     export const MINE_LAYERS_BASE_REWARD      = questBase * 720;     // Average of 1/4 squares revealed = 75 energy ~ 12 minutes ~ 720 pokemons
-    export const SHINY_BASE_REWARD            = questBase * 6000;    // Dimava
+    export const SHINY_BASE_REWARD            = questBase * 3000;    // Dimava
     export const USE_OAK_ITEM_BASE_REWARD     = DEFEAT_POKEMONS_BASE_REWARD; // not balanced at all for some oak items
 
     export const ACTIVE_QUEST_MULTIPLIER      = 4;
