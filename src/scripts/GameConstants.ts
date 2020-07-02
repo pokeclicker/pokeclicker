46da86db3e278c66d760808800396e78acca5821
RedSparr0w <admin@redsparr0w.com>
2020-07-03 00:45:02 +1200

    Fix pokedex filtering

--- src/scripts/GameConstants.ts
+++ src/scripts/GameConstants.ts
@@ -23,13 +23,13 @@ namespace GameConstants {
     export const MAX_AVAILABLE_REGION = 2; // Hoenn
 
     export const TotalPokemonsPerRegion = [
-        151, // 151
-        251, // 100
-        386, // 135
-        493, // 107
-        649, // 156
-        721, // 72
-        809, // 88
+        151, // 151 - Kanto
+        251, // 100 - Johto
+        386, // 135 - Hoenn
+        493, // 107 - Sinnoh
+        649, // 156 - Unova
+        721, // 72 - Kalos
+        809, // 88 - Alola
     ];
 
     export const ITEM_USE_TIME = 30;
