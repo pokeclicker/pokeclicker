a5808ff1827158456c809a12606bf7a48e30e336
RedSparr0w <admin@redsparr0w.com>
2020-07-02 14:50:12 +1200

    fixup

--- src/scripts/Update.ts
+++ src/scripts/Update.ts
@@ -2,8 +2,8 @@ class Update implements Saveable {
     defaults: object;
     saveKey = 'update';
 
-    version: string = '0.4.0';
-    saveVersion: string = '0.0.0';
+    version = '0.4.0';
+    saveVersion = '0.0.0';
 
     constructor() {}
 
@@ -13,32 +13,40 @@ class Update implements Saveable {
 
     check() {
         let playerData;
-        try{ playerData = JSON.parse(localStorage.player); }catch(O_o){ playerData = {statistics: {}}; }
+        try {
+            playerData = JSON.parse(localStorage.player);
+        } catch (ಠ_ಠ) {
+            playerData = {statistics: {}};
+        }
 
         // v0.4.0 - Statistics update
-        if (this.isOlderVersion(this.saveVersion, '0.4.0')){
-            try{
+        if (this.isOlderVersion(this.saveVersion, '0.4.0')) {
+            try {
                 const statistics = {
-                  ...playerData.statistics,
-                  pokemonCaptured: playerData._caughtAmount,
-                  pokemonDefeated: playerData._defeatedAmount,
-                  totalShinyPokemonCaptured: playerData._shinyCatches,
-                  totalPokemonCaptured: playerData.statistics.pokemonCaptured,
-                  totalPokemonDefeated: playerData.statistics.pokemonDefeated,
+                    ...playerData.statistics,
+                    pokemonCaptured: playerData._caughtAmount,
+                    pokemonDefeated: playerData._defeatedAmount,
+                    totalShinyPokemonCaptured: playerData._shinyCatches,
+                    totalPokemonCaptured: playerData.statistics.pokemonCaptured,
+                    totalPokemonDefeated: playerData.statistics.pokemonDefeated,
                 };
 
                 console.log('statistics', statistics);
                 App.game.statistics.fromJSON(statistics);
-            }catch(O_o){ console.error(`[update] v0.4.0 - Couldn't update statistics..`); }
+            } catch (ಠ_ಠ) {
+                console.error('[update] v0.4.0 - Couldn\'t update statistics..');
+            }
         }
 
         if (this.saveVersion != this.version) {
-            Notifier.notify({ title: `[v${this.version}] Game has been updated!`, message: `Check the changelog for details!`, type: GameConstants.NotificationOption.primary, timeout: 6e4 });
+            Notifier.notify({ title: `[v${this.version}] Game has been updated!`, message: 'Check the changelog for details!', type: GameConstants.NotificationOption.primary, timeout: 6e4 });
         }
     }
 
     fromJSON(json): void {
-        if (!json) return;
+        if (!json) {
+            return;
+        }
 
         this.saveVersion = json.version || '0.0.0';
     }
@@ -46,7 +54,7 @@ class Update implements Saveable {
     toJSON(): object {
         return {
             version: this.version,
-        }
+        };
     }
 
 }
