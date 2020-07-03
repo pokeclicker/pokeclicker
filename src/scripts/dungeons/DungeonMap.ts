25beb7e10bd6d83c3d79666e823d5b05e5324c24
RedSparr0w <admin@redsparr0w.com>
2020-07-04 10:48:05 +1200

    Possibly fix dungeon bug, Hopefully

--- src/scripts/dungeons/DungeonMap.ts
+++ src/scripts/dungeons/DungeonMap.ts
@@ -126,7 +126,9 @@ class DungeonMap {
         }
 
         // Shuffle the tiles randomly
-        this.shuffle(mapList);
+        while (mapList[mapList.length - Math.floor(this.size / 2) - 1].type() != GameConstants.DungeonTile.empty) {
+            this.shuffle(mapList);
+        }
 
         // Create a 2d array
         const map: DungeonTile[][] = [];
