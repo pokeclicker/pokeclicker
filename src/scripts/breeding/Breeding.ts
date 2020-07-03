40173b7956a08e64dc1ff3e7d811ef48cb7316af
RedSparr0w <admin@redsparr0w.com>
2020-07-03 20:39:40 +1200

    Fixup being able to obtain fossil Pokemon from future regions before reaching that region
    (cherry picked from commit 1c4548d28b7297189a8f2c67d4b8fdf3c3ca595c)

--- src/scripts/breeding/Breeding.ts
+++ src/scripts/breeding/Breeding.ts
@@ -112,6 +112,9 @@ class Breeding implements Feature {
     }
 
     public gainEgg(e: Egg) {
+        if (e.isNone()) {
+            return false;
+        }
         for (let i = 0; i < this._eggList.length; i++) {
             if (this._eggList[i]().isNone()) {
                 this._eggList[i](e);
@@ -202,6 +205,11 @@ class Breeding implements Feature {
 
     public createFossilEgg(fossil: string): Egg {
         const pokemonName = GameConstants.FossilToPokemon[fossil];
+        const pokemonNativeRegion = PokemonHelper.calcNativeRegion(pokemonName);
+        if (pokemonNativeRegion > player.highestRegion()) {
+            Notifier.notify({ message: 'You must progress further before you can uncover this fossil Pokemon!', type: GameConstants.NotificationOption.warning, timeout: 5e3 });
+            return new Egg();
+        }
         return this.createEgg(pokemonName, EggType.Fossil);
     }
 
