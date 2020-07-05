844018c3df5b76eb218adc9b4ad9be6bf4fa32f3
RedSparr0w <admin@redsparr0w.com>
2020-07-05 23:15:33 +1200

    Add Flying Pikachu event

--- src/scripts/specialEvents/SpecialEvents.ts
+++ src/scripts/specialEvents/SpecialEvents.ts
@@ -32,3 +32,16 @@ class SpecialEvents implements Feature {
 
     update(delta: number): void {}  // This method intentionally left blank
 }
+
+// TODO: Fetch events from a server each 1/2/3/6/12/24 hours
+// Create our events here for now
+SpecialEvents.newEvent(1, 'Flying Pikachu', 'Encounter Flying Pikachu for a limited time on any route in Kanto.',
+    // Start
+    new Date(2020, 6, 6), () => {
+        Object.keys(pokemonsPerRoute[GameConstants.Region.kanto]).forEach(route => pokemonsPerRoute[GameConstants.Region.kanto][route].land.push('Flying Pikachu'));
+    },
+    // End
+    new Date(2020, 6, 13), () => {
+        Object.keys(pokemonsPerRoute[GameConstants.Region.kanto]).forEach(route => pokemonsPerRoute[GameConstants.Region.kanto][route].land = pokemonsPerRoute[GameConstants.Region.kanto][route].land.filter(p => p != 'Flying Pikachu'));
+    }
+);
