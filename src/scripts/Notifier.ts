89a1ec94a9f477581e2ffee4733fe60329a45d28
RedSparr0w <admin@redsparr0w.com>
2020-07-05 23:14:15 +1200

    Make sure page loaded before allowing notifications
    (cherry picked from commit e6633cc65d6d192d58cedc74d2195b8d1abda622)

--- src/scripts/Notifier.ts
+++ src/scripts/Notifier.ts
@@ -2,27 +2,30 @@
 class Notifier {
 
     public static notify({ message, type = GameConstants.NotificationOption.primary, title = '', timeout = 3000, time = 'just now' }: { message: string; type?: GameConstants.NotificationOption; title?: string; timeout?: number; time?: string }) {
-        const toastID = Math.random().toString(36).substr(2, 9);
-        const toastHTML = `<div id="${toastID}" class="toast bg-${GameConstants.NotificationOption[type]}" data-autohide="false">
-    ${title ? `<div class="toast-header">
-      <strong class="mr-auto text-primary">${title || ''}</strong>
-      <small class="text-muted">${time}</small>
-      <button type="button" class="ml-2 mb-1 close" data-dismiss="toast">×</button>
-    </div>` : ''}
-    <div class="toast-body text-light">
-      ${message}
-      ${title ? '' : '<button type="button" class="ml-2 mb-1 close" data-dismiss="toast">×</button>'}
-    </div>
-  </div>`;
-        $('#toaster').prepend(toastHTML);
-        $('.toast').toast('show');
-        $(`#${toastID}`).on('shown.bs.toast', function (el) {
-            setTimeout(() => {
-                $(`#${toastID}`).toast('hide');
-            }, timeout);
-        });
-        $(`#${toastID}`).on('hidden.bs.toast', function () {
-            document.getElementById(toastID).remove();
+
+        $(document).ready(function() {
+            const toastID = Math.random().toString(36).substr(2, 9);
+            const toastHTML = `<div id="${toastID}" class="toast bg-${GameConstants.NotificationOption[type]}" data-autohide="false">
+                  ${title ? `<div class="toast-header">
+                    <strong class="mr-auto text-primary">${title || ''}</strong>
+                    <small class="text-muted">${time}</small>
+                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast">×</button>
+                  </div>` : ''}
+                  <div class="toast-body text-light">
+                    ${message}
+                    ${title ? '' : '<button type="button" class="ml-2 mb-1 close" data-dismiss="toast">×</button>'}
+                  </div>
+                </div>`;
+            $('#toaster').prepend(toastHTML);
+            $('.toast').toast('show');
+            $(`#${toastID}`).on('shown.bs.toast', function (el) {
+                setTimeout(() => {
+                    $(`#${toastID}`).toast('hide');
+                }, timeout);
+            });
+            $(`#${toastID}`).on('hidden.bs.toast', function () {
+                document.getElementById(toastID).remove();
+            });
         });
     }
 }
