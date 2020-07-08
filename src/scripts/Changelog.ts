cd1160266f802e0bb7d7997b6a772794599446b5
RedSparr0w <admin@redsparr0w.com>
2020-07-08 12:34:09 +1200

    Add information and confirmation check before traveling to next available region

--- src/scripts/Changelog.ts
+++ src/scripts/Changelog.ts
@@ -32,6 +32,7 @@ const changelogItems = [
     // v0.4.7
     new Changelog(changelogType.UPDATE, 'v0.4.7 - Layout'),
     new Changelog(changelogType.NEW, 'Added option to sort Pokémon list by base attack'),
+    new Changelog(changelogType.CHANGE, 'Added information and confirmation check before traveling to next available region'),
     new Changelog(changelogType.CHANGE, 'Updated the Oak Items layout'),
     new Changelog(changelogType.CHANGE, 'Updated the Hoenn map'),
     new Changelog(changelogType.FIXED, 'Event Pokémon no longer count towards achievements or being able to travel to next region'),
