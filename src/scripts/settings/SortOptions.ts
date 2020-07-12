89c44931a22ccf5c202a8908942efbc36baa3e38
Jessica Scoltock <jessica.scoltock@protonmail.com>
2020-07-12 11:58:03 +0000

    Add option to sort pokemon list by base attack (#702)
    * Refactor sort options
    
    Moves all sort option behaviour into a single location,
    to make adding new options easier and clean up the sort code
    
    * Add base attack sorting option to party list
    
    Co-authored-by: Isha Dijcks <ishadijcks@gmail.com>

--- src/scripts/settings/SortOptions.ts
+++ src/scripts/settings/SortOptions.ts
@@ -3,5 +3,53 @@ enum SortOptions {
     'name' = 1,
     'attack' = 2,
     'level' = 3,
-    'shiny' = 4
+    'shiny' = 4,
+    'baseAttack' = 5,
 }
+
+type SortOptionConfig = {
+    // Display text in sort dropdown
+    text: string;
+
+    // How to get the comparison value from a PartyPokemon
+    getValue: (p: PartyPokemon) => any;
+
+    // true if the default sort direction should be descending
+    invert?: boolean;
+}
+
+const SortOptionConfigs: Record<SortOptions, SortOptionConfig> = {
+    [SortOptions.id]: {
+        'text': 'Pokemon #',
+        'getValue': p => p.id,
+    },
+
+    [SortOptions.name]: {
+        'text': 'Name',
+        'getValue': p => p.name,
+    },
+
+    [SortOptions.attack]: {
+        'text': 'Attack',
+        'getValue': p => p.calculateAttack(),
+        'invert': true,
+    },
+
+    [SortOptions.level]: {
+        'text': 'Level',
+        'getValue': p => p.level,
+        'invert': true,
+    },
+
+    [SortOptions.shiny]: {
+        'text': 'Shiny',
+        'getValue': p => Number(App.game.party.alreadyCaughtPokemon(PokemonHelper.getPokemonByName(p.name).id, true)),
+        'invert': true,
+    },
+
+    [SortOptions.baseAttack]: {
+        'text': 'Base Attack',
+        'getValue': p => p.baseAttack,
+        'invert': true,
+    },
+};
