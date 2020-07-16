8268936e52c1cc6063b1269e71fd1bf562259da6
RedSparr0w <admin@redsparr0w.com>
2020-07-16 12:55:17 +1200

    Update shard modal layout

--- src/scripts/Changelog.ts
+++ src/scripts/Changelog.ts
@@ -33,6 +33,7 @@ const changelogItems = [
     new Changelog(changelogType.UPDATE, 'v0.4.11'),
     new Changelog(changelogType.NEW, 'Towns will show as orange if you have not yet completed the gym'),
     new Changelog(changelogType.CHANGE, 'The Cell Battery Oak Item will now gain points each time you find an item'),
+    new Changelog(changelogType.CHANGE, 'Updated the layout of the shard modal'),
     new Changelog(changelogType.FIXED, 'Stone evolutions will now only show the Pokéball if all possible evolutions have been caught'),
 
     // v0.4.10
