cecd8a01d52f3f0443350e14099f908164f5aaf6
RedSparr0w <admin@redsparr0w.com>
2020-07-16 18:01:19 +1200

    Make the map able to be hidden

--- src/scripts/Changelog.ts
+++ src/scripts/Changelog.ts
@@ -32,6 +32,7 @@ const changelogItems = [
     // v0.4.11
     new Changelog(changelogType.UPDATE, 'v0.4.11'),
     new Changelog(changelogType.NEW, 'Towns will show as orange if you have not yet completed the gym'),
+    new Changelog(changelogType.NEW, 'Map is able to be hidden now'),
     new Changelog(changelogType.CHANGE, 'The Cell Battery Oak Item will now gain points each time you find an item'),
     new Changelog(changelogType.CHANGE, 'Updated the layout of the shard modal'),
     new Changelog(changelogType.FIXED, 'Stone evolutions will now only show the Pokéball if all possible evolutions have been caught'),
