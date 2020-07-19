1b14e9b4d5b8eff58e3b4926ade127cbb4963624
RedSparr0w <admin@redsparr0w.com>
2020-07-19 22:52:13 +1200

    Moved shiny icon further into the corner in the Day Care view

--- src/scripts/Changelog.ts
+++ src/scripts/Changelog.ts
@@ -33,7 +33,8 @@ const changelogItems = [
     new Changelog(changelogType.UPDATE, 'v0.4.13'),
     new Changelog(changelogType.CHANGE, 'Updated Masterball pricing'),
     new Changelog(changelogType.CHANGE, 'Allow the game to continue running while hatchery menu is open'),
-    new Changelog(changelogType.CHANGE, 'Add button to open Dock for cities located nearby'),
+    new Changelog(changelogType.CHANGE, 'Added button to open Dock for cities located nearby'),
+    new Changelog(changelogType.CHANGE, 'Moved shiny icon further into the corner in the Day Care view, to make it easier to spot already shiny Pokémon'),
 
     // v0.4.12
     new Changelog(changelogType.UPDATE, 'v0.4.12'),
