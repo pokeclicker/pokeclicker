5bcd456167f8242577b8220ac8d84b937f66e8ef
RedSparr0w <admin@redsparr0w.com>
2020-07-30 18:17:20 +1200

    Update Kanto map add pokemon tower, add cinnibar mansion

---
+++ other/Map tools/pokemontower.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.07.21" name="pokemontower" tilewidth="16" tileheight="16" tilecount="36" columns="4">
+ <image source="pokemontower.png" width="64" height="144"/>
+</tileset>
