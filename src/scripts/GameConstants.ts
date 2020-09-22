c84585f255940aa3b6e366d1bad85f8cb8be26fd
anapple96 <62083669+anapple96@users.noreply.github.com>
2020-09-22 13:10:45 +0700

    Buff idleable quests

--- src/scripts/GameConstants.ts
+++ src/scripts/GameConstants.ts
@@ -158,10 +158,10 @@ namespace GameConstants {
     // Numbers calculated by Dimava assumes ability to 1 shot on high routes and some use oak items,
     //   which are now nerfed slightly until upgraded, so those numbers may need further adjusting
     const questBase = 1; // change this to scale all quest points
-    export const DEFEAT_POKEMONS_BASE_REWARD  = questBase * 1;
-    export const CAPTURE_POKEMONS_BASE_REWARD = DEFEAT_POKEMONS_BASE_REWARD / 0.8; // Defeat reward divided by chance to catch (guessed)
-    export const GAIN_MONEY_BASE_REWARD       = questBase * 0.0017;  // Dimava
-    export const GAIN_TOKENS_BASE_REWARD      = CAPTURE_POKEMONS_BASE_REWARD / 13; // <route number> tokens gained for every capture
+    export const DEFEAT_POKEMONS_BASE_REWARD  = questBase * 3;
+    export const CAPTURE_POKEMONS_BASE_REWARD = DEFEAT_POKEMONS_BASE_REWARD / 0.5; // Defeat reward divided by chance to catch (guessed)
+    export const GAIN_MONEY_BASE_REWARD       = questBase * 0.005;  // Dimava
+    export const GAIN_TOKENS_BASE_REWARD      = CAPTURE_POKEMONS_BASE_REWARD / 9; // <route number> tokens gained for every capture
     export const HATCH_EGGS_BASE_REWARD       = questBase * 33;      // Dimava
     export const MINE_LAYERS_BASE_REWARD      = questBase * 720;     // Average of 1/4 squares revealed = 75 energy ~ 12 minutes ~ 720 pokemons
     export const SHINY_BASE_REWARD            = questBase * 3000;    // Dimava
