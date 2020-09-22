f0677bec71fa797a6d2c72cc1f347cff4ed7c7f5
anapple96 <62083669+anapple96@users.noreply.github.com>
2020-09-22 17:52:55 +0700

    Nerf click attack

--- src/scripts/Battle.ts
+++ src/scripts/Battle.ts
@@ -26,9 +26,9 @@ class Battle {
      */
     public static pokemonAttack() {
         // TODO: figure out a better way of handling this
-        // Limit pokemon attack speed, Only allow 1 attack per 900ms
+        // Limit pokemon attack speed, Only allow 1 attack per 750ms
         const now = Date.now();
-        if (this.lastPokemonAttack > now - 900) {
+        if (this.lastPokemonAttack > now - 750) {
             return;
         }
         this.lastPokemonAttack = now;
@@ -48,7 +48,7 @@ class Battle {
         // TODO: figure out a better way of handling this
         // Limit click attack speed, Only allow 1 attack per 20ms (50 per second)
         const now = Date.now();
-        if (this.lastClickAttack > now - 20) {
+        if (this.lastClickAttack > now - 50) {
             return;
         }
         this.lastClickAttack = now;
