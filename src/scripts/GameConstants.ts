8d0d52b49467c0f957f0bd146af1a83755c78d75
LuqDragon <luquinhasdragon@gmail.com>
2020-09-23 16:47:29 -0300

    removed TODO

--- src/scripts/GameConstants.ts
+++ src/scripts/GameConstants.ts
@@ -97,7 +97,6 @@ namespace GameConstants {
         quest_ready_to_complete: new Sound('quest_ready_to_complete', 'Quest is ready to be completed'),
         quest_level_increased: new Sound('quest_level_increased', 'Quest level increased'),
         underground_energy_full: new Sound('underground_energy_full', 'Mining energy reached maximum capacity'),
-        // TODO: needs a notification
         ready_to_harvest: new Sound('ready_to_harvest', 'Farm ready to harvest'),
     };
     export const NotificationSetting = {
