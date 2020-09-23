b39295867683bf4822792139389baf54f70b19dc
anapple96 <62083669+anapple96@users.noreply.github.com>
2020-09-23 15:56:17 +0700

    Nerf egg prices

--- src/scripts/items/EggItem.ts
+++ src/scripts/items/EggItem.ts
@@ -47,11 +47,11 @@ class EggItem extends CaughtIndicatingItem {
 }
 
 
-ItemList['Fire_egg']     = new EggItem(GameConstants.EggItemType.Fire_egg, 1000);
-ItemList['Water_egg']    = new EggItem(GameConstants.EggItemType.Water_egg, 1000);
-ItemList['Grass_egg']    = new EggItem(GameConstants.EggItemType.Grass_egg, 1000);
-ItemList['Fighting_egg'] = new EggItem(GameConstants.EggItemType.Fighting_egg, 1000);
-ItemList['Electric_egg'] = new EggItem(GameConstants.EggItemType.Electric_egg, 1000);
-ItemList['Dragon_egg']   = new EggItem(GameConstants.EggItemType.Dragon_egg, 1000);
-ItemList['Pokemon_egg']  = new EggItem(GameConstants.EggItemType.Pokemon_egg, 1000);
-ItemList['Mystery_egg']  = new EggItem(GameConstants.EggItemType.Mystery_egg, 700);
+ItemList['Fire_egg']     = new EggItem(GameConstants.EggItemType.Fire_egg, 750);
+ItemList['Water_egg']    = new EggItem(GameConstants.EggItemType.Water_egg, 750);
+ItemList['Grass_egg']    = new EggItem(GameConstants.EggItemType.Grass_egg, 750);
+ItemList['Fighting_egg'] = new EggItem(GameConstants.EggItemType.Fighting_egg, 750);
+ItemList['Electric_egg'] = new EggItem(GameConstants.EggItemType.Electric_egg, 750);
+ItemList['Dragon_egg']   = new EggItem(GameConstants.EggItemType.Dragon_egg, 750);
+ItemList['Pokemon_egg']  = new EggItem(GameConstants.EggItemType.Pokemon_egg, 750);
+ItemList['Mystery_egg']  = new EggItem(GameConstants.EggItemType.Mystery_egg, 500);
