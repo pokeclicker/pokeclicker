9128ee591c2d1307ab8c89441024dee0c9b15862
LuqDragon <luquinhasdragon@gmail.com>
2020-09-26 01:56:28 -0300

    added a use multiplier to battle items

--- src/scripts/effectEngine/effectEngineRunner.ts
+++ src/scripts/effectEngine/effectEngineRunner.ts
@@ -23,8 +23,8 @@ class EffectEngineRunner {
         return player.effectList[itemName]();
     }
 
-    public static addEffect(itemName: string) {
-        player.effectList[itemName](Math.max(0, player.effectList[itemName]() +  GameConstants.ITEM_USE_TIME));
+    public static addEffect(itemName: string, amount: number) {
+        player.effectList[itemName](Math.max(0, player.effectList[itemName]() + (GameConstants.ITEM_USE_TIME * amount)));
         this.updateFormattedTimeLeft(itemName);
     }
 
