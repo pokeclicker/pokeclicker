f8a979be7edacb378222818bac7b3a261caa8347
LuqDragon <luquinhasdragon@gmail.com>
2020-09-29 04:53:57 -0300

    really fix now

--- src/scripts/achievements/CaughtPokemonByRegionRequirement.ts
+++ src/scripts/achievements/CaughtPokemonByRegionRequirement.ts
@@ -8,7 +8,7 @@ class CaughtUniquePokemonsByRegionRequirement extends Requirement {
     }
 
     public getProgress() {
-        return Math.min(new Set(App.game.party.caughtPokemon.filter(p => p.id > 0).map(p => Math.floor(p.id))).size, this.requiredValue);
+        return Math.min(new Set(App.game.party.caughtPokemon.filter(p => p.id > 0 && PokemonHelper.calcNativeRegion(p.name) === this.region).map(p => Math.floor(p.id))).size, this.requiredValue);
     }
 
     public hint(): string {
