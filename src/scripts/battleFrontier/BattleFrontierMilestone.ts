90d69d740833276b35ca9e863cd353f2d2547143
Pepper Muangmingsuk <33099029+HLXII@users.noreply.github.com>
2020-10-13 18:53:19 -0500

    Fix BF milestones

--- src/scripts/battleFrontier/BattleFrontierMilestone.ts
+++ src/scripts/battleFrontier/BattleFrontierMilestone.ts
@@ -2,10 +2,15 @@ class BattleFrontierMilestone {
     constructor (
         public stage: number,
         public rewardFunction: () => void,
-        public image: string = null
+        public image: string = null,
+        private _description?: string,
     ) {}
 
     gain () {
         this.rewardFunction();
+    }
+
+    get description() {
+        return this._description;
     }
 }
