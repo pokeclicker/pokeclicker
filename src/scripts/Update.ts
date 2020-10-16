bdaf729532c7a528edad588934d9c9902b66b80d
anapple96 <62083669+anapple96@users.noreply.github.com>
2020-10-16 22:29:40 +0700

    Bruh

--- src/scripts/Update.ts
+++ src/scripts/Update.ts
@@ -271,11 +271,11 @@ class Update implements Saveable {
             }
             try {
                 for (let s = 101; s < 134; s++) {
-                    saveData.statistics.routeKills = Update.moveIndex(saveData.statistics.dungeonsCleared, s - 52, s); // Adjust Hoenn index
+                    saveData.statistics.routeKills = Update.moveIndex(saveData.statistics.routeKills, s - 52, s); // Adjust Hoenn index
                 }
 
                 for (let s = 201; s < 230; s++) {
-                    saveData.statistics.routeKills = Update.moveIndex(saveData.statistics.dungeonsCleared, s - 118, s); // Adjust Sinnoh index
+                    saveData.statistics.routeKills = Update.moveIndex(saveData.statistics.routeKills, s - 118, s); // Adjust Sinnoh index
                 }
                 // Update save data
                 this.setSaveData(saveData);
