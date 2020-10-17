ce9c730b31ee22c982c14129c90b37f70d76e759
Spenser Jones <hello@spenserj.com>
2020-10-17 07:25:58 -0600

    Fix/typescript modules vscode config (#953)
    * fix: adds git/editorconfig settings for using LF line endings
    
    LF endings were chosen over CLRF as most/all Windows editors can handle
    LF endings, but CLRF often shows additional characters on some Mac/Linux
    editors. This follows the recommendation from Git
    (https://git-scm.com/book/en/v2/Customizing-Git-Git-Configuration) to
    use LF line endings when committing code on projects that are not
    exclusive to Windows.
    
    * fix(typescript): fixes VSCode warnings in modules
    
    * fix(typescript): fixes VSCode warnings in repo root
    
    * chore: remove unused tslint.json
    
    * chore: fix PR comments

--- src/modules/.eslintrc.js
+++ src/modules/.eslintrc.js
@@ -1,26 +1,35 @@
 const path = require('path');
 
 module.exports = {
-  parserOptions: {
-      project: path.resolve(__dirname, '../../tsconfigWithModules.json'),
-  },
-  extends: ['airbnb-typescript/base'],
-  globals: {
-    ko: 'readonly',
-  },
-  rules: {
-      indent: 'off',
-      '@typescript-eslint/indent': ['error', 4, { 'SwitchCase': 1 }],
-      '@typescript-eslint/comma-dangle': ['error', {
-        arrays: 'always-multiline',
-        objects: 'always-multiline',
-        imports: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'always-multiline',
-        enums: 'always-multiline',
-        generics: 'always-multiline',
-        tuples: 'always-multiline',
-      }],
-      '@typescript-eslint/lines-between-class-members': ['error', { exceptAfterSingleLine: true }],
-  },
+    root: true,
+    env: {
+        browser: true,
+        es2020: true,
+    },
+    parserOptions: {
+        project: [
+            path.resolve(__dirname, './tsconfig.eslint.json'),
+            path.resolve(__dirname, './tsconfig.json'),
+        ],
+        tsconfigRootDir: __dirname,
+    },
+    extends: ['airbnb-typescript/base'],
+    globals: {
+        ko: 'readonly',
+    },
+    rules: {
+        indent: 'off',
+        '@typescript-eslint/indent': ['error', 4, { SwitchCase: 1 }],
+        '@typescript-eslint/comma-dangle': ['error', {
+            arrays: 'always-multiline',
+            objects: 'always-multiline',
+            imports: 'always-multiline',
+            exports: 'always-multiline',
+            functions: 'always-multiline',
+            enums: 'always-multiline',
+            generics: 'always-multiline',
+            tuples: 'always-multiline',
+        }],
+        '@typescript-eslint/lines-between-class-members': ['error', { exceptAfterSingleLine: true }],
+    },
 };
