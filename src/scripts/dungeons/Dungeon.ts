3cb811940c5505264102bcbace8839fc323d8cd5
RedSparr0w <admin@redsparr0w.com>
2020-10-29 22:27:09 +1300

    Simplify logic

--- src/scripts/dungeons/Dungeon.ts
+++ src/scripts/dungeons/Dungeon.ts
@@ -39,15 +39,9 @@ class Dungeon {
     }
 
     public calculateAllPokemonNames() {
-        const pokemonNameSet = new Set(this.pokemonList);
-        this.availableBosses().forEach(boss => {
-            pokemonNameSet.add(boss.name);
-        });
-        this.allAvailablePokemonNames = [...pokemonNameSet];
-        this.bossList.forEach(boss => {
-            pokemonNameSet.add(boss.name);
-        });
-        this.allPokemonNames = [...pokemonNameSet];
+        // Put the names into a Set to filter out any duplicate values
+        this.allPokemonNames = [...new Set([...this.pokemonList, ...this.bossList.map(b => b.name)])];
+        this.allAvailablePokemonNames = [...new Set([...this.pokemonList, ...this.availableBosses().map(b => b.name)])];
     }
 
     public availableBosses(): DungeonBossPokemon[] {
