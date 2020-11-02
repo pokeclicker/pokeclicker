f26d6301e5adeec1b66170f0e413bb20202b46e6
LuqDragon <luquinhasdragon@gmail.com>
2020-11-01 22:32:17 -0300

    Update src/scripts/achievements/AchievementHandler.ts
    Co-authored-by: Danial <RedSparr0w@users.noreply.github.com>

--- src/scripts/achievements/AchievementHandler.ts
+++ src/scripts/achievements/AchievementHandler.ts
@@ -52,7 +52,7 @@ class AchievementHandler {
             this.resetPages();
         } else if (this.getAchievementListWithIndex().length === 0 && this.navigateIndex() > 0) {
             this.calculateNumberOfTabs();
-            this.navigateIndex(this.numberOfTabs());
+            this.navigateIndex(this.numberOfTabs()  - 1);
         }
     }
 
