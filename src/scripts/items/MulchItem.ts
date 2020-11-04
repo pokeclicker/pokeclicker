4ab481380848c4751e351d145f9a3492061b737b
RedSparr0w <admin@redsparr0w.com>
2020-11-04 16:36:57 +1300

    Remove unused argument

--- src/scripts/items/MulchItem.ts
+++ src/scripts/items/MulchItem.ts
@@ -1,9 +1,11 @@
 ///<reference path="Item.ts"/>
+///<reference path="../farming/MulchType.ts"/>
+
 class MulchItem extends Item {
     type: MulchType;
 
-    constructor(type: MulchType, basePrice: number, currency: GameConstants.Currency = GameConstants.Currency.farmPoint, displayName: string) {
-        super(MulchType[type], basePrice, currency, { multiplierDecreaser: MultiplierDecreaser.Berry }, displayName);
+    constructor(type: MulchType, basePrice: number, displayName: string) {
+        super(MulchType[type], basePrice, GameConstants.Currency.farmPoint, { multiplierDecreaser: MultiplierDecreaser.Berry }, displayName);
         this.type = type;
     }
 
@@ -16,7 +18,7 @@ class MulchItem extends Item {
 }
 
 // TODO: HLXII - Balance cost based on highest yield strategy
-ItemList['Boost_Mulch']   = new MulchItem(MulchType.Boost_Mulch, 200, undefined, 'Boost Mulch');
-ItemList['Rich_Mulch']  = new MulchItem(MulchType.Rich_Mulch, 300, undefined, 'Rich Mulch');
-ItemList['Surprise_Mulch']  = new MulchItem(MulchType.Surprise_Mulch, 400, undefined, 'Surprise Mulch');
-ItemList['Amaze_Mulch'] = new MulchItem(MulchType.Amaze_Mulch, 500, undefined, 'Amaze Mulch');
+ItemList['Boost_Mulch']   = new MulchItem(MulchType.Boost_Mulch, 200, 'Boost Mulch');
+ItemList['Rich_Mulch']  = new MulchItem(MulchType.Rich_Mulch, 300, 'Rich Mulch');
+ItemList['Surprise_Mulch']  = new MulchItem(MulchType.Surprise_Mulch, 400, 'Surprise Mulch');
+ItemList['Amaze_Mulch'] = new MulchItem(MulchType.Amaze_Mulch, 500, 'Amaze Mulch');
