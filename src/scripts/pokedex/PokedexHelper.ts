d7fcb017132a82cd084d8916c58472194f482674
LuqDragon <luquinhasdragon@gmail.com>
2020-11-05 19:07:58 -0300

    space

--- src/scripts/pokedex/PokedexHelper.ts
+++ src/scripts/pokedex/PokedexHelper.ts
@@ -182,7 +182,6 @@ class PokedexHelper {
     private static isPureType(pokemon: PokemonListData, type: (PokemonType | null)): boolean {
         return (pokemon.type.length === 1 && (type == null || pokemon.type[0] === type));
     }
-
 }
 
 $(document).ready(function () {
