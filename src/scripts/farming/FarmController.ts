13b0764e52d5ab24b57e2ff73a54fbc955ceeb74
Pepper Muangmingsuk <33099029+HLXII@users.noreply.github.com>
2020-11-15 21:44:40 -0600

    Revert "Remove tooltips when not visible"
    This reverts commit 2cd98436134fd72dfa13b8d132376fa04a235136.

--- src/scripts/farming/FarmController.ts
+++ src/scripts/farming/FarmController.ts
@@ -11,8 +11,6 @@ class FarmController {
     public static selectedMulch: KnockoutObservable<MulchType> = ko.observable(MulchType.Boost_Mulch);
     public static selectedShovel: KnockoutObservable<boolean> = ko.observable(false);
 
-    public static farmViewVisible: KnockoutObservable<boolean> = ko.observable(true);
-
     public static berryListVisible: KnockoutObservable<boolean> = ko.observable(true);
 
     public static multipliers = ['×1', '×10', '×100', '×1000', 'All'];
