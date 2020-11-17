447426f809be66edc67afa0e925160806382123d
Ultima1990 <69112975+Ultima1990@users.noreply.github.com>
2020-11-17 06:16:52 +0100

    unova(environment): Update Unova route/dungeon environments (#1072)
    * Unova Environments
    
    * Ice for Shoal Cave
    
    * Fix Striatons while I'm at it
    
    Co-authored-by: Danial <RedSparr0w@users.noreply.github.com>

--- src/modules/GameConstants.ts
+++ src/modules/GameConstants.ts
@@ -398,14 +398,15 @@ export const WaterAreas = {
     1: new Set([40, 41, 'Slowpoke Well']),
     2: new Set([105, 106, 107, 108, 109, 118, 122, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 'Shoal Cave', 'Seafloor Cavern']),
     3: new Set([218, 219, 220, 223, 230, 'Lake Verity', 'Lake Valor', 'Pastoria City']),
-    4: new Set(['Humilau City']),
+    4: new Set([17, 18, 21, 24, 'Undella Town', 'Humilau City']),
 };
 
 export const IceAreas = {
     0: new Set(['Seafoam Islands']),
     1: new Set(['Mahogany Town', 'Ice Path', 'Whirl Islands']),
-    2: new Set(['Sootopolis City']),
+    2: new Set(['Shoal Cave', 'Sootopolis City']),
     3: new Set([216, 217, 'Lake Acuity', 'Snowpoint City']),
+    4: new Set(['Giant Chasm']),
 };
 
 export const ForestAreas = {
@@ -413,7 +414,7 @@ export const ForestAreas = {
     1: new Set([36, 38, 43, 'Azalea Town', 'Ilex Forest']),
     2: new Set([119, 'Petalburg Woods']),
     3: new Set([201, 204, 'Eterna Forest', 'Eterna City', 'Fullmoon Island', 'Newmoon Island']),
-    4: new Set(['Lostlorn Forest', 'Pinwheel Forest', 'Giant Chasm', 'Pledge Grove', 'Castelia City']),
+    4: new Set([6, 'Lostlorn Forest', 'Pinwheel Forest', 'Pledge Grove', 'Floccesy Town']),
 };
 
 export const CaveAreas = {
@@ -421,7 +422,7 @@ export const CaveAreas = {
     1: new Set(['Cianwood City', 'Ruins of Alph', 'Union Cave', 'Mt Mortar', 'Dark Cave']),
     2: new Set(['Rustboro City', 'Dewford Town', 'Rusturf Tunnel', 'Granite Cave', 'New Mauville', 'Meteor Falls', 'Victory Road Hoenn']),
     3: new Set(['Oreburgh Gate', 'Oreburgh City', 'Ravaged Path', 'Wayward Cave', 'Mt. Coronet South', 'Iron Island', 'Mt. Coronet North', 'Victory Road Sinnoh']),
-    4: new Set(['Mistralton Cave', 'Seaside Cave', 'Twist Mountain', 'Reversal Mountain', 'Cave of Being', 'Relic Passage', 'Relica Castle', 'Victory Road Unova']),
+    4: new Set(['Mistralton Cave', 'Seaside Cave', 'Twist Mountain', 'Reversal Mountain', 'Cave of Being', 'Relic Passage', 'Relic Castle', 'Victory Road Unova']),
 };
 
 export const GemCaveAreas = {
@@ -429,7 +430,7 @@ export const GemCaveAreas = {
     1: new Set(['Blackthorn City', 'Mt Silver']),
     2: new Set(['Cave of Origin', 'Sky Pillar']),
     3: new Set(['Spear Pillar', 'Hall of Origin', 'Stark Mountain']),
-    4: new Set(['Chargestone Cave', 'Driftveil City']),
+    4: new Set(['Chargestone Cave']),
 };
 
 export const PowerPlantAreas = {
@@ -437,7 +438,7 @@ export const PowerPlantAreas = {
     1: new Set(['Tin Tower']),
     2: new Set(['Mauville City']),
     3: new Set(['Sunyshore City']),
-    4: new Set(['Castelia Sewers', 'Nimbasa City']),
+    4: new Set(['Castelia Sewers', 'Virbank City', 'Nimbasa City']),
 };
 
 export const MansionAreas = {
@@ -445,7 +446,7 @@ export const MansionAreas = {
     1: new Set(['Olivine City', 'Burned Tower']),
     2: new Set(['Lavaridge Town', 'Petalburg City', 'Jagged Pass', 'Fiery Path', 'Mt. Chimney']),
     3: new Set(['Old Chateau', 'Veilstone City', 'Canalave City', 'Snowpoint Temple']),
-    4: new Set(['Liberty Garden', 'Dreamyard', 'Mistralton City', 'Opelucid City']),
+    4: new Set(['Castelia City', 'Liberty Garden', 'Dreamyard', 'Mistralton City', 'Opelucid City']),
 };
 
 export const GraveyardAreas = {
@@ -453,7 +454,7 @@ export const GraveyardAreas = {
     1: new Set(['Ecruteak City']),
     2: new Set(['Mossdeep City', 'Mt. Pyre']),
     3: new Set(['Hearthome City']),
-    4: new Set(['Virbank City']),
+    4: new Set(['Celestial Tower']),
 };
 
 export enum Starter {
