490db19a1e6289f0d5d71fe1857736f5168687bd
RedSparr0w <admin@redsparr0w.com>
2020-11-18 10:54:47 +1300

    Give proteins their own menu

--- src/scripts/party/PartyPokemon.ts
+++ src/scripts/party/PartyPokemon.ts
@@ -102,10 +102,6 @@ class PartyPokemon implements Saveable {
         }
         if (ItemHandler.useItem('Protein')) {
             GameHelper.incrementObservable(this.proteinsUsed);
-            Notifier.notify({
-                message: `Used 1 × Protein on ${this.name}<br/>Total used: ${this.proteinsUsed().toLocaleString('en-US')}`,
-                type: NotificationConstants.NotificationOption.info,
-            });
         }
     }
 
