b20cfd7a89dd4907ddb4218bda0687a567cb0cff
Ryan Wilson <rywilson56@gmail.com>
2020-11-20 02:08:50 +0000

    ProfOak more accurate UniquePokemonCaught checks.

--- src/scripts/towns/ProfOakNPC.ts
+++ src/scripts/towns/ProfOakNPC.ts
@@ -8,22 +8,24 @@ class ProfOakNPC extends NPC {
     }
 
     get dialogHTML(): string {
-        if (PokemonHelper.calcUniquePokemonsByRegion(GameConstants.Region.sinnoh) >= 107) {
+        const UniquePokemonCaught = App.game.party.caughtPokemon.filter(p => p.id > 0).length;
+
+        if (UniquePokemonCaught >= 493) {
             return `<p>Congratulations, you're more than half-way completed on the national Pokédex!</p>
                     <p>Next stop is Unova! I've always wanted to visit Castelia City personally...</p>`;
         }
 
-        if (PokemonHelper.calcUniquePokemonsByRegion(GameConstants.Region.hoenn) >= 135) {
-            return `<p>Hello! Thanks for visiting again.</p>
+        if (UniquePokemonCaught >= 392) {
+            return `<p>That's another Pokédex completed! Fantastic.</p>
                     <p>I really appreciate being able to see your outstanding progress, thank you!</p>`;
         }
 
-        if (PokemonHelper.calcUniquePokemonsByRegion(GameConstants.Region.johto) >= 100) {
+        if (UniquePokemonCaught >= 254) {
             return `<p>Oh, another regional Pokédex completed so soon?</p>
                     <p>Amazing! Next stop is Hoenn, enjoy the sunshine while you're there!</p>`;
         }
 
-        if (PokemonHelper.calcUniquePokemonsByRegion(GameConstants.Region.kanto) >= 151) {
+        if (UniquePokemonCaught >= 151) {
             return `<p>Congratulations on completing your Kanto Pokédex!"</p>
                     <p>Your journey isn't over yet, a whole world awaits you! Onwards to Johto!</p>`;
         }
