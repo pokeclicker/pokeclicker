de654837e9cf5e8120b9f76406e88fea9be909b9
RedSparr0w <admin@redsparr0w.com>
2020-12-22 11:39:17 +1300

    Dragonite -> Snorlax

--- src/scripts/specialEvents/SpecialEvents.ts
+++ src/scripts/specialEvents/SpecialEvents.ts
@@ -126,19 +126,19 @@ SpecialEvents.newEvent('Lets GO!', 'Encounter special Eevee and Pikachu roaming 
     }
 );
 // Christmas
-SpecialEvents.newEvent('Merry Christmas!', 'Encounter Santa Dragonite for a limited time roaming around Kanto, Johto and Hoenn.',
+SpecialEvents.newEvent('Merry Christmas!', 'Encounter Santa Snorlax for a limited time roaming around Kanto, Johto and Hoenn.',
     // Start
     new Date(new Date().getFullYear(), 11, 24, 1), () => {
         // Add to every region excluding None
         GameHelper.enumNumbers(GameConstants.Region).filter(i => i != GameConstants.Region.none).forEach(region => {
-            RoamingPokemonList.add(region, new RoamingPokemon(pokemonMap['Santa Dragonite']));
+            RoamingPokemonList.add(region, new RoamingPokemon(pokemonMap['Santa Snorlax']));
         });
     },
     // End
     new Date(new Date().getFullYear(), 11, 30, 23), () => {
         // Remove from every region excluding None
         GameHelper.enumNumbers(GameConstants.Region).filter(i => i != GameConstants.Region.none).forEach(region => {
-            RoamingPokemonList.remove(region, 'Santa Dragonite');
+            RoamingPokemonList.remove(region, 'Santa Snorlax');
         });
     }
 );
