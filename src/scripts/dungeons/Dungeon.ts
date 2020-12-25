0983b8a9c7f721447424b13610959d13f4baf80c
anapple96 <62083669+anapple96@users.noreply.github.com>
2020-12-25 11:20:34 +0700

    Removing arceus

--- src/scripts/dungeons/Dungeon.ts
+++ src/scripts/dungeons/Dungeon.ts
@@ -821,7 +821,6 @@ dungeonList['P2 Laboratory'] = new Dungeon('P2 Laboratory',
         new DungeonBossPokemon('Porygon', 62000000, 100),
         new DungeonBossPokemon('Audino', 62000000, 100),
         new DungeonBossPokemon('Mawile', 62000000, 100),
-        new DungeonBossPokemon('Arceus (steel)', 62000000, 100),
         new DungeonBossPokemon('Sableye', 62000000, 100),
     ],
     396500, 230, 100);
