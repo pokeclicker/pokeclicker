5d2ca47b7a613ece2eb1b3704a7d3bad82021948
Jessica Scoltock <jessica.scoltock@protonmail.com>
2020-12-26 06:37:28 +0000

    Add lucky incense to money multiplier

--- src/scripts/items/BattleItem.ts
+++ src/scripts/items/BattleItem.ts
@@ -26,4 +26,4 @@ ItemList['xClick']          = new BattleItem(GameConstants.BattleItemType.xClick
 ItemList['Lucky_egg']       = new BattleItem(GameConstants.BattleItemType.Lucky_egg, '+50% Bonus to experience gained', 800, undefined, 'Lucky Egg', 'exp', 1.5);
 ItemList['Token_collector'] = new BattleItem(GameConstants.BattleItemType.Token_collector, '+50% Bonus to dungeon tokens gained', 1000, undefined, 'Token Collector', 'dungeonToken', 1.5);
 ItemList['Item_magnet']     = new BattleItem(GameConstants.BattleItemType.Item_magnet, '+50% Chance of gaining an extra item', 1500, undefined, 'Item Magnet');
-ItemList['Lucky_incense']   = new BattleItem(GameConstants.BattleItemType.Lucky_incense, '+50% Bonus to money gained', 2000, undefined, 'Lucky Incense');
+ItemList['Lucky_incense']   = new BattleItem(GameConstants.BattleItemType.Lucky_incense, '+50% Bonus to money gained', 2000, undefined, 'Lucky Incense', 'money', 1.5);
