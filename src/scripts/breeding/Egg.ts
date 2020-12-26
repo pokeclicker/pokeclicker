560310c64d20b5907501a29d22e13521cbfabe9e
unclebanks <49108377+unclebanks@users.noreply.github.com>
2020-12-25 21:05:45 -0800

    chore(modules): Move EggType to Module (#1161)
    Co-authored-by: Danial <RedSparr0w@users.noreply.github.com>

--- src/scripts/breeding/Egg.ts
+++ src/scripts/breeding/Egg.ts
@@ -1,5 +1,6 @@
 /// <reference path="../../declarations/GameHelper.d.ts" />
 /// <reference path="../../declarations/DataStore/common/Saveable.d.ts" />
+/// <reference path="../../declarations/breeding/EggType.d.ts" />
 
 class Egg implements Saveable {
     saveKey = 'egg';
