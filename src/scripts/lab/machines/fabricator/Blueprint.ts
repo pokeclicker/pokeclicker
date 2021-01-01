403d7ec6f95a1b9c012a2168acc7ee4bed29921f
Pepper Muangmingsuk <33099029+HLXII@users.noreply.github.com>
2021-01-01 14:38:45 -0600

    Initial Blueprints

--- src/scripts/lab/machines/fabricator/Blueprint.ts
+++ src/scripts/lab/machines/fabricator/Blueprint.ts
@@ -1,5 +1,10 @@
 class Blueprint {
 
-
+    constructor(
+        public type: BlueprintType,
+        public time: number,
+        public cost: { item: BagItem, amount: number}[],
+        public research?: Lab.Research
+    ) { }
 
 }
--- src/scripts/lab/machines/fabricator/Fabricator.ts
+++ src/scripts/lab/machines/fabricator/Fabricator.ts
@@ -12,6 +12,37 @@ class Fabricator extends Machine {
         return state;
     }
 
+    /**
+     * Fabricator Blueprints
+     */
+    public static blueprints: Blueprint[];
+    public static initialize() {
+        this.blueprints = [];
+        // Items
+        this.blueprints[BlueprintType.fire_stone] = new Blueprint(BlueprintType.fire_stone, 1000, [{item: {type: ItemType.shard, id: PokemonType.Fire}, amount: 1000}], Lab.Research.fire_stone);
+        this.blueprints[BlueprintType.water_stone] = new Blueprint(BlueprintType.water_stone, 1000, [], Lab.Research.water_stone);
+        this.blueprints[BlueprintType.thunder_stone] = new Blueprint(BlueprintType.thunder_stone, 1000, [], Lab.Research.thunder_stone);
+        this.blueprints[BlueprintType.leaf_stone] = new Blueprint(BlueprintType.leaf_stone, 1000, [], Lab.Research.leaf_stone);
+        this.blueprints[BlueprintType.moon_stone] = new Blueprint(BlueprintType.moon_stone, 1000, [], Lab.Research.moon_stone);
+        this.blueprints[BlueprintType.sun_stone] = new Blueprint(BlueprintType.sun_stone, 1000, [], Lab.Research.sun_stone);
+        this.blueprints[BlueprintType.dragon_scale] = new Blueprint(BlueprintType.dragon_scale, 1000, [], Lab.Research.dragon_scale);
+        this.blueprints[BlueprintType.metal_coat] = new Blueprint(BlueprintType.metal_coat, 1000, [], Lab.Research.metal_coat);
+        this.blueprints[BlueprintType.upgrade] = new Blueprint(BlueprintType.upgrade, 1000, [], Lab.Research.upgrade);
+        this.blueprints[BlueprintType.dubious_disc] = new Blueprint(BlueprintType.dubious_disc, 1000, [], Lab.Research.dubious_disc);
+        this.blueprints[BlueprintType.electirizer] = new Blueprint(BlueprintType.electirizer, 1000, [], Lab.Research.electirizer);
+        this.blueprints[BlueprintType.magmarizer] = new Blueprint(BlueprintType.magmarizer, 1000, [], Lab.Research.magmarizer);
+        this.blueprints[BlueprintType.protector] = new Blueprint(BlueprintType.protector, 1000, [], Lab.Research.protector);
+        this.blueprints[BlueprintType.reaper_cloth] = new Blueprint(BlueprintType.reaper_cloth, 1000, [], Lab.Research.reaper_cloth);
+        // Machines
+        this.blueprints[BlueprintType.fabricator] = new Blueprint(BlueprintType.fabricator, 1000, [], Lab.Research.fabricator);
+        this.blueprints[BlueprintType.plate_deconstructor] = new Blueprint(BlueprintType.plate_deconstructor, 1000, [], Lab.Research.plate_deconstructor);
+        this.blueprints[BlueprintType.plate_reconstructor] = new Blueprint(BlueprintType.plate_reconstructor, 1000, [], Lab.Research.plate_reconstructor);
+        //this.blueprints[BlueprintType.incubator] = new Blueprint(BlueprintType.incubator, 1000, [], Lab.Research);
+        this.blueprints[BlueprintType.generator] = new Blueprint(BlueprintType.generator, 1000, [], Lab.Research.generator);
+        this.blueprints[BlueprintType.fossil_reviver] = new Blueprint(BlueprintType.fossil_reviver, 1000, [], Lab.Research.fossil_reviver);
+        this.blueprints[BlueprintType.pokeball_factory] = new Blueprint(BlueprintType.pokeball_factory, 1000, [], Lab.Research.pokeball_factory);
+    }
+
     // Handle research upgrades
 
 }
