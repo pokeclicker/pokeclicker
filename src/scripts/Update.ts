74141efec687022070fd213df39c550f1f720ab5
Pepper Muangmingsuk <33099029+HLXII@users.noreply.github.com>
2021-01-02 17:33:00 -0600

    Moving temp save update to much higher version so I don't have to deal with merge conflicts

--- src/scripts/Update.ts
+++ src/scripts/Update.ts
@@ -328,7 +328,7 @@ class Update implements Saveable {
             }
         },
 
-        '0.7.2': ({ playerData, saveData }) => {
+        '0.8.0': ({ playerData, saveData }) => {
             // TODO: HLXII - Move this to the version it'll be released in
             for (const pokemon of saveData.party.caughtPokemon) {
                 if (pokemon.breeding) {
