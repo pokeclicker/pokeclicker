a4f1d6402b5306136ffd8221492d518806b91f68
Pepper Muangmingsuk <33099029+HLXII@users.noreply.github.com>
2021-01-22 01:51:29 -0600

    chore(refactor): Quest Generation Refactor (#1191)
    * Some cleanup doc/refactor
    
    * Move inProgress to createProgressObservables
    
    * Large Refactor
    
    * Bump version
    
    * Remove console logs
    
    Co-authored-by: Danial <RedSparr0w@users.noreply.github.com>
    Co-authored-by: RedSparr0w <admin@redsparr0w.com>

--- src/scripts/quests/questTypes/BuyPokeballsQuest.ts
+++ src/scripts/quests/questTypes/BuyPokeballsQuest.ts
@@ -1,9 +1,23 @@
 /// <reference path="../Quest.ts" />
 
 class BuyPokeballsQuest extends Quest implements QuestInterface {
-    constructor(amount: number, pokeball: GameConstants.Pokeball, reward: number) {
+
+    private pokeball: GameConstants.Pokeball;
+
+    constructor(amount: number, reward: number, pokeball: GameConstants.Pokeball) {
         super(amount, reward);
-        this.description = `Buy ${amount.toLocaleString('en-US')} ${GameConstants.Pokeball[pokeball]}s.`;
-        this.focus = App.game.statistics.pokeballsBought[pokeball];
+        this.pokeball = pokeball;
+        this.focus = App.game.statistics.pokeballsBought[this.pokeball];
+    }
+
+    get description(): string {
+        return `Buy ${this.amount.toLocaleString('en-US')} ${GameConstants.Pokeball[this.pokeball]}s.`;
+    }
+
+    toJSON() {
+        const json = super.toJSON();
+        json['name'] = this.constructor.name;
+        json['data'].push(this.pokeball);
+        return json;
     }
 }
