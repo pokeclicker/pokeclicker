7136829166e7c9a8f46b3fed64d8a1fbedcd3f41
Kush Patel <kushpatel0703@gmail.com>
2021-01-23 00:04:38 -0600

    Initializing Underground Power to 50 to prevent underground messages before unlocking underground

--- src/scripts/underground/Underground.ts
+++ src/scripts/underground/Underground.ts
@@ -6,7 +6,7 @@ class Underground implements Feature {
 
     upgradeList: Array<Upgrade>;
     defaults: Record<string, any>;
-    private _energy: KnockoutObservable<number> = ko.observable(0);
+    private _energy: KnockoutObservable<number> = ko.observable(50);
 
     public static itemSelected;
     public static energyTick: KnockoutObservable<number> = ko.observable(60);
