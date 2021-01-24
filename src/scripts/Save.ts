dfaa7e6cfe5b8813ed3c17a90b0abfe3b2204d60
Pepper Muangmingsuk <33099029+HLXII@users.noreply.github.com>
2021-01-23 22:57:50 -0600

    Remove unused method

--- src/scripts/Save.ts
+++ src/scripts/Save.ts
@@ -99,30 +99,6 @@ class Save {
         return res;
     }
 
-    public static initializeShards(saved?: Array<Array<number>>): Array<Array<KnockoutObservable<number>>> {
-        let res;
-        if (saved) {
-            res = saved.map((type) => {
-                return type.map((effectiveness) => {
-                    return ko.observable(effectiveness);
-                });
-            });
-        } else {
-            res = [];
-            for (const item in PokemonType) {
-                if (!isNaN(Number(item))) {
-                    res[item] = [];
-                    res[item][GameConstants.TypeEffectiveness.Immune] = ko.observable(0);
-                    res[item][GameConstants.TypeEffectiveness.NotVery] = ko.observable(0);
-                    res[item][GameConstants.TypeEffectiveness.Normal] = ko.observable(0);
-                    res[item][GameConstants.TypeEffectiveness.Very] = ko.observable(0);
-                }
-            }
-        }
-
-        return res;
-    }
-
     public static initializeEffects(saved?: Array<string>): { [name: string]: KnockoutObservable<number> } {
         const res = {};
         for (const obj in GameConstants.BattleItemType) {
