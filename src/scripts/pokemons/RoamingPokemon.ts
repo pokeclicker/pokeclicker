3eb3001334439a38b945502f9357e1febdf14448
RedSparr0w <admin@redsparr0w.com>
2021-01-29 23:04:42 +1300

    Fixup roaming MissingNo.

--- src/scripts/pokemons/RoamingPokemon.ts
+++ src/scripts/pokemons/RoamingPokemon.ts
@@ -1,9 +1,12 @@
 class RoamingPokemon {
+    public pokemon: DataPokemon;
+
     constructor(
-        public pokemon: DataPokemon,
-        public weight: number = 256,
+        public pokemonName: PokemonNameType,
         public unlockRequirement?: Requirement
-    ) { }
+    ) {
+        this.pokemon = pokemonMap[pokemonName];
+    }
 
     public isRoaming() {
         return this.unlockRequirement ? this.unlockRequirement.isCompleted() : true;
