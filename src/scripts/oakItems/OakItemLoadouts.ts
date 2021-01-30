0d5ea7192caacd1b00fc7d38e0ef8e188282a994
Kush Patel <kushpatel0703@gmail.com>
2021-01-30 11:41:27 -0600

    Fixed Linter Complaints

--- src/scripts/oakItems/OakItemLoadouts.ts
+++ src/scripts/oakItems/OakItemLoadouts.ts
@@ -1,8 +1,8 @@
 /// <reference path="../../declarations/GameHelper.d.ts" />
 
-class OakItemLoadouts{
+class OakItemLoadouts {
     defaults = {
-        available: 0
+        available: 0,
     }
 
     loadouts: OakItems.OakItem[][];
@@ -19,24 +19,24 @@ class OakItemLoadouts{
     activateLoadout(num: number) {
         App.game.oakItems.deactivateAll();
         this.loadouts[num - 1].forEach((item: OakItems.OakItem) => {
-            App.game.oakItems.activate(item)
+            App.game.oakItems.activate(item);
         });
     }
 
     addItemToLoadout(num: number, item: OakItems.OakItem) {
         if (this.loadouts[num - 1].length < 3) {
-            this.loadouts[num - 1].push(item)
-        } 
+            this.loadouts[num - 1].push(item);
+        }
     }
 
     clearLoadout(num: number) {
-        this.loadouts[num - 1] = []
+        this.loadouts[num - 1] = [];
     }
 
     openLoadoutModal() {
         $('#oakItemLoadoutModal').modal('show');
     }
-    
+
     // Knockout getters/setters
     get available() {
         return this._available();
