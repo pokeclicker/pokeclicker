b6dacfb2d2fb29f84c603526de202d72baf01da3
RedSparr0w <admin@redsparr0w.com>
2021-02-21 22:28:09 +1300

    Add default text for no roamers available yet

--- src/scripts/towns/RoamerNPC.ts
+++ src/scripts/towns/RoamerNPC.ts
@@ -10,6 +10,13 @@ class RoamerNPC extends NPC {
 
     get dialogHTML(): string {
         const route = RoamingPokemonList.getIncreasedChanceRouteByRegion(this.region);
+        const roamers = RoamingPokemonList.getRegionalRoamers(this.region);
+
+        // If no roaming Pokemon yet
+        if (!roamers.length) {
+            return `There hasn't been any reports of roaming Pokémon around ${GameConstants.camelCaseToString(GameConstants.Region[this.region])} lately..`;
+        }
+
         return super.dialogHTML.replace(/{ROUTE_NAME}/g, route()?.routeName);
     }
 }
