c1e97e76f81bca2000c49512dc37128abb5817bb
Jessica Chen <jessica.scoltock@protonmail.com>
2021-02-26 20:24:24 +0000

    Add more type info to global App

--- src/modules/globals.ts
+++ src/modules/globals.ts
@@ -1,7 +1,56 @@
-export {};
+// importing only types, as we are "allowed" to have circular type dependencies
+import type { Observable } from 'knockout';
+import type LogBook from './logbook/LogBook';
+import type BadgeCase from './DataStore/BadgeCase';
+import type Profile from './profile/Profile';
+import type Statistics from './DataStore/StatisticStore';
+import type Challenges from './challenges/Challenges';
+import type Multiplier from './multiplier/Multiplier';
+import type { GameState } from './GameConstants';
 
+// These types are only temporary while we are converting things to modules
+// As things are converted, we should import their types here for use,
+// instead of these cheap imitations.
+
+type TmpGameType = {
+    gameState: Observable<GameState>
+
+    // constructor properties
+    update: any,
+    profile: Profile,
+    breeding: any,
+    pokeballs: any,
+    wallet: any,
+    keyItems: any,
+    badgeCase: BadgeCase,
+    oakItems: any,
+    oakItemLoadouts: any,
+    categories: any,
+    party: any,
+    shards: any,
+    underground: any,
+    farming: any,
+    logbook: LogBook,
+    redeemableCodes: any,
+    statistics: Statistics,
+    quests: any,
+    specialEvents: any,
+    discord: any,
+    achievementTracker: any,
+    challenges: Challenges,
+    multiplier: Multiplier
+
+    // There are functions we could mention too,
+    // but they aren't mentioned in any modules (yet?)
+};
+
+type TmpAppType = {
+    game: TmpGameType
+};
+
+// Where all the magic happens
 declare global {
-    const App: any;
+    const App: TmpAppType;
 
     const player: any;
 
