555d7c5037121eef0ee3d46173714d1d9b156564
RedSparr0w <admin@redsparr0w.com>
2021-04-05 17:33:34 +1200

    Make Sylveon obtainable
    Just evolves using the soothe bell (any time of day)

--- src/scripts/pokemons/PokemonList.ts
+++ src/scripts/pokemons/PokemonList.ts
@@ -3038,6 +3038,7 @@ const pokemonList = createPokemonArray(
             new NightTimedStoneEvolution('Eevee', 'Umbreon', GameConstants.StoneType.Soothe_bell),
             new DungeonRestrictedLevelEvolution('Lake Acuity','Eevee','Glaceon', 20),
             new DungeonRestrictedLevelEvolution('Eterna Forest', 'Eevee', 'Leafeon', 20),
+            new StoneEvolution('Eevee', 'Sylveon', GameConstants.StoneType.Soothe_bell),
         ],
         'base': {
             'hitpoints': 55,
