fae853c88857a588b658f84189eeff7ba54345d7
RedSparr0w <admin@redsparr0w.com>
2021-04-05 14:17:17 +1200

    Add Team Flare Bryony sprite

---
+++ src/modules/Credits.ts
@@ -0,0 +1,19 @@
+// TODO: put in game somewhere
+type Credit = {
+    resources: string[],
+    name: string,
+    link?: string,
+};
+
+const Credits: Credit[] = [
+    {
+        resources: [
+            'Team Flare Bryony',
+        ],
+        name: 'Gnomowladny',
+        link: 'https://www.deviantart.com/gnomowladny',
+        // NOTES: Feel free to use it if you like, but don\'t forget to give me credit~!
+    },
+];
+
+export default Credits;
