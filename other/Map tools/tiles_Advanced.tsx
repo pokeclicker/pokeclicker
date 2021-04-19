49b67a6a5f53139ce7b706ee1e56c6e003e6d78c
Danial <admin@redsparr0w.com>
2021-04-19 17:32:32 +1200

    Add water paths

--- other/Map tools/tiles_Advanced.tsx
+++ other/Map tools/tiles_Advanced.tsx
@@ -1,4 +1,22 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="2020.07.21" name="tiles_Advanced" tilewidth="16" tileheight="16" tilecount="7984" columns="8">
+<tileset version="1.5" tiledversion="1.5.0" name="tiles_Advanced" tilewidth="16" tileheight="16" tilecount="7984" columns="8">
  <image source="tiles_Advanced.png" width="128" height="15971"/>
+ <wangsets>
+  <wangset name="Paths" type="mixed" tile="2042">
+   <wangcolor name="Water Path" color="#0000ff" tile="2042" probability="1"/>
+   <wangtile tileid="2017" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="2018" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="2019" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="2024" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="2025" wangid="1,1,1,1,1,1,1,0"/>
+   <wangtile tileid="2026" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="2027" wangid="1,0,1,1,1,1,1,1"/>
+   <wangtile tileid="2032" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="2033" wangid="1,1,1,1,1,0,1,1"/>
+   <wangtile tileid="2034" wangid="1,1,1,0,1,1,1,1"/>
+   <wangtile tileid="2040" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="2041" wangid="1,0,0,0,0,0,1,1"/>
+   <wangtile tileid="2042" wangid="1,1,1,0,0,0,1,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
