4279b38aaed30b21e567ad4de130bd819bc2ae2e
anapple96 <62083669+anapple96@users.noreply.github.com>
2021-05-06 07:48:02 +0700

    Snow Tree

---
+++ other/Map tools/Snow-Tree.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="Snow-Tree" tilewidth="16" tileheight="16" tilecount="6" columns="2">
+ <image source="snow_tree.png" width="32" height="48"/>
+</tileset>
