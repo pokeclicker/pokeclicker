9eda294f7f52556e7d41c6bf067be0f55e338d5e
Mephistic <deathbyfiresermon@gmail.com>
2021-06-13 13:15:19 -0400

    Revert "Splitting dungeon bosses into separate row of Dungeon Encounter list to make it more clear where the boss list actually begins"
    This reverts commit f1e92b93fd2c4ad03285931e32df3c9be5085399.

--- src/scripts/dungeons/Dungeon.ts
+++ src/scripts/dungeons/Dungeon.ts
@@ -159,12 +159,11 @@ class Dungeon {
         return this.pokemonList.concat(this.bossPokemonList);
     }
 
-
     /**
-     * Gets all non-boss Pokemon encounters in the dungeon
-     * Used for generating the dungeon encounter list view
+     * Finds all possible encounters in the Dungeon and their details.
+     * Used for generating the Dungeon Encounter view
      */
-    get normalEncounterList(): EncounterInfo[] {
+    get encounterList(): EncounterInfo[] {
         const encounterInfo = [];
 
         // Handling minions
@@ -188,17 +187,6 @@ class Dungeon {
             // Handling Trainers
             } else { /* We don't display minion Trainers */ }
         });
-
-        return encounterInfo;
-    }
-
-
-    /**
-     * Gets all boss encounters in the dungeon
-     * Used for generating the dungeon encounter list view
-     */
-    get bossEncounterList(): EncounterInfo[] {
-        const encounterInfo = [];
 
         // Handling Bosses
         this.bossList.forEach((boss) => {
