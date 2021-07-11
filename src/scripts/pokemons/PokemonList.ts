a57fa6c0b21fb925ca23261ce7c632b600248eae
ValorBeastFlame <benjimon1000@gmail.com>
2021-07-11 01:45:06 -0700

    region and type correction for 1 each
    ??? to alola, and galar meowth to steel from nroma.l

--- src/scripts/pokemons/PokemonList.ts
+++ src/scripts/pokemons/PokemonList.ts
@@ -1285,7 +1285,7 @@ const pokemonList = createPokemonArray(
         'name': 'Galarian Meowth',
         'nativeRegion': GameConstants.Region.galar,
         'catchRate': 255,
-        'type': [PokemonType.Normal],
+        'type': [PokemonType.Steel],
         'levelType': LevelType.mediumfast,
         'exp': 58,
         'eggCycles': 20,
@@ -20650,6 +20650,7 @@ const pokemonList = createPokemonArray(
     {
         'id': -793,
         'name': '???',
+        'nativeRegion': GameConstants.Region.alola,
         'type': [PokemonType.Rock, PokemonType.Poison],
         'eggCycles': 120,
         'levelType': LevelType.slow,
