bd2e4b757fe7941aa30b57207f693b9f078644b5
Danial <admin@redsparr0w.com>
2021-08-04 20:30:28 +1200

    Adjust amounts

--- src/scripts/quests/questTypes/GainTokensQuest.ts
+++ src/scripts/quests/questTypes/GainTokensQuest.ts
@@ -8,14 +8,14 @@ class GainTokensQuest extends Quest implements QuestInterface {
     }
 
     public static generateData(): any[] {
-        const multiplier = Math.pow(player.highestRegion() + 1, 2);
+        const multiplier = Math.pow(player.highestRegion() + 1, 3);
         const amount = SeededRand.intBetween(1000 * multiplier, 8000 * multiplier);
         const reward = this.calcReward(amount);
         return [amount, reward];
     }
 
     private static calcReward(amount: number): number {
-        const reward =  Math.ceil(amount * GameConstants.GAIN_TOKENS_BASE_REWARD * Math.pow(Math.max(1, player.highestRegion()), -1.2));
+        const reward =  Math.ceil((amount * GameConstants.GAIN_TOKENS_BASE_REWARD) / Math.pow(player.highestRegion() + 1, 2.6));
         return super.randomizeReward(reward);
     }
 
