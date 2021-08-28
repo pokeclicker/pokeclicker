9c5080e33de56270b19d6d235b873c139bacfaee
Danial <admin@redsparr0w.com>
2021-08-29 02:18:43 +1200

    Calculate reward weight based on how many of each type appear on routes

--- src/scripts/quests/questTypes/CapturePokemonTypesQuest.ts
+++ src/scripts/quests/questTypes/CapturePokemonTypesQuest.ts
@@ -1,43 +1,40 @@
 /// <reference path="../Quest.ts" />
 
 class CapturePokemonTypesQuest extends Quest implements QuestInterface {
+    public static maxWeight = 4;
 
     constructor(capturesNeeded: number, reward: number, public type: PokemonType) {
         super(capturesNeeded, reward);
         this.focus = ko.pureComputed(() => pokemonMap.filter(p => p.type.includes(this.type)).map(p => App.game.statistics.pokemonCaptured[p.id]()).reduce((a,b) => a + b, 0));
     }
 
-    public static rewardWeight: Record<PokemonType, number> = {
-        [PokemonType.None]:     0,
-        [PokemonType.Normal]:   1.4,
-        [PokemonType.Fire]:     2,
-        [PokemonType.Water]:    1.4,
-        [PokemonType.Electric]: 2,
-        [PokemonType.Grass]:    2,
-        [PokemonType.Ice]:      4,
-        [PokemonType.Fighting]: 1.4,
-        [PokemonType.Poison]:   1.4,
-        [PokemonType.Ground]:   2,
-        [PokemonType.Flying]:   1.4,
-        [PokemonType.Psychic]:  4,
-        [PokemonType.Bug]:      3,
-        [PokemonType.Rock]:     2,
-        [PokemonType.Ghost]:    4,
-        [PokemonType.Dragon]:   5,
-        [PokemonType.Dark]:     3,
-        [PokemonType.Steel]:    3,
-        [PokemonType.Fairy]:    5,
+    public static typeWeight(type: PokemonType): number {
+        const types = new Array(GameHelper.enumLength(PokemonType) - 1).fill(0);
+        Routes.regionRoutes.filter(r => r.isUnlocked()).forEach(r => {
+            Object.values(r.pokemon).flat().forEach(p => {
+                const pokemon = pokemonMap[p];
+                if (!pokemon || pokemon.id <= 0) {
+                    return;
+                }
+                pokemon.type.forEach(t => types[t]++);
+            });
+        });
+        const max = Math.max(...types);
+        return types.map(v => (-v + max) / max * this.maxWeight).map((v, i) => Math.max(1.2, Math.round(v * 100) / 100))[type];
     }
 
     public static generateData(): any[] {
         const amount = SeededRand.intBetween(50, 250);
-        const type = SeededRand.fromEnum(PokemonType);
+        let type = SeededRand.fromEnum(PokemonType);
+        while (this.typeWeight(type) >= this.maxWeight) {
+            type = SeededRand.fromEnum(PokemonType);
+        }
         const reward = this.calcReward(amount, type);
         return [amount, reward, type];
     }
 
     private static calcReward(amount: number, type: PokemonType): number {
-        const reward = amount * GameConstants.CAPTURE_POKEMONS_BASE_REWARD * this.rewardWeight[type];
+        const reward = amount * GameConstants.CAPTURE_POKEMONS_BASE_REWARD * this.typeWeight(type);
         return super.randomizeReward(reward);
     }
 
--- src/scripts/quests/questTypes/GainShardsQuest.ts
+++ src/scripts/quests/questTypes/GainShardsQuest.ts
@@ -1,6 +1,7 @@
 /// <reference path="../Quest.ts" />
 
 class GainShardsQuest extends Quest implements QuestInterface {
+    public static maxWeight = 4;
 
     private type: PokemonType;
 
@@ -10,37 +11,33 @@ class GainShardsQuest extends Quest implements QuestInterface {
         this.focus = App.game.statistics.shardsGained[this.type];
     }
 
-    public static rewardWeight: Record<PokemonType, number> = {
-        [PokemonType.None]:     0,
-        [PokemonType.Normal]:   1,
-        [PokemonType.Fire]:     2,
-        [PokemonType.Water]:    1,
-        [PokemonType.Electric]: 2,
-        [PokemonType.Grass]:    2,
-        [PokemonType.Ice]:      4,
-        [PokemonType.Fighting]: 1,
-        [PokemonType.Poison]:   1,
-        [PokemonType.Ground]:   2,
-        [PokemonType.Flying]:   1,
-        [PokemonType.Psychic]:  4,
-        [PokemonType.Bug]:      3,
-        [PokemonType.Rock]:     2,
-        [PokemonType.Ghost]:    4,
-        [PokemonType.Dragon]:   5,
-        [PokemonType.Dark]:     3,
-        [PokemonType.Steel]:    3,
-        [PokemonType.Fairy]:    5,
+    public static typeWeight(type: PokemonType): number {
+        const types = new Array(GameHelper.enumLength(PokemonType) - 1).fill(0);
+        Routes.regionRoutes.filter(r => r.isUnlocked()).forEach(r => {
+            Object.values(r.pokemon).flat().forEach(p => {
+                const pokemon = pokemonMap[p];
+                if (!pokemon || pokemon.id <= 0) {
+                    return;
+                }
+                pokemon.type.forEach(t => types[t]++);
+            });
+        });
+        const max = Math.max(...types);
+        return types.map(v => (-v + max) / max * this.maxWeight).map((v, i) => Math.max(1.2, Math.round(v * 100) / 100))[type];
     }
 
     public static generateData(): any[] {
-        const type = SeededRand.fromEnum(PokemonType);
         const amount = SeededRand.intBetween(200, 600);
+        let type = SeededRand.fromEnum(PokemonType);
+        while (this.typeWeight(type) >= this.maxWeight) {
+            type = SeededRand.fromEnum(PokemonType);
+        }
         const reward = this.calcReward(type, amount);
         return [amount, reward, type];
     }
 
     private static calcReward(type: PokemonType, amount: number): number {
-        const reward = amount * GameConstants.DEFEAT_POKEMONS_BASE_REWARD * this.rewardWeight[type];
+        const reward = amount * GameConstants.DEFEAT_POKEMONS_BASE_REWARD * this.typeWeight(type);
         return super.randomizeReward(reward);
     }
 
