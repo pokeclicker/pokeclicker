64130cdee6315c0192102222fbbda8c0bd4bfd50
Danial <admin@redsparr0w.com>
2021-08-29 12:40:24 +1200

    Calculate the weights once then select a possible type

--- src/scripts/quests/questTypes/GainShardsQuest.ts
+++ src/scripts/quests/questTypes/GainShardsQuest.ts
@@ -3,6 +3,7 @@
 class GainShardsQuest extends Quest implements QuestInterface {
     public static maxWeight = 4;
     public static minWeight = 1.2;
+    public static weights: Array<Record<string, number>> = [];
 
     private type: PokemonType;
 
@@ -12,7 +13,7 @@ class GainShardsQuest extends Quest implements QuestInterface {
         this.focus = App.game.statistics.shardsGained[this.type];
     }
 
-    public static typeWeight(type: PokemonType): number {
+    public static typeWeights(): Array<Record<string, number>> {
         const types = new Array(GameHelper.enumLength(PokemonType) - 1).fill(0);
         Routes.regionRoutes.filter(r => r.isUnlocked()).forEach(r => {
             Object.values(r.pokemon).flat().forEach(p => {
@@ -24,22 +25,22 @@ class GainShardsQuest extends Quest implements QuestInterface {
             });
         });
         const max = Math.max(...types);
-        return types.map(v => ((-v + max) / max) * (this.maxWeight - this.minWeight)).map(v => Math.round((v + this.minWeight) * 100) / 100)[type];
+        // Calculate the weight
+        return types.map(v => ((-v + max) / max) * (this.maxWeight - this.minWeight))
+            // map the type and rounded values
+            .map((weight, type) => ({type, weight: Math.round((weight + this.minWeight) * 100) / 100}));
     }
 
     public static generateData(): any[] {
         const amount = SeededRand.intBetween(200, 600);
-        let type = SeededRand.fromEnum(PokemonType);
-        // Don't allow type quest for types we can't encounter
-        while (this.typeWeight(type) >= this.maxWeight) {
-            type = SeededRand.fromEnum(PokemonType);
-        }
+        this.weights = this.typeWeights();
+        const type = SeededRand.fromArray(this.weights.filter(w => w.weight < this.maxWeight).map(w => w.type));
         const reward = this.calcReward(type, amount);
         return [amount, reward, type];
     }
 
     private static calcReward(type: PokemonType, amount: number): number {
-        const reward = amount * GameConstants.DEFEAT_POKEMONS_BASE_REWARD * this.typeWeight(type);
+        const reward = amount * GameConstants.DEFEAT_POKEMONS_BASE_REWARD * this.weights[type].weight;
         return super.randomizeReward(reward);
     }
 
