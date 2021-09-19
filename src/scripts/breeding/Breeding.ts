b7b3eeb206438a4528259be9005ae20b58245eb3
Danial <RedSparr0w@users.noreply.github.com>
2021-09-19 13:18:56 +1200

    change(hatchery): Limit to +32 queue slots per new region (#1593)
    * Limit to +32 queue slots per new region
    
    * Add comment

--- src/scripts/breeding/Breeding.ts
+++ src/scripts/breeding/Breeding.ts
@@ -389,7 +389,8 @@ class Breeding implements Feature {
     }
 
     public queueSlotsGainedFromRegion(region: GameConstants.Region): number {
-        return Math.max(4, 4 * Math.pow(2, region - 1));
+        // bewtween 4 → 32 queue slots gained when completing a region
+        return Math.min(32, Math.max(4, 4 * Math.pow(2, region - 1)));
     }
 
     get eggList(): Array<KnockoutObservable<Egg>> {
