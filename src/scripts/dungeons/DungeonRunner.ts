f089f9ee0d649dd092444a968a5e29962f1ce555
jk13pclick <jk13pclick@gmail.com>
2021-09-29 23:07:04 -0700

    Little fix

--- src/scripts/dungeons/DungeonRunner.ts
+++ src/scripts/dungeons/DungeonRunner.ts
@@ -137,25 +137,25 @@ class DungeonRunner {
             return App.game.pokeballs.gainPokeballs(GameConstants.Pokeball[GameConstants.humanifyString(input)],1);
 
         }  else if (Underground.getMineItemByName(input) != undefined && Underground.getMineItemByName(input).isStone() === false) {
-           Notifier.notify({
-               message: `Found ${amount} ${GameConstants.humanifyString(input)} in a dungeon chest`,
-               type: NotificationConstants.NotificationOption.success,
-               setting: NotificationConstants.NotificationSetting.dungeon_item_found,
+            Notifier.notify({
+                message: `Found ${amount} ${GameConstants.humanifyString(input)} in a dungeon chest`,
+                type: NotificationConstants.NotificationOption.success,
+                setting: NotificationConstants.NotificationSetting.dungeon_item_found,
             });
 
             return Underground.gainMineItem(Underground.getMineItemByName(input).id, amount);
 
         }  else if (ItemList[input].constructor.name == 'EvolutionStone' || 'EggItem' || 'BattleItem' || 'Vitamin' || 'EnergyRestore') {
             Notifier.notify({
-               message: `Found ${amount} ${GameConstants.humanifyString(input)} in a dungeon chest`,
+                message: `Found ${amount} ${GameConstants.humanifyString(input)} in a dungeon chest`,
                 type: NotificationConstants.NotificationOption.success,
-               setting: NotificationConstants.NotificationSetting.dungeon_item_found,
-           });
+                setting: NotificationConstants.NotificationSetting.dungeon_item_found,
+            });
 
-           return player.gainItem(ItemList[input].name, amount);
+            return player.gainItem(ItemList[input].name, amount);
 
         }  else {
-            return player.gainItem(ItemList["xAttack"], 1);
+            return player.gainItem(ItemList['xAttack'], 1);
         }
     }
 
