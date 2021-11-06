71ac2113d7398efa223df4c8a37f97f84834b58d
amative1 <14666630+amative1@users.noreply.github.com>
2021-11-06 01:59:46 -0400

    change(drops): Update slowpoke item drops (#1620)
    * Update PokemonList.ts
    
    Add King's Rock to Slowpoke drops
    
    * Update PokemonList.ts
    
    Co-authored-by: Danial <RedSparr0w@users.noreply.github.com>

--- src/scripts/pokemons/PokemonList.ts
+++ src/scripts/pokemons/PokemonList.ts
@@ -1893,6 +1893,7 @@ const pokemonList = createPokemonArray(
             'specialDefense': 40,
             'speed': 15,
         },
+        'heldItem': {type: ItemType.item, id: 'Kings_rock'},
     },
     {
         'id': 79.1,
@@ -1911,7 +1912,6 @@ const pokemonList = createPokemonArray(
             'specialDefense': 40,
             'speed': 15,
         },
-        'heldItem': {type: ItemType.item, id: 'Kings_rock'},
     },
     {
         'id': 80,
