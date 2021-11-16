0cda90efd17397d224a6212558a313bc05ef3452
Danial <admin@redsparr0w.com>
2021-11-16 18:32:40 +1300

    Minor changes
    Flattened the gyms first before processing

--- src/scripts/badgeCase/BadgeCaseController.ts
+++ src/scripts/badgeCase/BadgeCaseController.ts
@@ -3,6 +3,8 @@
 class BadgeCaseController {
     static getDisplayableBadges() {
         const region = player.highestRegion();
-        return GameConstants.RegionGyms.slice(0, region + 1).map(regionGyms => regionGyms.map(gym => BadgeEnums[gymList[gym].badgeReward]).filter(b => !b.startsWith('Elite') && b != 'None')).flat();
+        return GameConstants.RegionGyms.slice(0, region + 1).flat()
+            .map(gym => BadgeEnums[gymList[gym].badgeReward])
+            .filter(b => !b.startsWith('Elite') && b != 'None');
     }
 }
