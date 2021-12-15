d40d3669793fe489cc258943cb02f2ad066c8510
Danial <admin@redsparr0w.com>
2021-12-16 02:29:31 +1300

    More fixup

--- src/scripts/farming/BerryDeal.ts
+++ src/scripts/farming/BerryDeal.ts
@@ -13,7 +13,7 @@ class BerryDeal {
     }
 
     private static randomBerry(berryList: BerryType[]): BerryType {
-        return berryList[Math.floor(berryList.length * SeededRand.next())];
+        return Rand.fromArray(berryList);
     }
 
     private static randomBattleItem(): Item {
