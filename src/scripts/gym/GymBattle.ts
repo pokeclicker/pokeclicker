dbb678d5e831341671028767072e059319c6b48c
Danial <admin@redsparr0w.com>
2022-02-11 18:38:57 +1300

    remove double space

--- src/scripts/gym/GymBattle.ts
+++ src/scripts/gym/GymBattle.ts
@@ -22,7 +22,6 @@ class GymBattle extends Battle {
     public static defeatPokemon() {
         this.enemyPokemon().defeat(true);
 
-
         // Make gym "route" regionless
         App.game.breeding.progressEggsBattle(this.gym.badgeReward * 3 + 1, GameConstants.Region.none);
         this.index(this.index() + 1);
