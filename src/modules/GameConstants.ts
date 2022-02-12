5ff24dc49159645bbe496bba7e9776ee5362a560
Ultima1990 <69112975+Ultima1990@users.noreply.github.com>
2022-02-12 01:48:14 +0100

    change(alola): Fixup several sprites and names in Alola (#1684)
    * Fixup several sprites and names in Alola
    
    * Kalos Victory Road Black Belt
    
    Noticed this guy having a broken sprite so I just added his fix here too.
    
    * Nebby/The Bag overhaul, Lake of the Sunne and Moone
    
    * New background for Lake of the Sunne and Moone
    
    * Fix Mina's Trial Requirement
    
    * Fix fix
    
    * Update GameConstants.ts
    
    Co-authored-by: Danial <RedSparr0w@users.noreply.github.com>

--- src/modules/GameConstants.ts
+++ src/modules/GameConstants.ts
@@ -486,7 +486,7 @@ export const Environments: Record<string, EnvironmentData> = {
         [Region.sinnoh]: new Set([218, 219, 220, 223, 230, 'Lake Verity', 'Lake Valor', 'Pastoria City']),
         [Region.unova]: new Set([17, 18, 21, 24, 'Undella Town', 'Humilau City', 'Plasma Frigate']),
         [Region.kalos]: new Set([8, 'Coumarine City', 'Couriway Town', 'Sea Spirit\'s Den']),
-        [Region.alola]: new Set([15, 19, 20, 'Seafolk Village', 'Brooklet Hill']),
+        [Region.alola]: new Set([15, 19, 20, 'Seafolk Village', 'Brooklet Hill', 'Lake of the Sunne and Moone']),
         [Region.galar]: new Set(['Hulbury', 5, 6, 8, 9, 19, 21]),
     },
 
@@ -530,7 +530,7 @@ export const Environments: Record<string, EnvironmentData> = {
         [Region.sinnoh]: new Set(['Spear Pillar', 'Hall of Origin', 'Stark Mountain']),
         [Region.unova]: new Set(['Chargestone Cave', 'Mistralton Cave', 'Cave of Being']),
         [Region.kalos]: new Set(['Glittering Cave', 'Reflection Cave']),
-        [Region.alola]: new Set(['Altar of the Sunne and Moone', 'Nebby', 'Resolution Cave']),
+        [Region.alola]: new Set(['Altar of the Sunne and Moone', 'Resolution Cave']),
         [Region.galar]: new Set(['Galar Mine', 'Galar Mine No. 2']),
     },
 
@@ -847,7 +847,7 @@ export const AlolaGyms = [
     'Elite Olivia',
     'Elite Acerola',
     'Elite Kahili',
-    'Champion Hao',
+    'Champion Hau',
 ];
 
 export const GalarGyms = [
@@ -1057,7 +1057,7 @@ export const AlolaDungeons = [
     'Ruins of Abundance',
     'Aether Foundation',
     'Vast Poni Canyon',
-    'Nebby',
+    'Lake of the Sunne and Moone',
     'Ruins of Hope',
     'Poni Meadow',
     'Resolution Cave',
