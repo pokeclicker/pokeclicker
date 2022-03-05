6af71247514d45c1245814e7295b134930883dd9
jk13pclick <jk13pclick@gmail.com>
2022-03-04 20:30:20 -0800

    Gem case
    This is 90% done, the only thing that doesn't work is transferring the current shard totals

--- src/scripts/Update.ts
+++ src/scripts/Update.ts
@@ -592,6 +592,21 @@ class Update implements Saveable {
             if (saveData.badgeCase[58]) {
                 saveData.quests.questLines.push({state: 1, name: 'Quest for the DNA Splicers', quest: 0});
             }
+
+            // Shards -> Gems
+            delete saveData.quests.questList;
+
+            //TODO (or not): make these functions, or an equivalent, work. Right now, this update just zeros every shard.
+
+            //saveData.gems.gemWallet = saveData.shards.shardWallet;
+            //saveData.gems.gemCollapsed = saveData.shards.shardCollapsed;
+            //saveData.gems.gemUpgrades: saveData.shards.shardUpgrades;
+
+            if (saveData.keyItems['Shard_case'] = true) {
+                saveData.keyItems['Gem_case'] = true;
+                delete saveData.keyItems['Shard_case'];
+            }
+
             // Just incase statistics is not set
             saveData.statistics = saveData.statistics || {};
 
@@ -602,15 +617,6 @@ class Update implements Saveable {
                 gemsGained: saveData.statistics.shardsGained || 0,
             };
 
-            const badgeToKeyItems = {
-                [8]: 'Gem_case', //Earth badge
-            };
-            Object.keys(badgeToKeyItems).forEach(badge => {
-                const keyItem = badgeToKeyItems[badge];
-                if (saveData.badgeCase[badge]) {
-                    saveData.keyItems[keyItem] = true;
-                }
-            });
         },
     };
 
