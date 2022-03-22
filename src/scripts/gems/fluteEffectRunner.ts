9cd0bf77c859ae9f59ac89752c90b7dbc579234a
jk13pclick <jk13pclick@gmail.com>
2022-03-21 18:30:45 -0700

    Heat badge + fixup

--- src/scripts/gems/fluteEffectRunner.ts
+++ src/scripts/gems/fluteEffectRunner.ts
@@ -29,8 +29,8 @@ class fluteEffectRunner {
                     Notifier.notify({
                         message: `The ${GameConstants.humanifyString(itemName)}'s effect ran out!`,
                         type: NotificationConstants.NotificationOption.danger,
-                        sound: NotificationConstants.NotificationSound.battle_item_timer,
-                        setting: NotificationConstants.NotificationSetting.battle_item_timer,
+                        sound: NotificationConstants.NotificationSound.Items.battle_item_timer,
+                        setting: NotificationConstants.NotificationSetting.Items.battle_item_timer,
                     });
                 }
             }
