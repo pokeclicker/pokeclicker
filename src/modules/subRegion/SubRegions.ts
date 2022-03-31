48665132092c38c48a064da6a297daa55f2ca317
Danial <RedSparr0w@users.noreply.github.com>
2022-03-31 15:42:24 +1300

    change(alola): Split Melemele and Akala islands (#1929)

--- src/modules/subRegion/SubRegions.ts
+++ src/modules/subRegion/SubRegions.ts
@@ -43,8 +43,8 @@ SubRegions.addSubRegion(Region.hoenn, new SubRegion('Hoenn'));
 SubRegions.addSubRegion(Region.sinnoh, new SubRegion('Sinnoh'));
 SubRegions.addSubRegion(Region.unova, new SubRegion('Unova'));
 SubRegions.addSubRegion(Region.kalos, new SubRegion('Kalos'));
-SubRegions.addSubRegion(Region.alola, new SubRegion('Melemele Island', undefined, 'Hau\'oli City'));
-SubRegions.addSubRegion(Region.alola, new SubRegion('Melemele & Akala islands', new GymBadgeRequirement(BadgeEnums.FightiniumZ), 'Heahea City'));
+SubRegions.addSubRegion(Region.alola, new SubRegion('Melemele island', undefined, 'Hau\'oli City'));
+SubRegions.addSubRegion(Region.alola, new SubRegion('Akala island', new GymBadgeRequirement(BadgeEnums.FightiniumZ), 'Heahea City'));
 SubRegions.addSubRegion(Region.alola, new SubRegion('Ula\'ula & Poni islands', new GymBadgeRequirement(BadgeEnums.Elite_Nihilego), 'Malie City'));
 // For when Alola is split into 4 regions
 // SubRegions.addSubRegion(Region.alola, new SubRegion('Akala island', new GymBadgeRequirement(BadgeEnums.MelemeleKahuna), 'Heahea City'));
