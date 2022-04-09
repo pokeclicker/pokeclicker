ffd9ec29d40d1b50ba5313b7652abfc0746d93e6
JapuDCret <JapuDCret@users.noreply.github.com>
2022-04-09 09:12:34 +0200

    change(settings): Add option to disable Gym GO animation (#1967)
    * add option to disable gym countdown #1778
    
    * Update value names
    
    * fixup eslint :sweat:
    
    Co-authored-by: Danial <RedSparr0w@users.noreply.github.com>
    Co-authored-by: Danial <admin@redsparr0w.com>

--- src/modules/settings/index.ts
+++ src/modules/settings/index.ts
@@ -89,6 +89,7 @@ Settings.add(new Setting<string>('farmDisplay', 'Farm timer display:',
     ],
     'nextStage'));
 Settings.add(new BooleanSetting('currencyMainDisplayReduced', 'Shorten currency amount shown on main screen', false));
+Settings.add(new BooleanSetting('showGymGoAnimation', 'Show Gym GO animation', true));
 
 // CSS variable settings
 Settings.add(new CssVariableSetting('locked', 'Locked Location', [], '#000000'));
