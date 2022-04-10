b0ad059a1a839e72be6a8af9a7f1cff827a0aa5b
jaaslet <jonas_bms@hotmail.com>
2022-04-10 15:51:33 +0200

    Improved some messages

--- src/scripts/battleFrontier/BattleFrontierRunner.ts
+++ src/scripts/battleFrontier/BattleFrontierRunner.ts
@@ -64,6 +64,7 @@ class BattleFrontierRunner {
         Notifier.notify({
             title: 'Battle Frontier',
             message: `You managed to beat stage ${stageBeaten}.<br/>You received ${battlePointsEarned} BP`,
+            htmlLessMessage: `You managed to beat stage ${stageBeaten}.\nYou received ${battlePointsEarned} BP`,
             type: NotificationConstants.NotificationOption.success,
             setting: NotificationConstants.NotificationSetting.General.battle_frontier,
             timeout: 5 * GameConstants.MINUTE,
