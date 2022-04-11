aa66b63c76ea3559172f45db77d462b7709a43e2
jaaslet <jonas_bms@hotmail.com>
2022-04-11 20:55:09 +0200

    Removed default value: should always be false

--- src/modules/settings/NotificationSetting.ts
+++ src/modules/settings/NotificationSetting.ts
@@ -7,14 +7,14 @@ export default class NotificationSetting {
     name: string;
     displayName: string;
 
-    constructor(name: string, displayName: string, defaultValueInGame: boolean, defaultValueDesktop: boolean, warnOnBlocked: () => void, lockInGame: boolean = false) {
+    constructor(name: string, displayName: string, defaultValueInGame: boolean, warnOnBlocked: () => void, lockInGame: boolean = false) {
         this.warnOnBlocked = warnOnBlocked;
         this.name = name;
         this.displayName = displayName;
         if (!lockInGame) {
             this.inGameNotification = new BooleanSetting(name, displayName, defaultValueInGame ?? false);
         }
-        this.desktopNotification = new BooleanSetting(`${name}.desktop`, displayName, defaultValueDesktop ?? false);
+        this.desktopNotification = new BooleanSetting(`${name}.desktop`, displayName, false);
         this.desktopNotification.observableValue.subscribe((changedTo) => {
             if (changedTo) {
                 if (Notification.permission !== 'granted' && Notification.permission !== 'denied') {
