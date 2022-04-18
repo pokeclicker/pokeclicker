0841fbfe7543a29fd812c1d08cf4e48132e77dac
jaaslet <jonas_bms@hotmail.com>
2022-04-18 19:46:20 +0200

    Fixed hotkeys

--- src/scripts/GameController.ts
+++ src/scripts/GameController.ts
@@ -269,24 +269,21 @@ class GameController {
                 // Within towns
                 if (App.game.gameState === GameConstants.GameState.town) {
                     if (key === Settings.getSetting('hotkey.town.start').value) {
+                        if (player.town() instanceof DungeonTown) {
+                            DungeonRunner.initializeDungeon(player.town().dungeon);
+                        } else {
+                            player.town().content[0].onclick();
+                        }
                         if (player.town().gym) {
                             GymRunner.startGym(player.town().gym);
-                        } else if (player.town().dungeon) {
-                            if (player.town() instanceof DungeonTown) {
-                                DungeonRunner.initializeDungeon(player.town().dungeon);
-                            } else {
-                                MapHelper.moveToTown(player.town().dungeon.name);
-                            }
                         }
                         return e.preventDefault();
-                    } else if ('gymList' in player.town()) {
-                        if (isNumberKey) {
-                            // Check if a number higher than 0 and less than total Gyms was pressed
-                            if (numberKey < player.town().gymList.length) {
-                                GymRunner.startGym(player.town().gymList[numberKey]);
-                            }
-                            return e.preventDefault();
+                    } else if (isNumberKey) {
+                        // Check if a number higher than 0 and less than total Gyms was pressed
+                        if (numberKey < player.town().content.length) {
+                            player.town().content[numberKey].onclick();
                         }
+                        return e.preventDefault();
                     }
                 }
             }
