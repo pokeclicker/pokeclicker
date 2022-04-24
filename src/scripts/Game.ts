e9077eb51f6561b1ea53f0a2a7a9ccc7f0e7956e
jk13pclick <jk13pclick@gmail.com>
2022-04-23 18:20:16 -0700

    feature(flutes): Add Flutes (#1791)
    * Initial Flute addition
    
    * Creation of the ShardTrade Modal
    
    Committing this so I can mess with the Tab Visibility
    
    * Better modal
    
    * Big Flute dump, still needs way more
    
    * weird duplication fix
    
    * Working Timers
    
    * Better ShardCost, implemented effects + costs
    
    * On/Off + the rest of the flutes
    
    No effect for Grass/Time yet, horrendous images
    
    * Another TODO
    
    * Remove the bad button
    
    * Important TODO down
    
    * Temp Flute images
    
    * Moon Flute and Grass Flute Effects
    
    Still need Time Flute
    
    * Time Flute + new Images + Undella Shop
    
    * Test
    
    * Revert "Test"
    
    This reverts commit 9606b18912ce5bfc5a13a41107dd12abb3fba381.
    
    * Shards used text
    
    * Update battleItemContainer.html
    
    * Broken flute check fix, maybe shard fix?
    
    * Flute deactivation bugfix
    
    When shards ran out the flute deactivation was weird, this fixes it
    
    * Fixing the fix
    
    * Little notification update
    
    * Update effectEngineRunner.ts
    
    * Shard Challenge Check
    
    * Type Damage Boost
    
    Potentially controversial, this may make some of the more "niche" flutes better. If a flute that requires a certain type is active, pokémon of that type get a 0.5% boost in damage
    
    * Github Pages (revert when merged?)
    
    * Fix
    
    * Type Array, stop lagging
    
    Types were lagging the game, this should fix it
    
    * Merge branch 'develop' into pr/1244
    
    * Update src/modules/GameHelper.ts
    
    Co-authored-by: Jessica Chen <jessica.scoltock@protonmail.com>
    
    * Update GameHelper.ts
    
    * Code Review fixes #1
    
    * Code Review fixes Part 2
    
    Should be the rest
    
    * Code Review fixup 3
    
    * Gem Update
    
    Fixes a whole bunch of stuff, still ready for review
    
    * Heat badge + fixup
    
    * Update fluteEffectRunner.ts
    
    * NPC
    
    Where to get the first 6 flutes
    
    * Update Town.ts
    
    * Better tooltip
    
    A bit large, but way more helpful
    
    * Update src/scripts/gems/fluteEffectRunner.ts
    
    Formatted time
    
    Co-authored-by: Jessica Chen <jessica.scoltock@protonmail.com>
    
    * Tooltip fixup, battle item challenge notif
    
    * Update fluteEffectRunner.ts
    
    * Update battleItemContainer.html
    
    * Move tooltip HTML to fluteEffectRunner
    
    * Update fluteEffectRunner.ts
    
    * Update Farming.ts
    
    * Update fluteEffectRunner.ts
    
    * Duplicate image, cleanup
    
    * Tinify pngs
    
    * docs
    
    * Pages
    
    * Pages
    
    * Fixups, tooltips, etc
    
    * Pages
    
    * Update FluteItem.ts
    
    * Update script.min.js
    
    * Show "sold out" after trade
    
    * Added nicer way of showing owned flutes
    
    * Achievement tooltip now mentions flutes
    
    * More clear, when you don't own a flute
    
    * Now showing the multiplier in the description
    
    * Gemmaster is now ready for non-flute trades again
    
    * Code cleanup: Removed Flute from gemmaster
    
    * Keep consistent with other Battle Items
    
    * Update FluteItem.ts
    
    * Pages
    
    * Update TownList.ts
    
    * pages
    
    * Little fixes + pages
    
    * Pages?
    
    * Price nerf
    
    * GemMasterShop Fix
    
    * Update GemMasterShop.ts
    
    * Pages revert
    
    * fluteEffectRunner → FluteEffectRunner
    
    * remove _
    
    * Remove our static values that never change
    
    * Minify images, remove docs
    
    * Flutes runs out of Gems fix
    
    Fixed, hopefully
    
    * minor change
    
    these should both be the same already
    
    * Minor changes, enforce typing
    
    * fixup linting
    
    Co-authored-by: Jessica Chen <jessica.scoltock@protonmail.com>
    Co-authored-by: jaaslet <jonas_bms@hotmail.com>
    Co-authored-by: Danial <RedSparr0w@users.noreply.github.com>
    Co-authored-by: Danial <admin@redsparr0w.com>

--- src/scripts/Game.ts
+++ src/scripts/Game.ts
@@ -66,6 +66,7 @@ class Game {
         AchievementHandler.initialize(this.multiplier, this.challenges);
         FarmController.initialize();
         EffectEngineRunner.initialize(this.multiplier);
+        FluteEffectRunner.initialize(this.multiplier);
         ItemHandler.initilizeEvoStones();
         this.profile.initialize();
         this.breeding.initialize();
@@ -94,6 +95,7 @@ class Game {
         DailyDeal.generateDeals(this.underground.getDailyDealsMax(), now);
         BerryDeal.generateDeals(now);
         Weather.generateWeather(now);
+        GemDeal.generateDeals();
         RoamingPokemonList.generateIncreasedChanceRoutes(now);
 
         this.computeOfflineEarnings();
@@ -352,10 +354,14 @@ class Game {
         // Farm
         this.farming.update(GameConstants.TICK_TIME / GameConstants.SECOND);
 
-        // Effect Engine (battle items)
+        // Effect Engine (battle items and flutes)
         EffectEngineRunner.counter += GameConstants.TICK_TIME;
         if (EffectEngineRunner.counter >= GameConstants.EFFECT_ENGINE_TICK) {
             EffectEngineRunner.tick();
+        }
+        FluteEffectRunner.counter += GameConstants.TICK_TIME;
+        if (FluteEffectRunner.counter >= GameConstants.EFFECT_ENGINE_TICK) {
+            FluteEffectRunner.tick();
         }
 
         // Game timers
