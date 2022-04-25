926c43fcb3331346fb95ecda57ccc95609ae956c
jaaslet <jonas_bms@hotmail.com>
2022-04-25 17:57:15 +0200

    Missing rename

--- src/modules/subRegion/SubRegions.ts
+++ src/modules/subRegion/SubRegions.ts
@@ -46,10 +46,10 @@ SubRegions.addSubRegion(Region.unova, new SubRegion('Unova'));
 SubRegions.addSubRegion(Region.kalos, new SubRegion('Kalos'));
 SubRegions.addSubRegion(Region.alola, new SubRegion('Melemele island', undefined, 'Hau\'oli City'));
 SubRegions.addSubRegion(Region.alola, new SubRegion('Akala island', new GymBadgeRequirement(BadgeEnums.FightiniumZ), 'Heahea City'));
-SubRegions.addSubRegion(Region.alola, new SubRegion('Ula\'ula & Poni islands', new TemporaryBattleRequirement('UltraWormhole'), 'Malie City'));
+SubRegions.addSubRegion(Region.alola, new SubRegion('Ula\'ula & Poni islands', new TemporaryBattleRequirement('Ultra Wormhole'), 'Malie City'));
 // For when Alola is split into 4 regions
 // SubRegions.addSubRegion(Region.alola, new SubRegion('Akala island', new GymBadgeRequirement(BadgeEnums.MelemeleKahuna), 'Heahea City'));
-// SubRegions.addSubRegion(Region.alola, new SubRegion('Ula\'ula island', new TemporaryBattleRequirement('UltraWormhole'), 'Malie City'));
+// SubRegions.addSubRegion(Region.alola, new SubRegion('Ula\'ula island', new TemporaryBattleRequirement('Ultra Wormhole'), 'Malie City'));
 // SubRegions.addSubRegion(Region.alola, new SubRegion('Poni island', new ClearDungeonRequirement(1, GameConstants.getDungeonIndex('Aether Foundation')), 'Seafolk Village'));
 
 export enum AlolaSubRegions {
