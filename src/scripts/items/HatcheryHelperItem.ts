52b44ac32cbb01b564b45344b8bb0e575d0ab73f
Danial <RedSparr0w@users.noreply.github.com>
2022-04-27 14:32:41 +1200

    change(hatchery-helpers): Hatchery Helpers bonus (#2085)
    * Increase Efficiency based on number of hatches
    
    * Add new helper

--- src/scripts/items/HatcheryHelperItem.ts
+++ src/scripts/items/HatcheryHelperItem.ts
@@ -13,8 +13,8 @@ class HatcheryHelperItem extends Item {
     get description(): string {
         const hatcheryHelper = this.hatcheryHelper;
         return `Cost: <img src="assets/images/currency/${GameConstants.Currency[hatcheryHelper?.cost?.currency]}.svg" width="20px">&nbsp;${(hatcheryHelper?.cost?.amount ?? 0).toLocaleString('en-US')}/hatch<br/>
-        Step Efficiency: ${(hatcheryHelper?.stepEfficiency ?? 0).toLocaleString('en-US')}%<br/>
-        Attack Efficiency: ${(hatcheryHelper?.attackEfficiency ?? 0).toLocaleString('en-US')}%`;
+        Step Efficiency: ${(hatcheryHelper?.stepEfficiencyBase ?? 0).toLocaleString('en-US')}%<br/>
+        Attack Efficiency: ${(hatcheryHelper?.attackEfficiencyBase ?? 0).toLocaleString('en-US')}%`;
     }
 
     isAvailable(): boolean {
@@ -29,6 +29,7 @@ class HatcheryHelperItem extends Item {
 }
 
 // Berry Masters
+ItemList['HatcheryHelperJasmine'] = new HatcheryHelperItem('Jasmine', 10000000, GameConstants.Currency.money);
 ItemList['HatcheryHelperDakota'] = new HatcheryHelperItem('Dakota', 100000, GameConstants.Currency.dungeonToken);
 ItemList['HatcheryHelperCarey']  = new HatcheryHelperItem('Carey', 10000, GameConstants.Currency.questPoint);
 ItemList['HatcheryHelperKris']   = new HatcheryHelperItem('Kris', 2000, GameConstants.Currency.diamond);
