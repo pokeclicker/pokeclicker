558fe6c14af3d91981ef745958021629889deb14
Ultima <69112975+Ultima1990@users.noreply.github.com>
2022-05-15 16:21:30 +0200

    Alola
    Massive nerfs across the board. Not so good for veterans, but hopefully a better difficulty curve for new players. Shallowest Pokémon League curve too, since I had to not only fit the entire league between two routes, but Hapu was squeezed in there too. No routes between her grand trial and the league.

--- src/scripts/temporaryBattle/TemporaryBattleList.ts
+++ src/scripts/temporaryBattle/TemporaryBattleList.ts
@@ -2,7 +2,7 @@ const TemporaryBattleList: { [battleName: string]: TemporaryBattle } = {};
 
 TemporaryBattleList['Ultra Wormhole'] = new TemporaryBattle(
     'Ultra Wormhole',
-    [new GymPokemon('???', 264590972, 27)],
+    [new GymPokemon('???', 134834760, 27)],
     'The creature escaped back into the ultra wormhole.',
     [new GymBadgeRequirement(BadgeEnums.RockiumZ)]
 );
