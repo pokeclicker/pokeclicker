223c3eb9945dfef7a9cd8cd97e2490688f144651
jaaslet <jonas_bms@hotmail.com>
2022-05-22 16:28:14 +0200

    Code cleanup

--- src/scripts/towns/ProfNPC.ts
+++ src/scripts/towns/ProfNPC.ts
@@ -9,70 +9,85 @@ class ProfNPC extends NPC {
     }
 
     get dialogHTML(): string {
-        const uniquePokemonCaught = new Set(App.game.party.caughtPokemon.filter(p => p.id > 0).map(p => Math.floor(p.id))).size;
+        const requiresCompleteDex = App.game.challenges.list.requireCompletePokedex.active();
+        const nextRegionUnlocked = TownList[GameConstants.StartingTowns[this.region + 1]]?.isUnlocked() ?? false;
+        const completeDexAchievement = AchievementHandler.findByName(`${GameConstants.camelCaseToString(GameConstants.Region[this.region])} Master`);
 
-        const caughtPokemonCount = new Set(App.game.party.caughtPokemon.filter(p => p.id > 0 && PokemonHelper.calcNativeRegion(p.name) <= this.region).map(p => Math.floor(p.id))).size;
-        const remainingPokemon = GameConstants.MaxIDPerRegion[this.region] - caughtPokemonCount;
 
-        const championBadge = new Map<GameConstants.Region, BadgeEnums>();
-        championBadge.set(GameConstants.Region.kanto, BadgeEnums.Elite_KantoChampion);
-        championBadge.set(GameConstants.Region.johto, BadgeEnums.Elite_JohtoChampion);
-        championBadge.set(GameConstants.Region.hoenn, BadgeEnums.Elite_HoennChampion);
-        championBadge.set(GameConstants.Region.sinnoh, BadgeEnums.Elite_SinnohChampion);
-        championBadge.set(GameConstants.Region.unova, BadgeEnums.Elite_UnovaChampion);
-        championBadge.set(GameConstants.Region.kalos, BadgeEnums.Elite_KalosChampion);
-        championBadge.set(GameConstants.Region.alola, BadgeEnums.Elite_AlolaChampion);
-        championBadge.set(GameConstants.Region.galar, BadgeEnums.Elite_GalarChampion);
+        if (!nextRegionUnlocked) {
+            return `<p>Hello, new Champion, you've come a long way!</p>
+                    <p>Come see me once you've beat the Elite Four!</p>`;
+        }
 
-        let html: string;
+        let html = '';
 
-        if (App.game.badgeCase.hasBadge(championBadge.get(this.region))) {
-            if (remainingPokemon == 0) {
-                html += '<p>You\'ve done it, you\'ve caught all Pokémon in this region! Congratulations!</p>';
-            } else {
-                html += `<p>You still have ${remainingPokemon} left to catch in this region! You're almost there!</p>`;
+        if (completeDexAchievement.isCompleted()) {
+            switch (this.region) {
+                case GameConstants.Region.kanto:
+                    html += '<p>Congratulations on completing your Kanto Pokédex!</p>';
+                    break;
+                case GameConstants.Region.johto:
+                    html += '<p>Oh, another regional Pokédex completed so soon?</p>';
+                    break;
+                case GameConstants.Region.hoenn:
+                    html += '<p>That\'s another regional Pokédex completed! Fantastic.</p>';
+                    break;
+                case GameConstants.Region.sinnoh:
+                    html += '<p>Congratulations, you\'re more than half-way completed on the national Pokédex!</p>';
+                    break;
+                case GameConstants.Region.unova:
+                    html += '<p>Let me see your progress...Ah, fantastic, as usual!</p>';
+                    break;
+                case GameConstants.Region.kalos:
+                    html += '<p></p>';
+                    break;
+                case GameConstants.Region.alola:
+                    html += '<p></p>';
+                    break;
+                case GameConstants.Region.galar:
+                    html += '<p>TODO: add text before Galars release</p>';
+                    break;
+                default:
+                    throw new Error('Not impleted for this region yet');
             }
         } else {
-            html += '<p>Come see me once you\'ve beat the Elite Four!</p>';
+            if (requiresCompleteDex) {
+                html += 'To progress to the next region, you need to catch all Pokémons in this region.';
+            }
+            html += `<p>You still have ${completeDexAchievement.property.requiredValue - completeDexAchievement.getProgress()} left to catch in this region! You're almost there!</p>`;
         }
 
-        if (uniquePokemonCaught >= GameConstants.MaxIDPerRegion[GameConstants.Region.unova]) {
-            html += `<p>Let me see your progress...Ah, fantastic, as usual!</p>
-                    <p>Allow me some time to arrange tickets for your next destination.</p>`;
-            return html;
+        if (nextRegionUnlocked && (completeDexAchievement.isCompleted() || !requiresCompleteDex)) {
+            switch (this.region) {
+                case GameConstants.Region.kanto:
+                    html += '<p>Your journey isn\'t over yet, a whole world awaits you! Onwards to Johto!</p>';
+                    break;
+                case GameConstants.Region.johto:
+                    html += '<p>Amazing! Next stop is Hoenn, enjoy the sunshine while you\'re there!</p>';
+                    break;
+                case GameConstants.Region.hoenn:
+                    html += '<p>I really appreciate being able to see your outstanding progress, thank you! Sinnoh is next up.</p>';
+                    break;
+                case GameConstants.Region.sinnoh:
+                    html += '<p>Next stop is Unova! I\'ve always wanted to visit Castelia City personally...</p>';
+                    break;
+                case GameConstants.Region.unova:
+                    html += '<p>Allow me some time to arrange tickets for your next destination.</p>';
+                    break;
+                case GameConstants.Region.kalos:
+                    html += '<p></p>';
+                    break;
+                case GameConstants.Region.alola:
+                    html += '<p></p>';
+                    break;
+                case GameConstants.Region.galar:
+                    html += '<p>TODO: add text before Galars release</p>';
+                    break;
+                default:
+                    throw new Error('Not impleted for this region yet');
+            }
         }
 
-        if (uniquePokemonCaught >= GameConstants.MaxIDPerRegion[GameConstants.Region.sinnoh]) {
-            html += `<p>Congratulations, you're more than half-way completed on the national Pokédex!</p>
-                    <p>Next stop is Unova! I've always wanted to visit Castelia City personally...</p>`;
-            return html;
-        }
-
-        if (uniquePokemonCaught >= GameConstants.MaxIDPerRegion[GameConstants.Region.hoenn]) {
-            html += `<p>That's another regional Pokédex completed! Fantastic.</p>
-                    <p>I really appreciate being able to see your outstanding progress, thank you!</p>`;
-            return html;
-        }
-
-        if (uniquePokemonCaught >= GameConstants.MaxIDPerRegion[GameConstants.Region.johto]) {
-            html += `<p>Oh, another regional Pokédex completed so soon?</p>
-                    <p>Amazing! Next stop is Hoenn, enjoy the sunshine while you're there!</p>`;
-            return html;
-        }
-
-        if (uniquePokemonCaught >= GameConstants.MaxIDPerRegion[GameConstants.Region.kanto]) {
-            html += `<p>Congratulations on completing your Kanto Pokédex!</p>
-                    <p>Your journey isn't over yet, a whole world awaits you! Onwards to Johto!</p>`;
-            return html;
-        }
-
-        if (App.game.badgeCase.badgeCount() >= 13) {
-            html += `<p>Hello, new Champion, you've come a long way!</p>
-                    <p>If you complete your Pokédex I can arrange for you to travel to Johto!</p>`;
-            return html;
-        }
-
-        //Else, it does the default message.
-        return super.dialogHTML;
+        return html;
     }
 }
