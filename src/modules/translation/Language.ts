9fb60be0e82bbb9e6f670012228a7473c46497c1
Jessica Chen <jessica.scoltock@protonmail.com>
2022-05-22 05:04:47 +0100

    Add official pokemon names in other languages

--- src/modules/translation/Language.ts
+++ src/modules/translation/Language.ts
@@ -1,6 +1,13 @@
 enum Language {
+    'de' = 'de',
     'en' = 'en',
+    'es' = 'es',
     'fr' = 'fr',
+    'it' = 'it',
+    'ja' = 'ja',
+    'ko' = 'ko',
+    'zh-Hans' = 'zh-Hans',
+    'zh-Hant' = 'zh-Hant',
 }
 
 export default Language;
