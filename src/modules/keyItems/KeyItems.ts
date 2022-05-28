2a045b050eb9f1f0a22667aa09df258bca328a64
Ultima <69112975+Ultima1990@users.noreply.github.com>
2022-05-28 11:11:43 +0200

    Redo some text change that I accidentally undid during conflict resolution

--- src/modules/keyItems/KeyItems.ts
+++ src/modules/keyItems/KeyItems.ts
@@ -115,7 +115,7 @@ export default class KeyItems implements Feature {
             // TODO obtain somewhere at the start
             new KeyItem(KeyItemType.Holo_caster, 'A device that allows users to see and track Achievements. Completing Achievements gives useful bonuses.',
                 () => App.game.party.caughtPokemon.length >= 110, undefined, undefined, 'Holo Caster'),
-            new KeyItem(KeyItemType.Mystery_egg, 'A mysterious Egg obtained from Mr. Pokémon. This allows you to use the Pokémon Day Care to help improve your Pokémons attack; some baby Pokémon can only be found through breeding too!',
+            new KeyItem(KeyItemType.Mystery_egg, 'A mysterious Egg obtained from Mr. Pokémon. This allows you to use the Pokémon Day Care to help improve your Pokémon attack. Some baby Pokémon can only be found through breeding too!',
                 () => App.game.statistics.routeKills[Region.kanto][5]() >= ROUTE_KILLS_NEEDED, undefined, undefined, 'Mystery Egg'),
             new KeyItem(KeyItemType.Safari_ticket, 'This ticket grants access to the Safari Zone right outside Fuchsia City.'),
             new KeyItem(KeyItemType.Wailmer_pail, 'This is a tool for watering Berries to allow you to operate the farm.',
