548e5946134a623bdeaeb02c741c40d2bb9ad972
Qwertypop04 <63805905+Qwertypop04@users.noreply.github.com>
2022-06-02 23:38:32 +0100

    Fix. Maybe. Probably not.

--- src/scripts/towns/Town.ts
+++ src/scripts/towns/Town.ts
@@ -57,7 +57,7 @@ class Town {
 class DungeonTown extends Town {
     dungeon: Dungeon
 
-    constructor(name: string, region: GameConstants.Region, content:  TownContent[] = [], requirements: (Requirement | OneFromManyRequirement)[] = []) {
+    constructor(name: string, region: GameConstants.Region, requirements: (Requirement | OneFromManyRequirement)[] = [], content:  TownContent[] = []) {
         super(name, region, { requirements }, content);
         this.dungeon = dungeonList[name];
     }
