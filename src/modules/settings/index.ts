66885c45629b663aa8646240d98385bf274fe140
Danial <admin@redsparr0w.com>
2022-06-08 11:57:54 +1200

    Update label

--- src/modules/settings/index.ts
+++ src/modules/settings/index.ts
@@ -114,7 +114,7 @@ Settings.add(new CssVariableSetting('completed', 'Completed Location', [], '#fff
 Settings.add(new BooleanSetting('disableAutoDownloadBackupSaveOnUpdate', 'Disable automatic backup save downloading when game updates', false));
 Settings.add(new BooleanSetting('useWebWorkerForGameTicks', 'Make use of web workers for game ticks (more consistent game speed)', true));
 Settings.add(new BooleanSetting('disableOfflineProgress', 'Disable offline progress', false));
-Settings.add(new Setting<string>('saveReminder', 'Save Reminder Interval:',
+Settings.add(new Setting<string>('saveReminder', 'Save reminder interval (in game time):',
     [
         new SettingOption('Never', '0'),
         new SettingOption('1 Hour', (1 * HOUR).toString()),
