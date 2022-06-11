2ac56ebfc7368e544eafe0e6f41ff7880ad0f696
jaaslet <jonas_bms@hotmail.com>
2022-06-11 22:36:12 +0200

    Fixed eggqueue stuck with hatchery helpers

--- src/scripts/breeding/Breeding.ts
+++ src/scripts/breeding/Breeding.ts
@@ -153,10 +153,10 @@ class Breeding implements Feature {
         return App.game.party.hasMaxLevelPokemon() && (this.hasFreeEggSlot() || this.hasFreeQueueSlot());
     }
 
-    public hasFreeEggSlot(): boolean {
+    public hasFreeEggSlot(isHelper = false): boolean {
         let counter = 0;
-        for (const egg of this._eggList) {
-            if (!egg().isNone()) {
+        for (let i = 0; i < this._eggList.length; i++) {
+            if (!this._eggList[i]().isNone() || (!isHelper && this.hatcheryHelpers.hired()[i])) {
                 counter++;
             }
         }
@@ -168,12 +168,12 @@ class Breeding implements Feature {
         return slots && this.queueList().length < slots;
     }
 
-    public gainEgg(e: Egg) {
+    public gainEgg(e: Egg, isHelper = false) {
         if (e.isNone()) {
             return false;
         }
         for (let i = 0; i < this._eggList.length; i++) {
-            if (this._eggList[i]().isNone()) {
+            if (this._eggList[i]().isNone() && (isHelper || !this.hatcheryHelpers.hired()[i])) {
                 this._eggList[i](e);
                 return true;
             }
@@ -254,8 +254,8 @@ class Breeding implements Feature {
         return false;
     }
 
-    public gainPokemonEgg(pokemon: PartyPokemon | PokemonListData): boolean {
-        if (!this.hasFreeEggSlot()) {
+    public gainPokemonEgg(pokemon: PartyPokemon | PokemonListData, isHelper = false): boolean {
+        if (!this.hasFreeEggSlot(isHelper)) {
             Notifier.notify({
                 message: 'You don\'t have any free egg slots',
                 type: NotificationConstants.NotificationOption.warning,
@@ -268,7 +268,7 @@ class Breeding implements Feature {
             pokemon.breeding = true;
         }
 
-        return this.gainEgg(egg);
+        return this.gainEgg(egg, isHelper);
     }
 
     public hatchPokemonEgg(index: number): void {
