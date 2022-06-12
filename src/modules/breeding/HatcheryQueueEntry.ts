a5350fa1405432c70f33fe316510fdde84d84ca9
Shallot Mama <rawrbot37@gmail.com>
2022-06-12 18:12:25 -0500

    fix type guard

--- src/modules/breeding/HatcheryQueueEntry.ts
+++ src/modules/breeding/HatcheryQueueEntry.ts
@@ -11,7 +11,7 @@ export type HatcheryQueueEntry = PokemonNameType | EggNameType | FossilNameType;
 export default class HatcheryQueue {
     public static isHatcheryEgg(u: unknown): u is EggNameType { return typeof u === 'string' && Object.keys(GameConstants.EggItemType).includes(u); }
     public static isHatcheryFossil(u: unknown): u is FossilNameType { return typeof u === 'string' && Object.keys(GameConstants.FossilToPokemon).includes(u); }
-    public static isHatcheryPokemon(u: unknown): u is PokemonNameType { return !HatcheryQueue.isHatcheryEgg(u) && !HatcheryQueue.isHatcheryFossil(u); }
+    public static isHatcheryPokemon(u: HatcheryQueueEntry): u is PokemonNameType { return !HatcheryQueue.isHatcheryEgg(u) && !HatcheryQueue.isHatcheryFossil(u); }
 
     public static eggItemTypeToEggType = (eggName: EggNameType): EggType => {
         const type = eggName.split('_')[0];
