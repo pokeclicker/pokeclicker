33624c3fb47428f9ae6898e3ece7066d4f86992f
Ultima <69112975+Ultima1990@users.noreply.github.com>
2022-06-13 19:18:56 +0200

    Temporarybattles

--- src/scripts/temporaryBattle/TemporaryBattleList.ts
+++ src/scripts/temporaryBattle/TemporaryBattleList.ts
@@ -12,3 +12,76 @@ TemporaryBattleList['Ultra Megalopolis'] = new TemporaryBattle(
     'Necrozma fled.',
     [new GymBadgeRequirement(BadgeEnums.DarkiniumZ)]
 );
+TemporaryBattleList['Captain Mina'] = new TemporaryBattle(
+    'Captain Mina',
+    [
+        new GymPokemon('Mawile', 90200640, 51),
+        new GymPokemon('Granbull', 90200640, 51),
+        new GymPokemon('Ribombee', 102200640, 51),
+    ],
+    'Woah! I\'m shocked at your strength!',
+    [new TemporaryBattleRequirement('Ultra Megalopolis')]
+);
+TemporaryBattleList['Captain Ilima'] = new TemporaryBattle(
+    'Captain Ilima',
+    [
+        new GymPokemon('Gumshoos', 90200640, 51),
+        new GymPokemon('Smeargle', 90200640, 51),
+        new GymPokemon('Komala', 102200640, 51),
+    ],
+    'Yes! You have emerged victorious!',
+    [
+        new TemporaryBattleRequirement('Captain Mina'),
+        new ClearDungeonRequirement(1, GameConstants.getDungeonIndex('Hau\'oli Cemetery')),
+    ]
+);
+TemporaryBattleList['Captain Mallow'] = new TemporaryBattle(
+    'Captain Mallow',
+    [
+        new GymPokemon('Trevenant', 90200640, 51),
+        new GymPokemon('Shiinotic', 90200640, 51),
+        new GymPokemon('Tsareena', 102200640, 51),
+    ],
+    'Sure enough, when it comes to you and Pokémon, the quality of the ingredients shines forth!',
+    [new TemporaryBattleRequirement('Captain Ilima')]
+);
+TemporaryBattleList['Captain Lana'] = new TemporaryBattle(
+    'Captain Lana',
+    [
+        new GymPokemon('Lanturn', 90200640, 51),
+        new GymPokemon('Cloyster', 90200640, 51),
+        new GymPokemon('Araquanid', 102200640, 51),
+    ],
+    'Well! Once again, you certainly reeled me in.',
+    [new TemporaryBattleRequirement('Captain Mallow')]
+);
+TemporaryBattleList['Captain Kiawe'] = new TemporaryBattle(
+    'Captain Kiawe',
+    [
+        new GymPokemon('Arcanine', 90200640, 51),
+        new GymPokemon('Talonflame', 90200640, 51),
+        new GymPokemon('Alolan Marowak', 102200640, 51),
+    ],
+    'Not enough dancing!',
+    [new TemporaryBattleRequirement('Captain Lana')]
+);
+TemporaryBattleList['Captain Sophocles'] = new TemporaryBattle(
+    'Captain Sophocles',
+    [
+        new GymPokemon('Togedemaru', 90200640, 51),
+        new GymPokemon('Magnezone', 90200640, 51),
+        new GymPokemon('Alolan Golem', 102200640, 51),
+    ],
+    'I couldn\'t get it done. Don\'t worry about it, my precious Pokémon...',
+    [new TemporaryBattleRequirement('Captain Kiawe')]
+);
+TemporaryBattleList['Kahuna Nanu'] = new TemporaryBattle(
+    'Kahuna Nanu',
+    [
+        new GymPokemon('Sableye', 90200640, 51),
+        new GymPokemon('Absol', 90200640, 51),
+        new GymPokemon('Alolan Persian', 102200640, 51),
+    ],
+    '...',
+    [new TemporaryBattleRequirement('Captain Sophocles')]
+);
