9e1245cc67d2e524ef83088506dbe541542424f7
Shallot Mama <rawrbot37@gmail.com>
2022-06-16 16:11:02 -0500

    remove incorrect options from EggNameType

--- src/modules/breeding/HatcheryNameTypes.ts
+++ src/modules/breeding/HatcheryNameTypes.ts
@@ -2,6 +2,6 @@ import * as GameConstants from '../GameConstants';
 import { PokemonNameType } from '../pokemons/PokemonNameType';
 import EggType from './EggType';
 
-export type EggNameType = keyof typeof EggType;
+export type EggNameType = Exclude<keyof typeof EggType, 'Fossil' | 'Pokemon'>;
 export type FossilNameType = keyof typeof GameConstants.FossilToPokemon;
 export type HatcheryQueueEntryName = PokemonNameType | EggNameType | FossilNameType;
