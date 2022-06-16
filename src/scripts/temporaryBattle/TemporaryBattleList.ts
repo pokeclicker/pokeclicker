8062b7e02a49f8e4150dcfc7628c4ea3fe41584a
Qwertypop04 <63805905+Qwertypop04@users.noreply.github.com>
2022-06-16 17:10:00 +0100

    AZ is not from Alola

--- src/scripts/temporaryBattle/TemporaryBattleList.ts
+++ src/scripts/temporaryBattle/TemporaryBattleList.ts
@@ -1,17 +1,110 @@
 const TemporaryBattleList: { [battleName: string]: TemporaryBattle } = {};
 
-//Alola Temporary Battles
+//Kalos Temporary Battles
+TemporaryBattleList['AZ'] = new TemporaryBattle(
+    'AZ',
+    [
+        new GymPokemon('Torkoal', 247996000, 60),
+        new GymPokemon('Golurk', 247996000, 60),
+        new GymPokemon('Sigilyph', 247996000, 60),
+    ],
+    'Thank you very much for battling with me. Now I finally feel free…',
+    [new GymBadgeRequirement(BadgeEnums.Elite_KalosChampion)],
+    [],
+    () => {
+        App.game.party.gainPokemonById(670.05);
+    }
+);
 TemporaryBattleList['Ultra Wormhole'] = new TemporaryBattle(
     'Ultra Wormhole',
     [new GymPokemon('???', 264590972, 27)],
     'The creature escaped back into the ultra wormhole.',
     [new GymBadgeRequirement(BadgeEnums.RockiumZ)]
 );
+
+//Alola Temporary Battles
 TemporaryBattleList['Ultra Megalopolis'] = new TemporaryBattle(
     'Ultra Megalopolis',
     [new GymPokemon('Necrozma (Ultra)', 282601920, 60)],
     'Necrozma fled.',
-    [new GymBadgeRequirement(BadgeEnums.DarkiniumZ)]
+    [new GymBadgeRequirement(BadgeEnums.DarkiniumZ)],
+    [],
+    () => {
+        App.game.quests.getQuestLine('Mina\'s Trial').beginQuest();
+    }
+);
+TemporaryBattleList['Captain Mina'] = new TemporaryBattle(
+    'Captain Mina',
+    [
+        new GymPokemon('Mawile', 90200640, 51),
+        new GymPokemon('Granbull', 90200640, 51),
+        new GymPokemon('Ribombee', 102200640, 51),
+    ],
+    'Woah! I\'m shocked at your strength!',
+    [new TemporaryBattleRequirement('Ultra Megalopolis')]
+);
+TemporaryBattleList['Captain Ilima'] = new TemporaryBattle(
+    'Captain Ilima',
+    [
+        new GymPokemon('Gumshoos', 90200640, 51),
+        new GymPokemon('Smeargle', 90200640, 51),
+        new GymPokemon('Komala', 102200640, 51),
+    ],
+    'Yes! You have emerged victorious!',
+    [
+        new TemporaryBattleRequirement('Captain Mina'),
+        new ClearDungeonRequirement(1, GameConstants.getDungeonIndex('Hau\'oli Cemetery')),
+    ]
+);
+TemporaryBattleList['Captain Mallow'] = new TemporaryBattle(
+    'Captain Mallow',
+    [
+        new GymPokemon('Trevenant', 90200640, 51),
+        new GymPokemon('Shiinotic', 90200640, 51),
+        new GymPokemon('Tsareena', 102200640, 51),
+    ],
+    'Sure enough, when it comes to you and Pokémon, the quality of the ingredients shines forth!',
+    [new TemporaryBattleRequirement('Captain Ilima')]
+);
+TemporaryBattleList['Captain Lana'] = new TemporaryBattle(
+    'Captain Lana',
+    [
+        new GymPokemon('Lanturn', 90200640, 51),
+        new GymPokemon('Cloyster', 90200640, 51),
+        new GymPokemon('Araquanid', 102200640, 51),
+    ],
+    'Well! Once again, you certainly reeled me in.',
+    [new TemporaryBattleRequirement('Captain Mallow')]
+);
+TemporaryBattleList['Captain Kiawe'] = new TemporaryBattle(
+    'Captain Kiawe',
+    [
+        new GymPokemon('Arcanine', 90200640, 51),
+        new GymPokemon('Talonflame', 90200640, 51),
+        new GymPokemon('Alolan Marowak', 102200640, 51),
+    ],
+    'Not enough dancing!',
+    [new TemporaryBattleRequirement('Captain Lana')]
+);
+TemporaryBattleList['Captain Sophocles'] = new TemporaryBattle(
+    'Captain Sophocles',
+    [
+        new GymPokemon('Togedemaru', 90200640, 51),
+        new GymPokemon('Magnezone', 90200640, 51),
+        new GymPokemon('Alolan Golem', 102200640, 51),
+    ],
+    'I couldn\'t get it done. Don\'t worry about it, my precious Pokémon...',
+    [new TemporaryBattleRequirement('Captain Kiawe')]
+);
+TemporaryBattleList['Kahuna Nanu'] = new TemporaryBattle(
+    'Kahuna Nanu',
+    [
+        new GymPokemon('Sableye', 90200640, 51),
+        new GymPokemon('Absol', 90200640, 51),
+        new GymPokemon('Alolan Persian', 102200640, 51),
+    ],
+    '...',
+    [new TemporaryBattleRequirement('Captain Sophocles')]
 );
 
 //Galar Temporary Battles
