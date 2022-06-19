952226642dca5bdb417701ef4dcd9aa71798d61f
Qwertypop04 <63805905+Qwertypop04@users.noreply.github.com>
2022-06-19 02:41:31 +0100

    Dynamax Duskball

--- src/modules/GameConstants.ts
+++ src/modules/GameConstants.ts
@@ -1213,7 +1213,7 @@ export const TemporaryBattles = [
     'Dynamax Torkoal',
     'Sordward & Shielbert',
     'Dynamax Conkeldurr',
-    'Dynamax Duskball',
+    'Dynamax Dusknoir',
     'Bede4',
     'Dynamax Gigalith',
     'Dynamax Froslass',
