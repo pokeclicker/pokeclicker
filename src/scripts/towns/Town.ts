8866776dac6dca4edf3ed24d1d58c3302f8cc5e9
Qwertypop04 <63805905+Qwertypop04@users.noreply.github.com>
2022-06-23 21:12:17 +0100

    Update Town.ts

--- src/scripts/towns/Town.ts
+++ src/scripts/towns/Town.ts
@@ -57,8 +57,8 @@ class Town {
 class DungeonTown extends Town {
     dungeon: Dungeon
 
-    constructor(name: string, region: GameConstants.Region, requirements: (Requirement | OneFromManyRequirement)[] = [], content: TownContent[] = []) {
-        super(name, region, content, { requirements });
+    constructor(name: string, region: GameConstants.Region, requirements: (Requirement | OneFromManyRequirement)[] = [], npcs: NPC[] = [], content: TownContent[] = []) {
+        super(name, region, content, npcs, { requirements });
         this.dungeon = dungeonList[name];
     }
 }
