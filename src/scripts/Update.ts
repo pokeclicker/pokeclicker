5b7cb974933f50bbed57cea76e6bd5caae0eee6c
Justin <justinjdbennett@gmail.com>
2022-06-24 03:44:08 -0600

    misc text

--- src/scripts/Update.ts
+++ src/scripts/Update.ts
@@ -765,7 +765,7 @@ class Update implements Saveable {
             // Start pokerus
             setTimeout(async () => {
                 // Check if player wants to activate the new challenge modes
-                if (!await Notifier.confirm({ title: 'Slow EVs', message: 'New challenge mode added: Slow EVs.\n\nDiminishes the rate at which EVs are gained.\n\nThis is not the default and recommended way to play, and is an optional challenge.\n\nPlease choose if you would like this challenge mode to be disabled (reccomended, cannot be re-enabled later) or activated', confirm: 'Disable', cancel: 'Activate' })) {
+                if (!await Notifier.confirm({ title: 'Slow EVs', message: 'New challenge mode added: Slow EVs.\n\nDiminishes the rate at which EVs are gained.\n\nThis is not the default and recommended way to play, and is an optional challenge.\n\nPlease choose if you would like this challenge mode to be disabled (recommended, cannot be re-enabled later) or activated', confirm: 'Disable', cancel: 'Activate' })) {
                     App.game.challenges.list.slowEVs.activate();
                 }
             }, GameConstants.SECOND);
