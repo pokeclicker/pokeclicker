40a1f41dca785efc97c1ffaa4c6cf47bc259ca85
Dragonchitos <dragonchitos@gmail.com>
2022-06-30 19:36:52 +0200

    Remove CurrentLocation background

--- src/modules/settings/index.ts
+++ src/modules/settings/index.ts
@@ -102,7 +102,6 @@ Settings.add(new BooleanSetting('showGymGoAnimation', 'Show Gym GO animation', t
 
 // CSS variable settings
 Settings.add(new CssVariableSetting('locked', 'Locked Location', [], '#000000'));
-Settings.add(new CssVariableSetting('currentPlace', 'Current Location', [], '#55ff00'));
 Settings.add(new CssVariableSetting('incomplete', 'Incomplete Area', [], '#ff9100'));
 Settings.add(new CssVariableSetting('questAtLocation', 'Quest at Location', [], '#34BF45'));
 Settings.add(new CssVariableSetting('uncaughtPokemon', 'Uncaught Pokemon', [], '#3498db'));
