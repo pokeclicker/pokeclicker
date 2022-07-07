92005f677a299240cc615b5be50716b4d5f6a5e7
Qwertypop04 <63805905+Qwertypop04@users.noreply.github.com>
2022-07-07 20:45:15 +0100

    Wouldn't be a Thursday without something breaking due to a minor typo

--- src/scripts/towns/TownList.ts
+++ src/scripts/towns/TownList.ts
@@ -3497,7 +3497,7 @@ TownList.Freezington = new Town(
     [TemporaryBattleList.Calyrex, TemporaryBattleList.Glastrier, TemporaryBattleList.Spectrier, FreezingtonShop],
     {
         requirements: [new RouteKillRequirement(10, GameConstants.Region.galar, 46)],
-        npcs: [Crown TundraRoamerNPC],
+        npcs: [CrownTundraRoamerNPC],
     }
 );
 
