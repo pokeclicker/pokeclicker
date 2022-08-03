30075817ee1078910eaf764d3b3107c6307d530f
Qwertypop04 <63805905+Qwertypop04@users.noreply.github.com>
2022-08-03 01:29:33 +0100

    fix cap image and hide

--- src/scripts/dungeons/Dungeon.ts
+++ src/scripts/dungeons/Dungeon.ts
@@ -7077,7 +7077,7 @@ dungeonList['Pikachu Valley'] = new Dungeon('Pikachu Valley',
         new DungeonBossPokemon('Pikachu (Unova Cap)', 59764020, 15),
         new DungeonBossPokemon('Pikachu (Kalos Cap)', 59764020, 15),
         new DungeonBossPokemon('Pikachu (Alola Cap)', 59764020, 15, {requirement: new GymBadgeRequirement(BadgeEnums.Elite_AlolaChampion)}),
-        new DungeonBossPokemon('Pikachu (World Cap)', 59764020, 15, {requirement: new GymBadgeRequirement(BadgeEnums.Elite_GalarChampion)}),
+        new DungeonBossPokemon('Pikachu (World Cap)', 59764020, 15, {hide: true, requirement: new GymBadgeRequirement(BadgeEnums.Elite_GalarChampion)}),
     ],
     850000, 4);
 
