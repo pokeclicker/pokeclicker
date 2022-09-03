70b0acc9cb961523a994cd60efe7dfb09a485d5d
eugene171 <109317224+Farboo171@users.noreply.github.com>
2022-09-03 11:54:38 -0400

    new(pokemon): Pumpkaboo line size additions, Aegislash Blade Forme additions (#2917)
    * Pumpkaboo, Gorgeist, and Aegislash forme additions
    
    Additions of Pumpkaboo and Gourgeist sizes, and aegislash blade forme
    
    * Update Credits.ts
    
    * Updated Profile.ts to actually display the new trainer image
    
    * Tinypng and Update.ts for names
    
    * Moved to 0.9.15
    
    * Updates Update.ts with new rename syntax
    
    Copied syntax from the clone capitalization fixes
    
    Co-authored-by: Jaaslet <jonas_bms@hotmail.com>

--- src/modules/Credits.ts
+++ src/modules/Credits.ts
@@ -176,6 +176,7 @@ export const SpriteCredits: Credit[] = [
             'Pinkan Nidoran(M)',
             'Pinkan Primeape',
             'Pinkan Venonat',
+            'Koga Trainer',
         ],
         // Notes: Made specifically for PokéClicker.
     },
