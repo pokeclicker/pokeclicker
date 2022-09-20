107f1882b40ef315d55a7e80dc0123bb17dfe01b
Danial <admin@redsparr0w.com>
2022-09-21 09:09:42 +1200

    Fixup sounds

--- src/scripts/breeding/Egg.ts
+++ src/scripts/breeding/Egg.ts
@@ -79,7 +79,7 @@ class Egg implements Saveable {
         }
         this.updateShinyChance(amount, multiplier);
         this.steps(this.steps() + amount);
-        if (this.canHatch() && helper == false && App.game.breeding.queueList().length == 0) {
+        if (this.canHatch() && !helper) {
             if (this.type == EggType.Pokemon) {
                 Notifier.notify({
                     message: `${PokemonHelper.getPokemonById(this.pokemon).name} is ready to hatch!`,
