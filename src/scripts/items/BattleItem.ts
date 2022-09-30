7dd67c6c5f3be2a23f854e00f4e0ebfa55b2a7ca
Justin <justinjdbennett@gmail.com>
2022-09-29 21:54:25 -0600

    remove test stuff

--- src/scripts/items/BattleItem.ts
+++ src/scripts/items/BattleItem.ts
@@ -42,7 +42,7 @@ class BattleItem extends Item {
 }
 
 ItemList.xAttack         = new BattleItem(GameConstants.BattleItemType.xAttack, '+50% Bonus to Pokémon attack for 30 seconds', 600, undefined, 'X Attack', 'pokemonAttack', 1.5);
-ItemList.xClick          = new BattleItem(GameConstants.BattleItemType.xClick, '+50% Bonus to click attack for 30 seconds', 400, undefined, 'X Click', 'clickAttack', 1500);
+ItemList.xClick          = new BattleItem(GameConstants.BattleItemType.xClick, '+50% Bonus to click attack for 30 seconds', 400, undefined, 'X Click', 'clickAttack', 1.5);
 ItemList.Lucky_egg       = new BattleItem(GameConstants.BattleItemType.Lucky_egg, '+50% Bonus to experience gained for 30 seconds', 800, undefined, 'Lucky Egg', 'exp', 1.5);
 ItemList.Token_collector = new BattleItem(GameConstants.BattleItemType.Token_collector, '+50% Bonus to Dungeon Tokens gained for 30 seconds', 1000, undefined, 'Token Collector', 'dungeonToken', 1.5);
 ItemList.Dowsing_machine     = new BattleItem(GameConstants.BattleItemType.Dowsing_machine, 'Increased chance of gaining extra items for 30 seconds', 1500, undefined, 'Dowsing Machine');
