e25cb84f3dcb590a587c72b56cf6d70da9175513
Jessica Chen <jessica.scoltock@protonmail.com>
2022-10-02 21:02:00 +0100

    Translations: Make empty strings fallback to english

--- src/modules/translation/Translation.ts
+++ src/modules/translation/Translation.ts
@@ -48,6 +48,7 @@ export default class Translate {
                 backend: {
                     loadPath: '$TRANSLATIONS_URL/locales/{{lng}}/{{ns}}.json',
                 },
+                returnEmptyString: false,
             });
 
         i18next.on('initialized', () => {
