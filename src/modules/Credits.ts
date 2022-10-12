a910ec747dc2ef1cde22714a29b7538e1905c68d
Jaaslet <jonas_bms@hotmail.com>
2022-10-12 23:25:32 +0200

    eslint

--- src/modules/Credits.ts
+++ src/modules/Credits.ts
@@ -181,7 +181,7 @@ export const SpriteCredits: Credit[] = [
             'Dungeon Ladder',
             'Galar fossils items',
             'Galarica cuff and wreath icons',
-            'Linking cord cartoon style', 
+            'Linking cord cartoon style',
             'Luana',
             'Merilyn',
             'Pinkan Arbok',
