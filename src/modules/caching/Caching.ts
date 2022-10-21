7017210725e1b9b7b502a08a8e2a2554e8f0ffcd
ErzaDS <danilosaita@hotmail.com>
2022-10-21 20:34:38 -0300

    Updating cache ttl

--- src/modules/caching/Caching.ts
+++ src/modules/caching/Caching.ts
@@ -3,6 +3,7 @@ import Settings from '../settings/Settings';
 
 export default class Caching {
     private static caches: Map<string, NodeCache> = new Map();
+    private static cacheTtlInSeconds = 0;
     public static set(cacheName: string, key: string, val: any): void {
         if (Settings.getSetting('cacheTtlInSeconds').value > 0) {
             this.getCache(cacheName).set(key, val);
@@ -17,8 +18,13 @@ export default class Caching {
     }
 
     private static getCache(cacheName: string): NodeCache {
+        const cacheTtlInSecondsSetting = Settings.getSetting('cacheTtlInSeconds');
+        if (this.cacheTtlInSeconds !== cacheTtlInSecondsSetting.value) {
+            this.caches.clear();
+            this.cacheTtlInSeconds = cacheTtlInSecondsSetting.value;
+        }
         if (!this.caches.has(cacheName)) {
-            this.caches.set(cacheName, new NodeCache({ stdTTL: Settings.getSetting('cacheTtlInSeconds').value }));
+            this.caches.set(cacheName, new NodeCache({ stdTTL: this.cacheTtlInSeconds }));
         }
         return this.caches.get(cacheName);
     }
