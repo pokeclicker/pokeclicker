26673268ed9b592b06faccf6dd1e64e37b18d91e
HyruleTeam64 <106347315+HyruleTeam64@users.noreply.github.com>
2022-10-25 21:01:02 +0200

    Update QuestLineHelper.ts

--- src/scripts/quests/QuestLineHelper.ts
+++ src/scripts/quests/QuestLineHelper.ts
@@ -765,6 +765,62 @@ class QuestLineHelper {
 
         App.game.quests.questLines().push(galacticSinnohQuestLine);
     }
+    
+    //Giratina Origin quest
+    public static createGiratinaOriginQuestLine() {
+        const giratinaOriginQuestLine = new QuestLine('Zero\'s Ambition', 'Help Zero find an entrance to the Distortion World.', new MultiRequirement([new TemporaryBattleRequirement('Giratina'), new ObtainedPokemonRequirement(pokemonMap.Uxie), new ObtainedPokemonRequirement(pokemonMap.Mesprit), new ObtainedPokemonRequirement(pokemonMap.Azelf), new GymBadgeRequirement(BadgeEnums.Elite_SinnohChampion)]), GameConstants.BulletinBoards.Sinnoh);
+
+        const talktoZero1 = new TalkToNPCQuest(FightAreaZero1, 'Talk to Zero in the Fight Area.');
+        giratinaOriginQuestLine.addQuest(talktoZero1);
+
+        const readSinnohMyth = new TalkToNPCQuest(CanalaveSinnohMyth, 'Read the recently discovered book in Canalave City.');
+        giratinaOriginQuestLine.addQuest(readSinnohMyth);
+
+        const talktoZero2 = new TalkToNPCQuest(FightAreaZero2, 'Report to Zero what was in the book.');
+        giratinaOriginQuestLine.addQuest(talktoZero2);
+
+        const talktoMesprit = new TalkToNPCQuest(VerityMesprit, 'Ask Mesprit about the Distorsion World in Lake Verity.');
+        giratinaOriginQuestLine.addQuest(talktoMesprit);
+
+        const obtain10PurpleShards = new CustomQuest(10, undefined, 'Obtain 10 Purple Shards.', () => player.mineInventory().find(item => item.name == 'Purple Shard').amount());
+        giratinaOriginQuestLine.addQuest(obtain10PurpleShards);
+
+        const talktoAzelf = new TalkToNPCQuest(ValorAzelf, 'Ask Azelf about the Distorsion World in Lake Valor.');
+        giratinaOriginQuestLine.addQuest(talktoAzelf);
+
+        const obtain10OchreShards = new CustomQuest(10, undefined, 'Obtain 10 Ochre Shards.', () => player.mineInventory().find(item => item.name == 'Ochre Shard').amount());
+        giratinaOriginQuestLine.addQuest(obtain10OchreShards);
+
+        const talktoUxie = new TalkToNPCQuest(AcuityUxie, 'Ask Uxie about the Distorsion World in Lake Acuity.');
+        giratinaOriginQuestLine.addQuest(talktoUxie);
+
+        const obtain10CrimsonShards = new CustomQuest(10, undefined, 'Obtain 10 Crimson Shards.', () => player.mineInventory().find(item => item.name == 'Crimson Shard').amount());
+        giratinaOriginQuestLine.addQuest(obtain10CrimsonShards);
+
+        const clearSendoffSpring = new CustomQuest(1, 0, 'Clear Sendoff Spring to meet the Lake Trio.', () => App.game.statistics.dungeonsCleared[GameConstants.getDungeonIndex('Sendoff Spring')]());
+        giratinaOriginQuestLine.addQuest(clearSendoffSpring);
+
+        const talktoLakeTrio = new TalkToNPCQuest(SendoffSpringLakeTrio, 'Talk to the Lake Trio in Sendoff Spring.');
+        giratinaOriginQuestLine.addQuest(talktoLakeTrio);
+
+        const chargeDistortionKey = new MultipleQuestsQuest(
+            [
+                new GainGemsQuest(500, 0, PokemonType.Ghost),
+                new CustomQuest(1, undefined, 'Gain 1 Spooky Plate.', () => player.mineInventory().find(item => item.name == 'Spooky Plate').amount()),
+            ], 'Charge the key to the Distortion World.');
+        giratinaOriginQuestLine.addQuest(chargeDistortionKey);
+
+        const talktoZero3 = new TalkToNPCQuest(SendoffSpringZero, 'Talk to Zero in Sendoff Spring.');
+        giratinaOriginQuestLine.addQuest(talktoZero3);
+
+        const clearZero = new CustomQuest (1, 0, 'Defeat Zero to protect the key.', () => App.game.statistics.temporaryBattleDefeated[GameConstants.getTemporaryBattlesIndex('Zero')]());
+        giratinaOriginQuestLine.addQuest(clearZero);
+
+        const catchGiratina = new CaptureSpecificPokemonQuest('Giratina (Origin)', 'Catch the ruler of the Distortion World', 1, false, 0, undefined);
+        giratinaOriginQuestLine.addQuest(catchGiratina);
+
+        App.game.quests.questLines().push(giratinaOriginQuestLine);
+    }
 
     // Unova QuestLines
     // Started upon defeating Virbank City's gym.
