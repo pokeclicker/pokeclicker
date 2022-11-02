b698a482b1f53d2602593cd656c1bc20be94ce7d
ErzaDS <danilosaita@hotmail.com>
2022-11-02 17:53:14 -0300

    fixing nested modals, introjs adjustments

--- src/modules/utilities/Information.ts
+++ src/modules/utilities/Information.ts
@@ -3,8 +3,6 @@ import introJs from 'intro.js';
 export default class Information {
     static defaultOptions = {
         showStepNumbers: false, // Hide the red step number on element
-        hideNext: true, // Hide next button on last step
-        hidePrev: true, // Hide prev button on first step
         exitOnOverlayClick: false, // Disable this, incase user clicking intensely
         showBullets: false,
     };
