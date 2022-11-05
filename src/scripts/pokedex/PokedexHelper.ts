ffe3ccca0dbde5753661f6b59d5959836eb0dfc1
osherwott <114451054+osherwott@users.noreply.github.com>
2022-11-05 17:07:56 -0500

    syntax fix

--- src/scripts/pokedex/PokedexHelper.ts
+++ src/scripts/pokedex/PokedexHelper.ts
@@ -114,10 +114,10 @@ class PokedexHelper {
             if (!alreadyCaught && pokemon.id != Math.floor(pokemon.id)) {
                 return false;
             }
-            if (Number.isInteger(pokemon.id) && ((pokemon.id+0.01) || (pokemon.id+0.1)) != null) {
-                for (let i = 1; i <=99; i++) {
-                    if (App.game.party.alreadyCaughtPokemon(pokemon.id+(0.01*i))) {
-                return false;
+            if (Number.isInteger(pokemon.id) && ((pokemon.id + 0.01) || (pokemon.id + 0.1)) != null) {
+                for (let i = 1; i <= 99; i++) {
+                    if (App.game.party.alreadyCaughtPokemon(pokemon.id + (0.01 * i))) {
+                        return false;
                     }
                 }
             }
