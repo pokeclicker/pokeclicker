9d534ecfb8e0f23d33bcc94cdf1e5de984b93714
osherwott <114451054+osherwott@users.noreply.github.com>
2022-11-05 21:08:33 -0500

    Prevent caught base form from disappearing

--- src/scripts/pokedex/PokedexHelper.ts
+++ src/scripts/pokedex/PokedexHelper.ts
@@ -114,7 +114,8 @@ class PokedexHelper {
             if (!alreadyCaught && pokemon.id != Math.floor(pokemon.id)) {
                 return false;
             }
-            if (Number.isInteger(pokemon.id) && ((pokemon.id + 0.01) || (pokemon.id + 0.1)) != null) {
+            // Alternate forms that are caught without base form
+            if (!alreadyCaught && pokemon.id = Math.floor(pokemon.id) && ((pokemon.id + 0.01) || (pokemon.id + 0.1)) != null) {
                 for (let i = 1; i <= 99; i++) {
                     if (App.game.party.alreadyCaughtPokemon(pokemon.id + (0.01 * i))) {
                         return false;
