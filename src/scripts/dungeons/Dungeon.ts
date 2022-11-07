4e302b02409d1483622e9e9756f02278c4b8f6aa
Qwertypop04 <63805905+Qwertypop04@users.noreply.github.com>
2022-11-07 20:16:27 +0000

    change(johto): Removes the certain Unown A from Ruins of Alph (#3330)

--- src/scripts/dungeons/Dungeon.ts
+++ src/scripts/dungeons/Dungeon.ts
@@ -1806,10 +1806,6 @@ dungeonList['Ruins of Alph'] = new Dungeon('Ruins of Alph',
     },
     60600,
     [
-        new DungeonBossPokemon('Unown (A)', 280000, 14, {
-            hide: true,
-            requirement: new ObtainedPokemonRequirement(pokemonMap['Unown (A)'], true),
-        }),
         ...AlphUnownList.map((char) => new DungeonBossPokemon(`Unown (${char})` as PokemonNameType, 280000, 14, {
             hide: true,
             requirement: new SeededDateRequirement(() => SeededDateRand.fromArray(AlphUnownList) == char),
