a546a74e060ff3ab18c7be990569e82c7caff5f7
someone soul <97617298+Someonealive-QN@users.noreply.github.com>
2022-12-17 07:53:57 -0300

    Update TownList.ts

--- src/scripts/towns/TownList.ts
+++ src/scripts/towns/TownList.ts
@@ -3348,8 +3348,8 @@ const ProfJuniper = new ProfNPC('Prof. Juniper',
     GameConstants.Region.unova,
     'Let me see your progress...Ah, fantastic, as usual!',
     'Allow me some time to arrange tickets for your next destination.',
-], { 
-    image: 'assets/images/npcs/Professor Juniper.png', 
+], {
+    image: 'assets/images/npcs/Professor Juniper.png',
 });
 
 const ProfBurnet = new NPC('Professor Burnet', [
