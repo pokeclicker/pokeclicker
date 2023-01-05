b478f615bc3f1e4af7be5203ae83dfd0019a5fae
CypherX <cypherx00@gmail.com>
2023-01-04 20:04:21 -0500

    Expanded vitamin management modal

--- src/modules/items/VitaminController.ts
+++ src/modules/items/VitaminController.ts
@@ -3,7 +3,7 @@ import { VitaminType } from '../GameConstants';
 export default class VitaminController {
     public static currentlySelected = ko.observable(VitaminType.Protein).extend({ numeric: 0 });
     public static add = ko.observable(true).extend({ boolean: null });
-    public static multiplier = ['×1', '×5', 'x10', 'Max'];
+    public static multiplier = ['×1', '×5', '×10', 'Max'];
     public static multiplierIndex = ko.observable(0);
 
     public static incrementMultiplier() {
