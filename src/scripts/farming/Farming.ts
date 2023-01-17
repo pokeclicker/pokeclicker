fd8c302ee2202e45dc72c667cae03ed3c251e00e
osherwott <114451054+osherwott@users.noreply.github.com>
2023-01-17 09:46:37 -0600

    Size Units conversion update

--- src/scripts/farming/Farming.ts
+++ src/scripts/farming/Farming.ts
@@ -2183,4 +2183,8 @@ class Farming implements Feature {
     public static getFirmness(firmness: BerryFirmness): BerryType[] {
         return App.game.farming.berryData.filter(berry => berry.firmness === firmness).map(berry => berry.type);
     }
+    public static sizeUnitConverter: Record<SizeUnits, number> = {
+      [SizeUnits.sipoint]: (number) => number, // default is cm
+      [SizeUnits.imperial]: (number) => number / 2.54 // inches
+    };
 }
