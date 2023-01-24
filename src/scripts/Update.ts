84ad5be3551470c23774a688f5d41b1eab125dea
someone soul <97617298+Someonealive-QN@users.noreply.github.com>
2023-01-24 14:38:21 -0300

    Update Update.ts

--- src/scripts/Update.ts
+++ src/scripts/Update.ts
@@ -1942,9 +1942,6 @@ class Update implements Saveable {
             saveData.statistics.temporaryBattleDefeated = Update.moveIndex(saveData.statistics.temporaryBattleDefeated, 71);
             saveData.statistics.temporaryBattleDefeated = Update.moveIndex(saveData.statistics.temporaryBattleDefeated, 72);
             saveData.statistics.temporaryBattleDefeated = Update.moveIndex(saveData.statistics.temporaryBattleDefeated, 73);
-
-            //Marquis Grant
-            saveData.statistics.temporaryBattleDefeated = Update.moveIndex(saveData.statistics.temporaryBattleDefeated, 139);
         },
     };
 
