262653b19aa4f9fae1bf461daf1a9c21460d0766
jaaslet <jonas_bms@hotmail.com>
2023-01-28 00:27:34 +0100

    Now escaping html...

--- src/modules/utilities/TextMerger.ts
+++ src/modules/utilities/TextMerger.ts
@@ -1,6 +1,7 @@
 import type { Observable } from 'knockout';
 
 export default class TextMerger {
+    private static tempElementForEscape = document.createElement('textarea');
     private static mergeValues : { [name: string]: Observable<string> };
 
     public static mergeText(text: string) : string {
@@ -16,7 +17,7 @@ export default class TextMerger {
         mergeSubstrings.forEach((s) => {
             const key = s.substring(1, s.length - 1).toLocaleLowerCase();
             if (this.mergeValues[key]()) {
-                textResult = text.replace(s, this.mergeValues[key]());
+                textResult = text.replace(s, this.escapeHtml(this.mergeValues[key]()));
             }
         });
 
@@ -27,5 +28,10 @@ export default class TextMerger {
         this.mergeValues = {
             playername: App.game.profile.name,
         };
+    }
+
+    private static escapeHtml(text: string) {
+        TextMerger.tempElementForEscape.textContent = text;
+        return TextMerger.tempElementForEscape.innerHTML;
     }
 }
