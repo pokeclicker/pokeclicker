09b4d2e88afbb10687929632fe23384cdb7997e0
Danial <admin@redsparr0w.com>
2023-02-02 18:12:18 +1300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -62,7 +62,7 @@ module.exports = {
     // forceCoverageMatch: [],
 
     // A path to a module which exports an async function that is triggered once before all test suites
-    // globalSetup: undefined,
+    globalSetup: './jest.global.setup.js',
 
     // A path to a module which exports an async function that is triggered once after all test suites
     // globalTeardown: undefined,
