d88205a140d8f5eb9f096dc9eb18549f565db21a
Danial <admin@redsparr0w.com>
2023-02-02 14:41:02 +1300

    Initial testing and such

---
+++ src/modules/Credits.test.ts
@@ -0,0 +1,15 @@
+import {
+    describe, expect, it,
+} from '@jest/globals';
+import { SpriteCredits, CodeCredits } from './Credits';
+
+describe('Test Credits', () => {
+    it('should have the correct properties', () => {
+        [...SpriteCredits, ...CodeCredits].forEach((credit) => {
+            expect(credit).toHaveProperty('name');
+            expect(credit).toHaveProperty('link');
+            expect(credit).toHaveProperty('image');
+            expect(credit).toHaveProperty('resources');
+        });
+    });
+});
