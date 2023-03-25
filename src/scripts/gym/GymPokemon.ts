eb273f92b1a3ea4c0968b7204a10ef6613976267
jaaslet <jonas_bms@hotmail.com>
2023-03-25 10:11:43 +0100

    Boolean to GameConstants.ShadowStatus

--- src/scripts/gym/GymPokemon.ts
+++ src/scripts/gym/GymPokemon.ts
@@ -3,10 +3,10 @@ class GymPokemon {
     maxHealth: number;
     level: number;
     shiny: boolean;
-    shadow: boolean; // Currently don't do anything. Will be added later
+    shadow: GameConstants.ShadowStatus; // Currently don't do anything. Will be added later
     requirements: Requirement[];
 
-    constructor(name: PokemonNameType, maxHealth: number, level: number, requirements: Requirement | Requirement[] = [], shiny?: boolean, shadow?: boolean) {
+    constructor(name: PokemonNameType, maxHealth: number, level: number, requirements: Requirement | Requirement[] = [], shiny?: boolean, shadow?: GameConstants.ShadowStatus) {
         this.name = name;
         this.maxHealth = maxHealth;
         this.level = level;
