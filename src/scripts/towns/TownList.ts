f5f2887621e0941dff6566a658b0e1c77ed88456
osherwott <114451054+osherwott@users.noreply.github.com>
2023-04-02 17:27:59 -0500

    commma

--- src/scripts/towns/TownList.ts
+++ src/scripts/towns/TownList.ts
@@ -5938,7 +5938,7 @@ TownList['Verdant Cavern'] = new DungeonTown(
     [new MultiRequirement([
         new RouteKillRequirement(10, GameConstants.Region.alola, 2),
         new QuestLineStepCompletedRequirement('Welcome to paradise, cousin!', 7),
-    ]),]
+    ],)]
     undefined,
     { npcs: [ReconSquad3] }
 );
