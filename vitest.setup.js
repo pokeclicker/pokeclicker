dac6c868f7d1509ea86124fd15a56de64db063dd
Danial <admin@redsparr0w.com>
2023-04-07 14:20:07 +1200

    Fixups

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,13 +1,3 @@
-import { afterAll, beforeAll } from 'vitest';
 import * as ko from 'knockout';
-
-beforeAll(() => {
-    window.ko = ko;
-});
-
-afterAll(() => {
-    delete window.ko;
-});
-
 
 window.ko = ko;
