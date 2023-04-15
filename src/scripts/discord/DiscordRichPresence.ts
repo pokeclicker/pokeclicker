8c573ef2c54eb9a91234807a3d66cc30ee50143b
umbralOptimatum <umbralOptimatum@users.noreply.github.com>
2023-04-14 20:11:24 -0400

    Only enable Discord RP once game has loaded
    Settings don't load on the start screen, so even if the player disables Discord Rich Presence, the desktop app will think it is enabled before the game loads and attempt to connect to Discord.

--- src/scripts/discord/DiscordRichPresence.ts
+++ src/scripts/discord/DiscordRichPresence.ts
@@ -234,7 +234,7 @@ class DiscordRichPresence {
         const nextArea = player?.route() ? Routes.getName(player.route(), player.region) : (player?.town() ? player.town().name : 'Unknown Area');
 
         const discordRPCValues: Record<string, any> = {
-            enabled: Settings.getSetting('discord-rp.enabled').observableValue(),
+            enabled: (player != undefined ? Settings.getSetting('discord-rp.enabled').observableValue() : false),
             line1: this.replaceDiscordText(Settings.getSetting('discord-rp.line-1').value || '  '),
             line2: this.replaceDiscordText(Settings.getSetting('discord-rp.line-2').value || '  '),
         };
