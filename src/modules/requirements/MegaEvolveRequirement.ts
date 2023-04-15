6b873b5aa30ab0fe2d9659b2de4fc90da31bf3ae
CypherX <cypherx00@gmail.com>
2023-04-15 16:41:11 -0400

    Update hints

--- src/modules/requirements/MegaEvolveRequirement.ts
+++ src/modules/requirements/MegaEvolveRequirement.ts
@@ -20,10 +20,15 @@ export default class MegaEvolveRequirement extends Requirement {
         const attackRequired = pokemonMap[this.name].attack * this.baseAttackMultiplier;
         if (this.getProgress()) {
             return 'Use a Key Stone to Mega Evolve.';
-        } else if (App.game.party.getPokemonByName(this.name).attack < attackRequired) {
-            return `Needs at least ${attackRequired.toLocaleString('en-US')} attack to Mega Evolve.`;
         } else {
-            return `${this.name} holds no Mega Stone.`;
+            const hints = [];
+            if (!player.hasMegaStone(this.megaStone)) {
+                hints.push(`${this.name} needs the ${MegaStoneType[this.megaStone]} Mega Stone.`);
+            }
+            if (App.game.party.getPokemonByName(this.name).attack < attackRequired) {
+                hints.push(`Needs at least ${attackRequired.toLocaleString('en-US')} attack to Mega Evolve.`);
+            }
+            return hints.join('<br />');
         }
     }
 }
