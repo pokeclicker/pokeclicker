f77bd44aa56f51827cfb4dfa175f77e3b13db28b
CypherX <cypherx00@gmail.com>
2023-05-07 16:14:23 -0400

    Revert "Safari map color overlay"
    This reverts commit 8374cedd5b69aa571d1d34bfc112b6816b513e4f.

--- src/scripts/safari/SafariTownContent.ts
+++ src/scripts/safari/SafariTownContent.ts
@@ -11,19 +11,4 @@ class SafariTownContent extends TownContent {
     public onclick(): void {
         Safari.openModal();
     }
-    public areaStatus(): areaStatus {
-        if (SafariPokemonList.list[this.parent.region]) {
-            const safariPokemon = SafariPokemonList.list[this.parent.region]()[0].safariPokemon.map(p => p.name);
-
-            if (!RouteHelper.listCompleted(safariPokemon, false)) {
-                return areaStatus.uncaughtPokemon;
-            } else if (!RouteHelper.listCompleted(safariPokemon, true)) {
-                return areaStatus.uncaughtShinyPokemon;
-            } else if (RouteHelper.minPokerus(safariPokemon) < GameConstants.Pokerus.Resistant) {
-                return areaStatus.missingResistant;
-            }
-        }
-
-        return areaStatus.completed;
-    }
 }
