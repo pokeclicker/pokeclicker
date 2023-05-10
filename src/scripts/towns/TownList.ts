9a0cd3c67cb04b75f6453ea0ad02d03eee8ed80b
osherwott <114451054+osherwott@users.noreply.github.com>
2023-05-09 21:50:10 -0500

    Move Ghetsis back to canon location

--- src/scripts/towns/TownList.ts
+++ src/scripts/towns/TownList.ts
@@ -4970,7 +4970,7 @@ TownList['Plasma Frigate'] = new DungeonTown(
             new QuestLineCompletedRequirement('Quest for the DNA Splicers'),
         ]),
     ],
-    [TemporaryBattleList['Colress 3'], TemporaryBattleList['Plasma Shadow 2'], TemporaryBattleList['Plasma Shadow 3'], TemporaryBattleList['Plasma Shadow 4'], TemporaryBattleList['Ghetsis 1'], TemporaryBattleList['Ghetsis 2']],
+    [TemporaryBattleList['Colress 3'], TemporaryBattleList['Plasma Shadow 2'], TemporaryBattleList['Plasma Shadow 3'], TemporaryBattleList['Plasma Shadow 4']],
     {
         npcs: [GiantChasmColress, GiantChasmShadowTriad],
     }
@@ -4982,7 +4982,8 @@ TownList['Giant Chasm'] = new DungeonTown(
     [
         new ClearDungeonRequirement(1, GameConstants.getDungeonIndex('Plasma Frigate')),
         new QuestLineStepCompletedRequirement('Quest for the DNA Splicers', 15),
-    ]
+    ],
+    [TemporaryBattleList['Ghetsis 1'], TemporaryBattleList['Ghetsis 2']]
 );
 TownList['Cave of Being'] = new DungeonTown(
     'Cave of Being',
