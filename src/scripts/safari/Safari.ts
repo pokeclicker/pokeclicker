e5b104aba45f49e57465759c00b03fe94c390bad
Jaaslet <jonas_bms@hotmail.com>
2023-05-11 07:40:09 +0200

    feature(safari): Safari items! (#3998)
    * Can now pick up safari item
    
    * Now shows notification on item gained
    
    * Items now shows up after battle ended
    
    * improved image
    
    * Less risky switch
    
    * Items can no longer spawn on top of pokemon
    
    * Refactor: find item after pickup
    
    * Added requirements

--- src/scripts/safari/Safari.ts
+++ src/scripts/safari/Safari.ts
@@ -1,6 +1,7 @@
 class Safari {
     static grid: Array<Array<number>>;
     static pokemonGrid: KnockoutObservableArray<SafariPokemon> = ko.observableArray([]);
+    static itemGrid: KnockoutObservableArray<SafariItem> = ko.observableArray([]);
     static player: Point = new Point(12, 20);
     static lastDirection = 'up';
     static nextDirection: string;
@@ -27,6 +28,7 @@ class Safari {
         this.activeRegion(player.region);
         Safari.grid = [];
         Safari.pokemonGrid([]);
+        Safari.itemGrid([]);
         Safari.playerXY.x = 0;
         Safari.playerXY.y = 0;
         Safari.lastDirection = 'up';
@@ -281,6 +283,7 @@ class Safari {
             Safari.playerXY.y = newPos.y;
             $('#sprite').animate(offset, 250, 'linear', () => {
                 Safari.checkBattle();
+                Safari.checkItem();
                 Safari.isMoving = false;
                 if (Safari.walking) {
                     if (!Safari.checkBattle() && Safari.queue[0]) {
@@ -316,6 +319,12 @@ class Safari {
         }
     }
 
+    public static spawnItemCheck() {
+        if (Rand.boolean()) {
+            this.spawnRandomItem();
+        }
+    }
+
     public static despawnPokemonCheck() {
         let index = this.pokemonGrid().length;
         while (index-- > 0) {
@@ -328,7 +337,10 @@ class Safari {
     private static spawnRandomPokemon() {
         const y = Rand.floor(this.sizeY());
         const x = Rand.floor(this.sizeX());
-        if (!this.canMove(x, y) || (x == this.playerXY.x && y == this.playerXY.y) || this.pokemonGrid().find(p => p.x === x && p.y === y)) {
+        if (!this.canMove(x, y) ||
+            (x == this.playerXY.x && y == this.playerXY.y) ||
+            this.pokemonGrid().find(p => p.x === x && p.y === y) ||
+            this.itemGrid().find(i => i.x === x && i.y === y)) {
             return;
         }
         const pokemon = SafariPokemon.random();
@@ -337,6 +349,20 @@ class Safari {
         pokemon.y = y;
         pokemon.steps = this.sizeX() + this.sizeY() + Rand.floor(21);
         this.pokemonGrid.push(pokemon);
+    }
+
+    private static spawnRandomItem() {
+        const x = Rand.floor(this.sizeX());
+        const y = Rand.floor(this.sizeY());
+        if (!this.canMove(x, y) || (x == this.playerXY.x && y == this.playerXY.y) ||
+            this.itemGrid().find(i => i.x === x && i.y === y) ||
+            this.pokemonGrid().find(p => p.x === x && p.y === y)) {
+            return;
+        }
+        if (!SafariItemController.currentRegionHasItems()) {
+            return;
+        }
+        this.itemGrid.push(new SafariItem(x, y));
     }
 
     private static directionToXY(dir: string) {
@@ -405,6 +431,22 @@ class Safari {
             }
         }
         return false;
+    }
+
+    private static checkItem() {
+        const itemOnPlayer = this.itemGrid().findIndex(p => p.x === Safari.playerXY.x && p.y === Safari.playerXY.y);
+        if (itemOnPlayer >= 0) {
+            const item = SafariItemController.getRandomItem();
+            const name = BagHandler.displayName(item);
+            BagHandler.gainItem(item);
+            Notifier.notify({
+                message: `You found ${GameHelper.anOrA(name)} ${name}!`,
+                image: BagHandler.image(item),
+                type: NotificationConstants.NotificationOption.success,
+                setting: NotificationConstants.NotificationSetting.Items.dropped_item,
+            });
+            Safari.itemGrid.splice(itemOnPlayer, 1);
+        }
     }
 
     private static calculateStartPokeballs() {
