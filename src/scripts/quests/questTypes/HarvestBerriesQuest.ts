a15c510bd6983e09497521a62bb7ef067ef0acca
eugene171 <109317224+Farboo171@users.noreply.github.com>
2023-05-28 19:14:28 -0400

    change(quest): Farm Quest Buffs to make them potentially worth doing (#4371)
    * QuestBuff
    
    * Update src/scripts/quests/questTypes/HarvestBerriesQuest.ts
    
    Co-authored-by: CypherX <cypherx00@gmail.com>
    
    ---------
    
    Co-authored-by: CypherX <cypherx00@gmail.com>
    Co-authored-by: Jessica Chen <jessica.scoltock@protonmail.com>

--- src/scripts/quests/questTypes/HarvestBerriesQuest.ts
+++ src/scripts/quests/questTypes/HarvestBerriesQuest.ts
@@ -16,7 +16,7 @@ class HarvestBerriesQuest extends Quest implements QuestInterface {
 
     public static generateData(): any[] {
         // Getting available Berries (always include Gen 1 Berries)
-        const availableBerries = App.game.farming.berryData.filter(berry => App.game.farming.unlockedBerries[berry.type]() || berry.type < BerryType.Persim);
+        const availableBerries = App.game.farming.berryData.filter(berry => (App.game.farming.unlockedBerries[berry.type]() && berry.growthTime[3] < 12000) || berry.type < BerryType.Persim);
         const berry = SeededRand.fromArray(availableBerries);
 
         const maxAmt = Math.min(300, Math.ceil(432000 / berry.growthTime[3]));
@@ -29,10 +29,10 @@ class HarvestBerriesQuest extends Quest implements QuestInterface {
 
     private static calcReward(amount: number, berryType: BerryType): number {
         const harvestTime = App.game.farming.berryData[berryType].growthTime[3];
-        const harvestAmt = Math.max(4, Math.ceil(App.game.farming.berryData[berryType].harvestAmount));
+        const harvestAmt = Math.max(4, Math.ceil(App.game.farming.berryData[berryType].harvestAmount / 2));
         const plantAmt = amount / harvestAmt;
         const fieldAmt = plantAmt / App.game.farming.plotList.length;
-        const reward = Math.ceil(fieldAmt * Math.pow(harvestTime, .7) * 10);
+        const reward = Math.ceil(fieldAmt * Math.pow(harvestTime, .7) * 30);
         return super.randomizeReward(reward);
     }
 
