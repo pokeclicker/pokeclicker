78216343a03824c2467ec2c033b0724240da1e27
eugene171 <109317224+Farboo171@users.noreply.github.com>
2023-05-29 13:48:37 -0400

    new: Safari Levels (#4377)
    * Safari Level
    
    * lint
    
    * Ball Bait XP Swap

--- src/scripts/safari/Safari.ts
+++ src/scripts/safari/Safari.ts
@@ -16,6 +16,28 @@ class Safari {
     static balls: KnockoutObservable<number> = ko.observable().extend({ numeric: 0 });
     static activeRegion: KnockoutObservable<GameConstants.Region> = ko.observable(GameConstants.Region.none);
 
+    // Safari level
+    static maxSafariLevel = 40;
+    static safariExp: KnockoutComputed<number> = ko.pureComputed(() => {
+        return App.game.statistics.safariRocksThrown() * 10 +
+            App.game.statistics.safariBaitThrown() * 5 +
+            App.game.statistics.safariBallsThrown() * 10 +
+            App.game.statistics.safariPokemonCaptured() * 50;
+    });
+    static safariLevel: KnockoutComputed<number> = ko.pureComputed(() => {
+        for (let i = 1; i <= Safari.maxSafariLevel; i++) {
+            if (Safari.safariExp() < Safari.expRequiredForLevel(i)) {
+                return i - 1;
+            }
+        }
+        return Safari.maxSafariLevel;
+    });
+    static percentToNextSafariLevel: KnockoutComputed<number> = ko.pureComputed(() => {
+        const expForNextLevel = Safari.expRequiredForLevel(Safari.safariLevel() + 1) - Safari.expRequiredForLevel(Safari.safariLevel());
+        const expThisLevel = Safari.safariExp() - Safari.expRequiredForLevel(Safari.safariLevel());
+        return expThisLevel / expForNextLevel * 100;
+    });
+
     public static sizeX(): number {
         return Math.floor(document.querySelector('#safariModal .modal-dialog').scrollWidth / 32);
     }
@@ -463,6 +485,10 @@ class Safari {
             });
         }
         return false;
+    }
+
+    private static expRequiredForLevel(level: number) {
+        return Math.ceil(2000 * (1.2 ** (level - 1) - 1));
     }
 }
 
