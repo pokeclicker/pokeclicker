a3e563c03c881418dc229b682bea6ecb60713c31
DraKay <114853432+DraKay@users.noreply.github.com>
2023-05-30 22:52:59 +0200

    Update PokeballItem.ts

--- src/modules/items/PokeballItem.ts
+++ src/modules/items/PokeballItem.ts
@@ -6,15 +6,12 @@ export default class PokeballItem extends Item {
     type: PokeballType;
 
     constructor(type: PokeballType, basePrice: number, currency: Currency = Currency.money, options?: ShopOptions, displayName?: string) {
-        super(PokeballType[type], basePrice, currency, options, displayName, undefined, 'pokeball');
+        super(PokeballType[type], basePrice, currency, options, displayName, 'A ball for catching Pokémon.', undefined, 'pokeball');
         this.type = type;
     }
 
     gain(amt: number) {
         App.game.pokeballs.gainPokeballs(this.type, amt);
     }
-    
-    get description(): string {
-        return this._description || `A ball for catch Pokémon. Can be bought in shops or find in dungeons.`;
-    }
+
 }
