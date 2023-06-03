2693609dfe9f2b7b0d78bb07107522a4fc50cfc8
EatPant2nd <106291026+EatPant2nd@users.noreply.github.com>
2023-06-03 19:12:53 -0400

    Added optional description to Berry Quests
    Added optional custom description to Berry Quests.

--- src/scripts/quests/questTypes/HarvestBerriesQuest.ts
+++ src/scripts/quests/questTypes/HarvestBerriesQuest.ts
@@ -4,9 +4,10 @@ class HarvestBerriesQuest extends Quest implements QuestInterface {
 
     private berryType: BerryType;
 
-    constructor(amount: number, reward: number, berryType: BerryType) {
+    constructor(amount: number, reward: number, berryType: BerryType, description: string = undefined) {
         super(amount, reward);
         this.berryType = berryType;
+        this.customDescription = description;
         this.focus = App.game.statistics.berriesHarvested[this.berryType];
     }
 
@@ -37,6 +38,9 @@ class HarvestBerriesQuest extends Quest implements QuestInterface {
     }
 
     get description(): string {
+        if (this.customDescription) {
+            return this.customDescription;
+        }
         return `Harvest ${this.amount.toLocaleString('en-US')} ${BerryType[this.berryType]} ${GameConstants.pluralizeString('Berry', this.amount)} at the farm.`;
     }
 
