ad0e2ee9f44e6a07fc262a030f8150a39590e484
Jaaslet <jonas_bms@hotmail.com>
2023-06-18 11:38:06 +0200

    Added support for routes and temp battles

--- src/scripts/ExternalHelper.ts
+++ src/scripts/ExternalHelper.ts
@@ -1,8 +1,10 @@
 class ExternalHelper {
     private static townCache: {[name: string] : boolean} = {};
     private static questlineCache: {[name: string] : boolean} = {};
+    private static temporaryBattleCache: {[name: string] : boolean} = {};
+    private static routeCache: {[name: string] : boolean} = {};
 
-    public static isInLiveVersion(content: Town | QuestLine) {
+    public static isInLiveVersion(content: Town | QuestLine | TemporaryBattle | RegionRoute) {
         if (content instanceof Town) {
             if (content.region > GameConstants.MAX_AVAILABLE_REGION) {
                 return false;
@@ -17,6 +19,21 @@ class ExternalHelper {
                 ExternalHelper.questlineCache[content.name] = ExternalHelper.containsDevRequirement(content.requirement);
             }
             return !ExternalHelper.questlineCache[content.name];
+        }
+        if (content instanceof TemporaryBattle) {
+            if (ExternalHelper.temporaryBattleCache[content.name] == undefined) {
+                ExternalHelper.temporaryBattleCache[content.name] = ExternalHelper.containsDevRequirement(content.requirements);
+            }
+            return !ExternalHelper.temporaryBattleCache[content.name];
+        }
+        if (content instanceof RegionRoute) {
+            if (content.region > GameConstants.MAX_AVAILABLE_REGION) {
+                return false;
+            }
+            if (ExternalHelper.routeCache[content.routeName] == undefined) {
+                ExternalHelper.routeCache[content.routeName] = ExternalHelper.containsDevRequirement(content.requirements);
+            }
+            return !ExternalHelper.routeCache[content.routeName];
         }
         return true;
     }
@@ -39,7 +56,12 @@ class ExternalHelper {
                 containsDevRequirement = ExternalHelper.isInLiveVersion(r.cachedQuest);
             } else if (r instanceof QuestLineStepCompletedRequirement) {
                 containsDevRequirement = ExternalHelper.isInLiveVersion(r.cachedQuest);
+            } else if (r instanceof TemporaryBattleRequirement) {
+                containsDevRequirement = ExternalHelper.isInLiveVersion(TemporaryBattleList[r.battleName]);
+            } else if (r instanceof RouteKillRequirement) {
+                containsDevRequirement = ExternalHelper.isInLiveVersion(Routes.getRoute(r.region, r.route));
             }
+
             if (containsDevRequirement) {
                 return false;
             }
