20346eff54c73801ea4e372ccd69239522f66915
CypherX <cypherx00@gmail.com>
2023-07-06 09:55:42 -0400

    SafariEncounter, fix rotation timezone issues

--- src/scripts/safari/SafariPokemonList.ts
+++ src/scripts/safari/SafariPokemonList.ts
@@ -1,10 +1,17 @@
-type SafariType = {
-    name: PokemonNameType,
-    weight: number
+class SafariEncounter {
+    constructor(
+        public name: PokemonNameType,
+        public weight: number,
+        private requireCaught = false
+    ) {}
+
+    public isAvailable(): boolean {
+        return !this.requireCaught ? true : App.game.party.alreadyCaughtPokemonByName(this.name);
+    }
 }
 
 class SafariPokemonList {
-    public static list: Record<GameConstants.Region, KnockoutObservable<Array<SafariType>>> = {
+    public static list: Record<GameConstants.Region, KnockoutObservable<Array<SafariEncounter>>> = {
         [GameConstants.Region.kanto]: ko.observableArray(),
         [GameConstants.Region.kalos]: ko.observableArray(),
     };
@@ -16,23 +23,23 @@ class SafariPokemonList {
 
     private static generateKantoSafariList() {
         // Lower weighted pokemon will appear less frequently, equally weighted are equally likely to appear
-        const pokemon : SafariType[] = [
-            {name: 'Nidoran(F)', weight: 15},
-            {name: 'Nidorina', weight: 10 },
-            {name: 'Nidoran(M)', weight: 25 },
-            {name: 'Nidorino', weight: 10 },
-            {name: 'Exeggcute', weight: 20 },
-            {name: 'Paras', weight: 5 },
-            {name: 'Parasect', weight: 15 },
-            {name: 'Rhyhorn', weight: 10 },
-            {name: 'Chansey', weight: 4 },
-            {name: 'Scyther', weight: 4 },
-            {name: 'Pinsir', weight: 4 },
-            {name: 'Kangaskhan', weight: 15 },
-            {name: 'Tauros', weight: 10 },
-            {name: 'Cubone', weight: 10 },
-            {name: 'Marowak', weight: 5 },
-            {name: 'Tangela', weight: 4 },
+        const pokemon : SafariEncounter[] = [
+            new SafariEncounter('Nidoran(F)', 15),
+            new SafariEncounter('Nidorina', 10),
+            new SafariEncounter('Nidoran(M)', 25),
+            new SafariEncounter('Nidorino', 10),
+            new SafariEncounter('Exeggcute', 20),
+            new SafariEncounter('Paras', 5),
+            new SafariEncounter('Parasect', 15),
+            new SafariEncounter('Rhyhorn', 10),
+            new SafariEncounter('Chansey', 4),
+            new SafariEncounter('Scyther', 4),
+            new SafariEncounter('Pinsir', 4),
+            new SafariEncounter('Kangaskhan', 15),
+            new SafariEncounter('Tauros', 10),
+            new SafariEncounter('Cubone', 10),
+            new SafariEncounter('Marowak', 5),
+            new SafariEncounter('Tangela', 4),
         ];
 
         SafariPokemonList.list[GameConstants.Region.kanto](pokemon);
@@ -45,23 +52,24 @@ class SafariPokemonList {
                 && PokemonHelper.calcNativeRegion(p.name) <= GameConstants.MAX_AVAILABLE_REGION));
 
         const batchCount = Math.ceil(shuffledPokemon.length / GameConstants.FRIEND_SAFARI_POKEMON);
-        const startIndex = (Math.floor((new Date()).getTime() / (24 * 60 * 60 * 1000)) % batchCount) * GameConstants.FRIEND_SAFARI_POKEMON;
+        const now = new Date();
+        const startIndex = (Math.floor((now.getTime() - now.getTimezoneOffset() * 60 * 1000) / (24 * 60 * 60 * 1000)) % batchCount) * GameConstants.FRIEND_SAFARI_POKEMON;
         const endIndex = startIndex + GameConstants.FRIEND_SAFARI_POKEMON;
 
-        const pokemon: SafariType[] = shuffledPokemon.slice(startIndex, endIndex).map((p) => {
-            return { name: p.name, weight: 10, locked: !App.game.party.alreadyCaughtPokemonByName(p.name) };
+        const pokemon: SafariEncounter[] = shuffledPokemon.slice(startIndex, endIndex).map((p) => {
+            return new SafariEncounter(p.name, 10, true);
         });
 
-        pokemon.push({ name: 'Shuckle', weight: 2 });
-        pokemon.push({ name: 'Stunfisk', weight: 2 });
-        pokemon.push({ name: 'Magmar', weight: 2 });
-        pokemon.push({ name: 'Maractus', weight: 2 });
-        pokemon.push({ name: 'Klefki', weight: 2 });
-        pokemon.push({ name: 'Breloom', weight: 2 });
-        pokemon.push({ name: 'Woobat', weight: 2 });
-        pokemon.push({ name: 'Golurk', weight: 2 });
-        pokemon.push({ name: 'Marowak', weight: 2 });
-        pokemon.push({ name: 'Lapras', weight: 2 });
+        pokemon.push(new SafariEncounter('Shuckle', 2));
+        pokemon.push(new SafariEncounter('Stunfisk', 2));
+        pokemon.push(new SafariEncounter('Magmar', 2));
+        pokemon.push(new SafariEncounter('Maractus', 2));
+        pokemon.push(new SafariEncounter('Klefki', 2));
+        pokemon.push(new SafariEncounter('Breloom', 2));
+        pokemon.push(new SafariEncounter('Woobat', 2));
+        pokemon.push(new SafariEncounter('Golurk', 2));
+        pokemon.push(new SafariEncounter('Marowak', 2));
+        pokemon.push(new SafariEncounter('Lapras', 2));
 
         SafariPokemonList.list[GameConstants.Region.kalos](pokemon);
     }
