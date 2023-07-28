78a12d7bb335f3e0674ed3cde65bf523883d2b81
osherwott <114451054+osherwott@users.noreply.github.com>
2023-07-28 00:26:11 -0500

    wip(orre): Orre organization (#4585)
    * peons update
    
    * update and condense credits
    
    * Update and correct XD peons and names
    
    * change xd placeholders
    
    changed exol to (commander)
    added placeholder (hexagon)
    added placeholder (yellow)
    
    * Spread out Orre route unlocks
    
    Now unlock at the same time as towns near them to instill a sense of progress
    
    * more names
    
    * special Colosseum peons are Miror B Peons
    * fixed temp battle image name
    
    * trainer name
    
    * Update QuestLineHelper.ts
    
    * Update QuestLineHelper.ts
    
    * Change names
    
    *XD is now hidden in female and male Peons
    *XD removed form all other Peons
    
    * Update TemporaryBattleList.ts
    
    ---------
    
    Co-authored-by: Jaaslet <jonas_bms@hotmail.com>

--- src/modules/routes/RouteData.ts
+++ src/modules/routes/RouteData.ts
@@ -1036,7 +1036,7 @@ Routes.add(new RegionRoute(
     new RoutePokemon({
         land: ['Sandshrew', 'Gligar', 'Trapinch'],
     }),
-    [new DevelopmentRequirement(), new QuestLineCompletedRequirement('Shadows in the Desert')],
+    [new DevelopmentRequirement(), new QuestLineStepCompletedRequirement('Shadows in the Desert', 1)],
     undefined,
     HoennSubRegions.Orre,
     true,
@@ -1047,7 +1047,7 @@ Routes.add(new RegionRoute(
     new RoutePokemon({
         land: ['Hoppip', 'Phanpy', 'Surskit'],
     }),
-    [new DevelopmentRequirement(), new QuestLineCompletedRequirement('Shadows in the Desert')],
+    [new DevelopmentRequirement(), new QuestLineStepCompletedRequirement('Shadows in the Desert', 14)],
     undefined,
     HoennSubRegions.Orre,
     true,
@@ -1058,7 +1058,7 @@ Routes.add(new RegionRoute(
     new RoutePokemon({
         land: ['Zubat', 'Aron', 'Wooper'],
     }),
-    [new DevelopmentRequirement(), new QuestLineCompletedRequirement('Shadows in the Desert')],
+    [new DevelopmentRequirement(), new QuestLineStepCompletedRequirement('Shadows in the Desert', 17)],
     undefined,
     HoennSubRegions.Orre,
     true,
