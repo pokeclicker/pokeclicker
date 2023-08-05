cbd26bb458b57316dd16dbb3e9b347564c8ca1e8
eugene171 <109317224+Farboo171@users.noreply.github.com>
2023-08-05 15:47:19 -0400

    Adding images and an easter egg

--- src/scripts/towns/TownList.ts
+++ src/scripts/towns/TownList.ts
@@ -4128,6 +4128,13 @@ const SnoverBreeder = new NPC('Snover Breeder', [
     'I\'ve been finding ways to grow Snover Berries using humane methods',
     'Snover are happy to let you pick their berries, then they wander away into the wilderness. Just be kind to them.',
 ], {image: 'assets/images/npcs/Pokémon Breeder (female).png'});
+const GrotleAcornParty = new NPC('Grotle and Friends', [
+    '<i>Several friendly Pokémon are snacking on a Grotle\'s acorns.</i>',
+], {
+    image: 'assets/images/npcs/specialNPCs/Grotle Acorn Party.png',
+    requirement: new ObtainedPokemonRequirement('Grotle (Acorn)'),
+});
+
 
 //Sinnoh Towns
 TownList['Twinleaf Town'] = new Town(
@@ -4290,7 +4297,7 @@ TownList['Secret Berry Shop'] = new Town(
         requirements: [
             new RouteKillRequirement(10, GameConstants.Region.sinnoh, 217),
         ],
-        npcs: [SnoverBreeder],
+        npcs: [SnoverBreeder, GrotleAcornParty],
         ignoreAreaStatus: true,
     }
 );
