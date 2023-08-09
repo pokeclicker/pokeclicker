04ce775387020f932034f3aca757dc641f3a320f
Nydaleclya <hmend@outlook.de>
2023-08-09 20:47:46 +0200

    DefeatTemporaryBattleQuest

---
+++ src/scripts/quests/questTypes/DefeatTemporaryBattleQuest.ts
@@ -0,0 +1,18 @@
+class DefeatTemporaryBattleQuest extends Quest implements QuestInterface {
+    customReward?: () => void;
+
+    constructor(public temporaryBattle: string, public customDescription: string, customReward?: (() => void)) {
+        super(1, 0);
+        this.focus = App.game.statistics.temporaryBattleDefeated[GameConstants.getTemporaryBattlesIndex(this.temporaryBattle)];
+        this.customDescription = customDescription;
+        this.customReward = typeof customReward == 'function' ? customReward : undefined;
+    }
+
+    claim(): boolean {
+        if (this.customReward !== undefined) {
+            this.customReward();
+        }
+        return super.claim();
+    }
+
+}
