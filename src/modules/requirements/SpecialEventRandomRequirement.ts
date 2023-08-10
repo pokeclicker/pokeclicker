2dd70e4d553412ff0823b31e5491c5667035c3bb
CypherX <cypherx00@gmail.com>
2023-08-09 22:06:56 -0400

    Halloween refactor

---
+++ src/modules/requirements/SpecialEventRandomRequirement.ts
@@ -0,0 +1,15 @@
+import SeededRand from '../utilities/SeededRand';
+import SpecialEventRequirement from './SpecialEventRequirement';
+
+export default class SpecialEventRandomRequirement extends SpecialEventRequirement {
+    private isAvailable = false;
+
+    constructor(specialEventName: string) {
+        super(specialEventName);
+        this.isAvailable = SeededRand.boolean();
+    }
+
+    public getProgress(): number {
+        return +(this.isAvailable && super.getProgress());
+    }
+}
