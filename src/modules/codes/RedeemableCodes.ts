240e257d3fa27fcf2e1d9faa55db9de52ea712eb
eugene171 <109317224+Farboo171@users.noreply.github.com>
2023-09-04 07:20:53 -0400

    Few Coin

--- src/modules/codes/RedeemableCodes.ts
+++ src/modules/codes/RedeemableCodes.ts
@@ -201,7 +201,7 @@ export default class RedeemableCodes implements Saveable {
                 while (quest.curQuest() < quest.totalQuests) {
                     quest.curQuestObject().complete();
                 }
-                App.game.wallet.gainDungeonTokens(2000);
+                App.game.wallet.gainDungeonTokens(200);
                 Notifier.notify({
                     title: 'Tutorial Skip',
                     message: 'You have skipped the tutorial, and found a stash of Dungeon Tokens.',
