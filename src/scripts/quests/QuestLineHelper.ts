1a630ed630ef2297124c61eeb400d2a7c84de58d
RedzepR <143606926+RedzepR@users.noreply.github.com>
2023-09-09 15:43:51 +0200

    change(quest): Diancie Quest changes (#4769)
    * - Removed the duoblade mon requirement
    - changed the wording of the quest steps around Blade Aegislash so they talk about the Steels' aegislash
    - Remove the reward from the quest step and make it a gift NPC
    - bugfix the fairy gem step of the diancie quest
    
    * Fixed Cipher Peon Naps Dungeontrainer
    
    * - Cleaned up some leftover code
    
    * TownList run fix
    
    * Removed unintended Visual Studio formatting
    
    * Changed GiftNPC Aegislashs name
    
    * requirement change
    
    ---------
    
    Co-authored-by: eugene171 <109317224+Farboo171@users.noreply.github.com>

--- src/scripts/quests/QuestLineHelper.ts
+++ src/scripts/quests/QuestLineHelper.ts
@@ -2271,7 +2271,7 @@ class QuestLineHelper {
 
     // Available post-E4, must have captured Doublade
     public static createPrincessDiancieQuestLine() {
-        const princessDiancieQuestLine = new QuestLine('Princess Diancie', 'Princess Diancie has been spotted in Kalos! She\'s searching for something.', new MultiRequirement([new ObtainedPokemonRequirement('Doublade'), new GymBadgeRequirement(BadgeEnums.Elite_Malva), new GymBadgeRequirement(BadgeEnums.Elite_Siebold), new GymBadgeRequirement(BadgeEnums.Elite_Wikstrom), new GymBadgeRequirement(BadgeEnums.Elite_Drasna)]) , GameConstants.BulletinBoards.Kalos);
+        const princessDiancieQuestLine = new QuestLine('Princess Diancie', 'Princess Diancie has been spotted in Kalos! She\'s searching for something.', new MultiRequirement([new GymBadgeRequirement(BadgeEnums.Elite_Malva), new GymBadgeRequirement(BadgeEnums.Elite_Siebold), new GymBadgeRequirement(BadgeEnums.Elite_Wikstrom), new GymBadgeRequirement(BadgeEnums.Elite_Drasna)]) , GameConstants.BulletinBoards.Kalos);
 
         const catchFairy = new CustomQuest(100, undefined, 'Capture or hatch 100 Fairy-type Pokémon to follow Diancie\'s Fairy Aura.', () => {
             return pokemonMap.filter(p => p.type.includes(PokemonType.Fairy)).map(p => App.game.statistics.pokemonCaptured[p.id]()).reduce((a,b) => a + b, 0);
@@ -2290,20 +2290,12 @@ class QuestLineHelper {
         const fightSteels = new DefeatTemporaryBattleQuest('Millis and Argus Steel', 'Millis and Argus Steel let you do the hard work while they set up an ambush in Shalour City.');
         princessDiancieQuestLine.addQuest(fightSteels);
 
-        const BladeAegislashReward = () => {
-            App.game.party.gainPokemonByName('Aegislash (Blade)');
-            Notifier.notify({
-                title: princessDiancieQuestLine.name,
-                message: 'Your Doublade has evolved into Blade Forme Aegislash!',
-                type: NotificationConstants.NotificationOption.success,
-                timeout: 3e4,
-            });
-        };
-
-        const bladeForme = new TalkToNPCQuest(ExamineAegislash, 'Your Doublade learned something from the Steels, examine it in Shalour City to find out what!', BladeAegislashReward);
+        const bladeForme = new TalkToNPCQuest(ExamineAegislash, 'Millis and Argus Steel fled but their Aegislash stayed behind. Look for it in Shalour City and find out what it wants!');
         princessDiancieQuestLine.addQuest(bladeForme);
 
-        const heartDiamond = new CustomQuest(1000, undefined, 'Diancie needs help building a Heart Diamond to stabilize the Diamond Domain. Gather some Fairy Gems for her.', App.game.statistics.gemsGained[17], 0, () => ItemList.Heart_Diamond_Diancie.gain(1));
+        const heartDiamond = new GainGemsQuest(1000, 0, PokemonType.Fairy);
+        heartDiamond.customDescription = 'Diancie needs help building a Heart Diamond to stabilize the Diamond Domain. Gather some Fairy Gems for her.';
+        heartDiamond.onLoad = () => ItemList.Heart_Diamond_Diancie.gain(1);
         princessDiancieQuestLine.addQuest(heartDiamond);
 
         const thanksDiancie = new TalkToNPCQuest(ThanksDiancie, 'Talk to Princess Diancie in Shalour City.');
