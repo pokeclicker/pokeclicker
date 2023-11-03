0e486d3aa58184016a3f596477889b821441a24e
EatPant2nd <106291026+EatPant2nd@users.noreply.github.com>
2023-11-03 13:10:05 -0300

    Added Galar Champion Requirement to Secrets of the Jungle
    Added Galar Champion requirement to Secrets of the Jungle Quest Line since it was showing as available as early as Alola where it was impossible to go accept the quest as it's located in one of Galar's DLC areas.

--- src/scripts/quests/QuestLineHelper.ts
+++ src/scripts/quests/QuestLineHelper.ts
@@ -3372,7 +3372,7 @@ class QuestLineHelper {
 
     // Available after defeating Ash Ketchum Alola
     public static createJungleSecretsQuestLine() {
-        const jungleSecretsQuestLine = new QuestLine('Secrets of the Jungle', 'Discover the secrets of the jungle.', new TemporaryBattleRequirement('Ash Ketchum Alola'), GameConstants.BulletinBoards.Armor);
+        const jungleSecretsQuestLine = new QuestLine('Secrets of the Jungle', 'Discover the secrets of the jungle.', new MultiRequirement([new GymBadgeRequirement(BadgeEnums.Elite_GalarChampion), new TemporaryBattleRequirement('Ash Ketchum Alola')]), GameConstants.BulletinBoards.Armor);
 
         const talktoJungleAsh1 = new TalkToNPCQuest(JungleAsh1, 'Ash Ketchum wants to talk to you at the Master Dojo.');
         jungleSecretsQuestLine.addQuest(talktoJungleAsh1);
