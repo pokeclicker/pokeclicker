545b41355d08eac7d541e923ff8f506ce36fb5c6
Qwertypop04 <63805905+Qwertypop04@users.noreply.github.com>
2023-12-01 02:05:50 +0000

    Update TemporaryBattleList.ts

--- src/scripts/temporaryBattle/TemporaryBattleList.ts
+++ src/scripts/temporaryBattle/TemporaryBattleList.ts
@@ -2264,12 +2264,12 @@ TemporaryBattleList['Red Genesect 2'] = new TemporaryBattle(
     'Red Genesect 2',
     [new GymPokemon('Genesect (High-Speed)', 182858488, 80, undefined, true)],
     undefined,
-    [new QuestLineStepCompletedRequirement('The Legend Awakened', 6)],
+    [new QuestLineStepCompletedRequirement('The Legend Awakened', 5)],
     undefined,
     {
         displayName: 'Red Genesect',
         hideTrainer: true,
-        visibleRequirement: new QuestLineStepCompletedRequirement('The Legend Awakened', 6),
+        visibleRequirement: new QuestLineStepCompletedRequirement('The Legend Awakened', 5),
     }
 );
 TemporaryBattleList.DreamResearcher = new TemporaryBattle(
