ba93a15b50c7b90eca6a2de76e5e0d3678f3c7d6
Sean Davis <wormania@gmail.com>
2023-12-28 23:50:53 +0000

    Change SeededRand to use a hybrid of xorshift and MCG

--- src/modules/utilities/SeededDateRand.ts
+++ src/modules/utilities/SeededDateRand.ts
@@ -2,7 +2,7 @@ import SeededRand from './SeededRand';
 
 export default class SeededDateRand extends SeededRand {
     public static next(): number {
-        return this.state / this.MOD;
+        return this.state / this.MAX_UINT_32;
     }
 
     public static seedWithDate(d: Date): void {
