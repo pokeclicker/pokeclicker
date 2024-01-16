1443045b6f59e89489d45519d0d3ec97b7cde2ea
CypherX <cypherx00@gmail.com>
2024-01-16 17:47:45 -0500

    Better implementation, no bootstrap events

--- src/modules/settings/index.ts
+++ src/modules/settings/index.ts
@@ -429,5 +429,5 @@ ModalCollapseList.forEach((collapse) => {
 });
 
 // Resizable modules
-Settings.add(new Setting<number>('moduleHeight.pokeballSelector', '', [], 310));
-Settings.add(new Setting<number>('moduleHeight.pokemonListContainer', '', [], 410));
+Settings.add(new Setting<number>('moduleHeight.pokeballSelectorBody', '', [], 265));
+Settings.add(new Setting<number>('moduleHeight.pokemonListBody', '', [], 365));
