c37b291a93a9b527eb255a978c5abdc6b7e412ea
RedSparr0w <RedSparr0w@users.noreply.github.com>
2024-02-07 17:40:56 +1300

    chore(publish): Try not overwrite PR previews

--- publish.js
+++ publish.js
@@ -6,6 +6,7 @@ console.info('Starting publish..');
 
 ghpages.publish('docs', {
     src: ['./**/*', '../package.json'],
+    remove: '!./preview/**/*',
     branch: 'master',
     dest: 'docs',
     message: `[v${version}] Live website`,
