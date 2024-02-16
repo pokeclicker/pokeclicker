a0415e238d48d621ab437bc938d089679864a595
umbralOptimatum <umbralOptimatum@users.noreply.github.com>
2024-02-15 22:52:36 -0500

    Move remaining PokemonHelper method to module

--- src/scripts/pokemons/PokemonHelper.ts
+++ src/scripts/pokemons/PokemonHelper.ts
@@ -1,7 +1,3 @@
-///<reference path="../GameConstants.d.ts"/>
-
 class PokemonHelper extends TmpPokemonHelper {
-    public static isMegaEvolution(pokemonName: PokemonNameType): boolean {
-        return PokemonHelper.getPokemonPrevolution(pokemonName).some((e) => e.evolvedPokemon == pokemonName && e.restrictions.some((r) => r instanceof MegaEvolveRequirement));
-    }
+
 }
