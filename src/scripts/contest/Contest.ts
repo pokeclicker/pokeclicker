2c1286349923c70d5caac463a90f4ca4ea9d6a46
osherwott <114451054+osherwott@users.noreply.github.com>
2024-04-13 14:27:04 -0500

    start removing Contest.ts

--- src/scripts/contest/Contest.ts
+++ src/scripts/contest/Contest.ts
@@ -9,12 +9,10 @@ class Contest {
     constructor(
         public rank: ContestRank,
         public contestType: ContestType,
-        public moneyReward: number = 0,
         public pokemons: PartyPokemon[] = App.game.party.caughtPokemon.filter((p) => {
             const pk = PokemonHelper.getPokemonById(p.id);
             return [pk.contestType1, pk.contestType2, pk.contestType3].find(c => c === contestType || c === ContestType.Balanced) !== -1;
         }),
-        public rewardFunction = () => {}, // TODO: ribbons
         public trainers: ContestTrainer[] = ContestOpponents[rank]
     ) {
     }
