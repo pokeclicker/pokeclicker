pokeclicker/pokeclicker

commit 732b8354df3041017aa70a0163389818ae641be6

change(safari): Improved Safari Pokémon List and its display (#5157)
* Safari List

* Female

---------

Co-authored-by: CypherX <cypherx00@gmail.com>

src/components/townView.html
@@ -182,5 +182,34 @@
         </ul>
       </div>
     </knockout>
+    <knockout data-bind="if: player.town().content.find(c => c instanceof SafariTownContent)">
+        <div id="dungeonPokemonList" class="card pt-2 pb-3 px-1">
+          <!--Display all available Pokémon in this Safari-->
+          <ul class="list-inline mb-0">
+            <!-- ko foreach: SafariPokemonList.getDisplayList() -->
+            <li class="list-inline-item">
+              <img class="dungeon-pokemon-pkrs" src="" data-bind="
+                  attr:{ src: $data.pkrsImage },
+                  tooltip: {
+                      title: $data.EVs,
+                      placement: 'bottom',
+                      trigger: 'hover'
+                  }" />
+              <img class="dungeon-pokemon-preview" src="" data-bind="attr:{ src: $data.image },
+                  css: { 'dungeon-pokemon-locked': $data.uncaught }" onerror="this.src='assets/images/npcs/specialNPCs/Mysterious Trainer.png';"/>
+              <sup class="shiny" data-bind="visible: $data.shiny">✨</sup>
+              <img class="lock" src="assets/images/breeding/lock.svg" data-bind="
+                  hidden: !$data.lock,
+                  tooltip: {
+                      title: $data.lockMessage,
+                      placement: 'top',
+                      trigger: 'hover',
+                      boundary: 'window'
+                  }" />
+            </li>
+            <!-- /ko -->
+          </ul>
+        </div>
+      </knockout>
 </div>
 </knockout>

src/scripts/safari/SafariPokemonList.ts
@@ -1,14 +1,17 @@
 class SafariEncounter {
+    public requirement: Requirement;
     constructor(
         public name: PokemonNameType,
         public weight: number,
         public environments: SafariEnvironments[] = [SafariEnvironments.Grass],
-        private requireCaught = false,
-        public showUncaught = false // Whether the pokemon silhouette will be shown in the ranger NPC when uncaught
-    ) {}
+        requirement?: true | Requirement, // True is used to simplify Friend Safari Pokémon generation
+        public hide = true // Hide from the list
+    ) {
+        this.requirement = requirement === true ? new ObtainedPokemonRequirement(this.name) : requirement;
+    }
 
     public isAvailable(): boolean {
-        return !this.requireCaught ? true : App.game.party.alreadyCaughtPokemonByName(this.name);
+        return this.requirement?.isCompleted() ?? true;
     }
 }
 
@@ -54,8 +57,8 @@ class SafariPokemonList {
             new SafariEncounter('Poliwag', 15, [SafariEnvironments.Water]),
             new SafariEncounter('Goldeen', 15, [SafariEnvironments.Water]),
             new SafariEncounter('Seaking', 5, [SafariEnvironments.Water]),
-            new SafariEncounter('Dratini', 10, [SafariEnvironments.Water], true),
-            new SafariEncounter('Dragonair', 4, [SafariEnvironments.Water], true),
+            new SafariEncounter('Dratini', 10, [SafariEnvironments.Water], true, false),
+            new SafariEncounter('Dragonair', 4, [SafariEnvironments.Water], true, false),
         ];
 
         SafariPokemonList.list[GameConstants.Region.kanto](pokemon);
@@ -177,7 +180,7 @@ class SafariPokemonList {
         const endIndex = startIndex + GameConstants.FRIEND_SAFARI_POKEMON;
 
         const pokemon: SafariEncounter[] = shuffledPokemon.slice(startIndex, endIndex).map((p) => {
-            return new SafariEncounter(p, 10, SafariPokemonList.getEnvironmentByPokemonType(p), true, true);
+            return new SafariEncounter(p, 10, SafariPokemonList.getEnvironmentByPokemonType(p), true, false);
         });
 
         pokemon.push(new SafariEncounter('Shuckle', 2));
@@ -211,5 +214,35 @@ class SafariPokemonList {
             safariEnvironments.push(SafariEnvironments.Grass);
         }
         return safariEnvironments;
+    }
+
+    public static getDisplayList(region = player.region): EncounterInfo[] {
+        const encounters = [];
+
+        if (!SafariPokemonList.list[region]) {
+            return encounters;
+        }
+
+        const list = SafariPokemonList.list[region]();
+        list.forEach(e => {
+            if (e.hide && !e.isAvailable()) {
+                return;
+            }
+            const pokemon = PokemonHelper.getPokemonByName(e.name);
+            const partyPokemon = App.game.party.getPokemonByName(e.name);
+            const eData = {
+                image: PokemonHelper.getImage(pokemon.id, undefined, undefined, GameConstants.ShadowStatus.None),
+                pkrsImage: partyPokemon?.pokerus > GameConstants.Pokerus.Uninfected ? `assets/images/breeding/pokerus/${GameConstants.Pokerus[partyPokemon.pokerus]}.png` : '',
+                EVs: partyPokemon?.pokerus >= GameConstants.Pokerus.Contagious ? `EVs: ${partyPokemon.evs().toLocaleString('en-US')}` : '',
+                shiny:  partyPokemon?.shiny || false,
+                hide: false, // We already filter out hidden Pokémon
+                uncaught: !partyPokemon,
+                lock: !e.isAvailable(),
+                lockMessage: e.isAvailable() ? '' : e.requirement.hint(),
+            };
+            encounters.push(eData);
+        });
+
+        return encounters;
     }
 }

src/scripts/towns/SafariPokemonNPC.ts
@@ -1,41 +1,0 @@
-class SafariPokemonNPC extends NPC {
-
-    constructor(
-        public name: string,
-        public dialog: string[],
-        public region: GameConstants.Region,
-        image: string = undefined,
-        requirement?: Requirement | MultiRequirement | OneFromManyRequirement
-    ) {
-        super(name, dialog, {image: image, requirement: requirement});
-    }
-
-    get dialogHTML(): string {
-        if (!SafariPokemonList.list[this.region]) {
-            return 'Huh!? This is a restricted area, kid. You can\'t be here!';
-        }
-
-        let pokemonHTML = SafariPokemonList.list[this.region]().map((p) => {
-            const isAvailable = p.isAvailable();
-            if (!isAvailable && !p.showUncaught) {
-                // Don't show uncaught pokemon
-                return '';
-            }
-            let html = `<img width="72" class="d-block ${!isAvailable ? 'dungeon-pokemon-locked' : ''}" src="assets/images/pokemon/${PokemonHelper.getPokemonByName(p.name).id}.png" />`;
-            if (this.region === GameConstants.Region.kalos) {
-                const partyPokemon = App.game.party.getPokemonByName(p.name);
-                if (partyPokemon?.pokerus) {
-                    html += `<img class="d-block mx-auto" src="assets/images/breeding/pokerus/${GameConstants.Pokerus[partyPokemon.pokerus]}.png" />`;
-                }
-                return `<div class="mb-1">${html}</div>`;
-            }
-            return html;
-        });
-
-        if (this.region === GameConstants.Region.kalos) {
-            pokemonHTML = pokemonHTML.slice(0, GameConstants.FRIEND_SAFARI_POKEMON); // display just the daily pokemon for friend safari
-        }
-
-        return `${super.dialogHTML}<div class="d-flex flex-wrap justify-content-around">${pokemonHTML.join('')}</div>`;
-    }
-}

src/scripts/towns/Town.ts
@@ -3,7 +3,6 @@
 ///<reference path="KantoBerryMasterNPC.ts"/>
 ///<reference path="ProfNPC.ts"/>
 ///<reference path="RoamerNPC.ts"/>
-///<reference path="SafariPokemonNPC.ts"/>
 ///<reference path="GiftNPC.ts"/>
 ///<reference path="TownContent.ts"/>
 

src/scripts/towns/TownList.ts
@@ -793,10 +793,6 @@ const NewIslandAsh2 = new NPC('Ash Ketchum',
     }
 );
 
-const KantoSafariRanger = new SafariPokemonNPC('Safari Ranger', [
-    'All sorts of unique Pokémon can be found in the Safari Zone!',
-], GameConstants.Region.kanto, 'assets/images/npcs/Pokémon Breeder (male).png');
-
 const BugCatcherPinsir = new NPC('Bug Catcher Michel', [
     'I heard there was a stone hidden in the Safari Zone that makes Pinsir stronger!',
     'But... I don\'t have a high enough Safari Level to find it.',
@@ -1017,7 +1013,7 @@ TownList['Safari Zone'] = new Town(
     [new SafariTownContent()],
     {
         requirements: [new CustomRequirement(ko.pureComputed(() => +App.game.keyItems.hasKeyItem(KeyItemType.Safari_ticket)), 1, 'Obtain the Safari Ticket')],
-        npcs: [KantoSafariRanger, BugCatcherPinsir],
+        npcs: [BugCatcherPinsir],
     }
 );
 TownList['Cinnabar Island'] = new Town(
@@ -4139,10 +4135,6 @@ const CelesticGrandma = new NPC('Cynthia\'s Grandmother', [
     'Did you know that in Johto they don’t see Pokémon like Mamoswine? It’s strange too, because you don’t even need a stone to evolve Piloswine… maybe they should try the Day Care?',
 ]);
 
-const SinnohSafariRanger = new SafariPokemonNPC('Safari Ranger', [
-    'There are some Pokémon here that can\'t be found anywhere else in Sinnoh!',
-], GameConstants.Region.sinnoh, 'assets/images/npcs/Pokemon Ranger (female).png');
-
 const PalParkWarden = new NPC('Pal Park Warden', [
     'Hey, welcome to the Pal Park! Have you been to my Dad’s Safari Zone in Kanto? We don’t have as many Pokémon here, but I’ve heard that a flower Pokémon found here can bloom when it’s sunny outside!',
 ]);
@@ -4600,7 +4592,7 @@ TownList['Great Marsh'] = new Town(
     [new SafariTownContent()],
     {
         requirements: [new RouteKillRequirement(10, GameConstants.Region.sinnoh, 218)],
-        npcs: [SinnohSafariRanger],
+        npcs: [],
     }
 );
 TownList['Snowpoint City'] = new Town(
@@ -6328,17 +6320,12 @@ const VivillonPhotobook = new NPC('Vivillon Photobook', [
     ]),
 });
 
-const KalosSafariRanger = new SafariPokemonNPC('Safari Ranger', [
-    'We\'ve had sightings of several unique Pokémon today along with the usual familiar faces!',
-    'These Pokémon will hide from trainers unless captured elsewhere first.',
-], GameConstants.Region.kalos, 'assets/images/npcs/Pokemon Ranger (female).png');
-
 const FriendlyAttendant = new NPC('Friendly Attendant', [
     'Welcome to the Friend Safari!',
     'This place is a lot like the Kanto Safari Zone, except we get a much wider variety of Pokémon coming through here.',
     'Our park staff stocks the Safari with different hard-to-find Pokémon every day. Many of these Pokémon can\'t be caught anywhere else in the world!',
     'As new and rare types of Pokémon are discovered, park staff will add them to our rotation of potential stock!',
-]);
+], {image: 'assets/images/npcs/Pokemon Ranger (female).png'});
 
 const BugCatcherScizor = new NPC('Bug Catcher Elliot', [
     'I heard there was a stone hidden in the Friend Safari that makes Scizor stronger!',
@@ -6565,7 +6552,7 @@ TownList['Friend Safari'] = new Town(
     [new SafariTownContent('Enter Friend Safari')],
     {
         requirements: [new GymBadgeRequirement(BadgeEnums.Elite_KalosChampion)],
-        npcs: [KalosSafariRanger, FriendlyAttendant, BugCatcherScizor],
+        npcs: [FriendlyAttendant, BugCatcherScizor],
     }
 );
 

commit e28212be8549244d1915e77f6ef4b990ce887ff0

chore(settings): Settings refactor: new SearchSetting class (#5162)
* New SearchSetting type of setting for better handling regexes

* sneaky error fix from previously merged PR

fixed error caused by #5157

---------

Co-authored-by: CypherX <cypherx00@gmail.com>

src/components/townView.html
@@ -182,7 +182,7 @@
         </ul>
       </div>
     </knockout>
-    <knockout data-bind="if: player.town().content.find(c => c instanceof SafariTownContent)">
+    <knockout data-bind="if: player.town.content.find(c => c instanceof SafariTownContent)">
         <div id="dungeonPokemonList" class="card pt-2 pb-3 px-1">
           <!--Display all available Pokémon in this Safari-->
           <ul class="list-inline mb-0">

src/modules/GameHelper.ts
@@ -162,6 +162,20 @@ export default class GameHelper {
         return s.replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&');
     }
 
+    public static safelyBuildRegex(pattern: string, allowRaw = true, flags = 'i'): RegExp {
+        if (allowRaw && /^\/.+\/$/.test(pattern)) {
+            // pattern is a string representation of regex e.g. /pattern/, remove the slashes
+            const rawPattern = pattern.slice(1, -1);
+            try {
+                return new RegExp(rawPattern, flags);
+            } catch {
+                return new RegExp(GameHelper.escapeStringRegex(rawPattern), flags);
+            }
+        } else {
+            return new RegExp(GameHelper.escapeStringRegex(pattern), flags);
+        }
+    }
+
     public static twoDigitNumber(n: number): string {
         // For use in clocks / showing time
         // Turns 4 into 04, does nothing to 23, turns 173 into 73

src/modules/settings/SearchSetting.ts
@@ -1,0 +1,19 @@
+import GameHelper from '../GameHelper';
+import Setting from './Setting';
+
+export default class SearchSetting extends Setting<string> {
+    public readonly regex: ko.PureComputed<RegExp>;
+
+    constructor(name: string, displayName: string, defaultValue: string) {
+        super(
+            name,
+            displayName,
+            [],
+            defaultValue,
+        );
+
+        this.regex = ko.pureComputed(() => {
+            return GameHelper.safelyBuildRegex(this.observableValue());
+        });
+    }
+}