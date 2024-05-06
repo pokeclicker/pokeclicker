77aa3160d7d571230696315bbd4056404aa43050
EatPant2nd <106291026+EatPant2nd@users.noreply.github.com>
2024-05-06 18:39:03 -0400

    Added Certain Johto Pokémon to Kanto
    Added Houndour, Sunkern, and Slugma to specific Kanto routes. They have "Caught + Johto Champion" requirements to both follow canon and to also force the player to acquire them as egg Pokémon first.

--- src/modules/routes/RouteData.ts
+++ src/modules/routes/RouteData.ts
@@ -84,6 +84,7 @@ Routes.add(new RegionRoute(
     new RoutePokemon({
         land: ['Caterpie', 'Metapod', 'Weedle', 'Kakuna', 'Pidgey', 'Oddish', 'Abra', 'Bellsprout'],
         water: ['Tentacool', 'Krabby', 'Horsea', 'Magikarp'],
+        special: [new SpecialRoutePokemon(['Sunkern'], new MultiRequirement([new ObtainedPokemonRequirement('Sunkern'), new GymBadgeRequirement(BadgeEnums.Elite_JohtoChampion)]))],
     }),
     [new TemporaryBattleRequirement('Blue 2')],
     4.1,
@@ -166,6 +167,7 @@ Routes.add(new RegionRoute(
     'Kanto Route 7', Region.kanto, 7,
     new RoutePokemon({
         land: ['Pidgey', 'Vulpix', 'Oddish', 'Meowth', 'Growlithe', 'Bellsprout'],
+        special: [new SpecialRoutePokemon(['Houndour'], new MultiRequirement([new ObtainedPokemonRequirement('Houndour'), new GymBadgeRequirement(BadgeEnums.Elite_JohtoChampion)]))],
     }),
     [new RouteKillRequirement(10, Region.kanto, 8)],
     10.2,
@@ -229,6 +231,7 @@ Routes.add(new RegionRoute(
     'Kanto Route 16', Region.kanto, 16,
     new RoutePokemon({
         land: ['Rattata', 'Raticate', 'Spearow', 'Doduo'],
+        special: [new SpecialRoutePokemon(['Slugma'], new MultiRequirement([new ObtainedPokemonRequirement('Slugma'), new GymBadgeRequirement(BadgeEnums.Elite_JohtoChampion)]))],
     }),
     [
         new OneFromManyRequirement([
@@ -243,6 +246,7 @@ Routes.add(new RegionRoute(
     'Kanto Route 17', Region.kanto, 17,
     new RoutePokemon({
         land: ['Rattata', 'Raticate', 'Spearow', 'Fearow', 'Doduo'],
+        special: [new SpecialRoutePokemon(['Slugma'], new MultiRequirement([new ObtainedPokemonRequirement('Slugma'), new GymBadgeRequirement(BadgeEnums.Elite_JohtoChampion)]))],
     }),
     [
         new OneFromManyRequirement([
@@ -257,6 +261,7 @@ Routes.add(new RegionRoute(
     'Kanto Route 18', Region.kanto, 18,
     new RoutePokemon({
         land: ['Rattata', 'Raticate', 'Spearow', 'Fearow', 'Doduo'],
+        special: [new SpecialRoutePokemon(['Slugma'], new MultiRequirement([new ObtainedPokemonRequirement('Slugma'), new GymBadgeRequirement(BadgeEnums.Elite_JohtoChampion)]))],
     }),
     [
         new OneFromManyRequirement([
