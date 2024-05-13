316a31f115518b29e899d2bcaebecdcb9454c282
osherwott <114451054+osherwott@users.noreply.github.com>
2024-05-13 02:02:05 -0500

    simplify ContestBattlePokemon

--- src/scripts/contest/ContestBattlePokemon.ts
+++ src/scripts/contest/ContestBattlePokemon.ts
@@ -2,26 +2,11 @@
 
 class ContestBattlePokemon extends BattlePokemon {
     constructor(
-            public name: PokemonNameType,
-            public id: number,
-            public type1: PokemonType = PokemonType.None,
-            public type2: PokemonType = PokemonType.None,
-            maxHealth: number,
-            public level: number,
-            public catchRate: number,
-            public exp: number,
-            public reward: Amount = new Amount(0, GameConstants.Currency.money),
-            public shiny: boolean,
-            public gemReward = 1,
-            public gender: number,
-            public shadow: GameConstants.ShadowStatus,
-            public encounterType: EncounterType,
-            public heldItem?: BagItem,
-            public ep: number = GameConstants.BASE_EP_YIELD,
-            public contestType1: ContestType = ContestType.None,
-            public contestType2: ContestType = ContestType.None,
-            public contestType3: ContestType = ContestType.None
+        public contestType1: ContestType = ContestType.None,
+        public contestType2: ContestType = ContestType.None,
+        public contestType3: ContestType = ContestType.None,
+        ...args: ConstructorParameters<typeof BattlePokemon>
     ) {
-        super(name, id, type1, type2, maxHealth, level, catchRate, exp, reward, shiny, gemReward, gender, shadow, encounterType, heldItem, ep);
+        super(...args);
     }
 }
