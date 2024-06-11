fbd1672854de313b0221a21cc23b6712797ab94e
osherwott <114451054+osherwott@users.noreply.github.com>
2024-06-10 19:10:45 -0500

    Lunar duo dungeon bosses during moon phases

--- src/scripts/dungeons/Dungeon.ts
+++ src/scripts/dungeons/Dungeon.ts
@@ -7777,6 +7777,7 @@ dungeonList['Fullmoon Island'] = new Dungeon('Fullmoon Island',
     [
         new DungeonBossPokemon('Lunatone', 11000000, 100),
         new DungeonBossPokemon('Clefable', 11000000, 100),
+        new DungeonBossPokemon('Cresselia', 11000000, 100, {requirement: new MoonCyclePhaseRequirement([0, 1, 7])}),
     ],
     96500, 230);
 
@@ -7798,7 +7799,7 @@ dungeonList['Newmoon Island'] = new Dungeon('Newmoon Island',
     [
         new DungeonBossPokemon('Lunatone', 9900000, 100),
         new DungeonBossPokemon('Absol', 9900000, 100),
-        new DungeonBossPokemon('Darkrai', 11000000, 100),
+        new DungeonBossPokemon('Darkrai', 11000000, 100, {requirement: new MoonCyclePhaseRequirement([4, 3, 5, 2, 6])}),
     ],
     96500, 230);
 
