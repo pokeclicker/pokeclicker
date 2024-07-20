5297b3df2dd50cfa77ea897d14e01fb66b4765ce
Delbaere Thibaut <delbaere_thibaut@hotmail.be>
2024-07-20 22:28:41 +0200

    recurssive travelling of the tree
    recurssive travalling of the tree of the requirements + simplify the three functions in one + eslint

--- src/scripts/encountersInfo/RouteInfo.ts
+++ src/scripts/encountersInfo/RouteInfo.ts
@@ -18,13 +18,15 @@ class RouteInfo {
             [...new Set(pokemonList.headbutt)].forEach(pokemonName => {
                 pokemonArray.push({id: PokemonHelper.getPokemonByName(pokemonName).id, name: pokemonName, type: 'headbutt'});
             });
-            
+
             [...new Set(pokemonList.special.filter(p => p.isAvailable()))].forEach(special => {
                 [...new Set(special.pokemon)].forEach(pokemonName => {
                     pokemonArray.push({id: PokemonHelper.getPokemonByName(pokemonName).id, name: pokemonName, type: 'special', requirement: special.req});
-                })
+                });
             });
-            pokemonArray.sort((a, b) => {return a.id - b.id;});
+            pokemonArray.sort((a, b) => {
+                return a.id - b.id;
+            });
         }
         const roamerList = RoamingPokemonList.getSubRegionalGroupRoamers(player.region, RoamingPokemonList.findGroup(player.region, player.subregion));
         const roamerArray = [];
@@ -32,7 +34,9 @@ class RouteInfo {
             [...new Set(roamerList)].forEach(roamer => {
                 roamerArray.push({id: roamer.pokemon.id, name: roamer.pokemonName, type: 'roamer', requirement: roamer.unlockRequirement});
             });
-            roamerArray.sort((a, b) => {return a.id - b.id;});
+            roamerArray.sort((a, b) => {
+                return a.id - b.id;
+            });
         }
         return {pokemons: pokemonArray, roamers: roamerArray};
     }
@@ -43,58 +47,37 @@ class RouteInfo {
 
     public static getInformations(pokemon) {
         if (pokemon.type == 'roamer') {
-            if (RouteInfo.isSpecialEventRequirement(pokemon.requirement)) {
-                return {tooltip: "Event Roaming Pokémon", image: "event_roaming.png"};
+            if (RouteInfo.isRequirement(pokemon.requirement, SpecialEventRequirement)) {
+                return {tooltip: 'Event Roaming Pokémon', image: 'event_roaming.png'};
+            } else {
+                return {tooltip: 'Roaming Pokémon', image: 'roaming.png'};
             }
-            else {
-                return {tooltip: "Roaming Pokémon", image: "roaming.png"};
+        } else if (pokemon.type == 'special') {
+            if (RouteInfo.isRequirement(pokemon.requirement, SpecialEventRequirement)) {
+                return {tooltip: 'Event Pokémon', image: 'event.png'};
+            } else if (RouteInfo.isRequirement(pokemon.requirement, WeatherRequirement)) {
+                return {tooltip: 'Weather Pokémon', image: 'weather.png'};
+            } else if (RouteInfo.isRequirement(pokemon.requirement, DayOfWeekRequirement)) {
+                return {tooltip: 'Day of Week Pokémon', image: 'day_of_week.png'};
+            } else {
+                return {tooltip: 'Special Pokémon', image: 'special.png'};
             }
+        } else if (pokemon.type == 'water' && pokemon.super_rod) {
+            return {tooltip: 'Super Rod Pokémon', image: 'super_rod.png'};
         }
-        else if (pokemon.type == 'special') {
-            if (RouteInfo.isSpecialEventRequirement(pokemon.requirement)) {
-                return {tooltip: "Event Pokémon", image: "event.png"};
-            }
-            else if (RouteInfo.isWeatherRequirement(pokemon.requirement)) {
-                return {tooltip: "Weather Pokémon", image: "weather.png"};
-            }
-            else if (RouteInfo.isDayOfWeekRequirement(pokemon.requirement)) {
-                return {tooltip: "Day of Week Pokémon", image: "day_of_week.png"};
-            }
-            else {
-                return {tooltip: "Special Pokémon", image: "special.png"};
-            }
-        } 
-        else if (pokemon.type == 'water' && pokemon.super_rod) {
-            return {tooltip: "Super Rod Pokémon", image: "super_rod.png"};
-        }
-        return {tooltip: "", image: ""};
+        return {tooltip: '', image: ''};
     }
 
-    private static isSpecialEventRequirement(requirement) {
-        if (requirement instanceof SpecialEventRequirement) return true;
-        if (requirement instanceof MultiRequirement) {
-            for (let req of requirement.requirements) {
-                if (RouteInfo.isSpecialEventRequirement(req)) return true;
-            }
+    private static isRequirement(requirement, type) {
+        //I traverse all the Requirement tree recursively to check if one of the requirements is the one I want
+        if (requirement instanceof type) {
+            return true;
         }
-        return false;
-    }
-
-    private static isWeatherRequirement(requirement) {
-        if (requirement instanceof WeatherRequirement) return true;
-        if (requirement instanceof MultiRequirement) {
-            for (let req of requirement.requirements) {
-                if (RouteInfo.isWeatherRequirement(req)) return true;
-            }
-        }
-        return false;
-    }
-
-    private static isDayOfWeekRequirement(requirement) {
-        if (requirement instanceof DayOfWeekRequirement) return true;
-        if (requirement instanceof MultiRequirement) {
-            for (let req of requirement.requirements) {
-                if (RouteInfo.isDayOfWeekRequirement(req)) return true;
+        if (requirement?.requirements) {
+            for (const req of requirement.requirements) {
+                if (RouteInfo.isRequirement(req, type)) {
+                    return true;
+                }
             }
         }
         return false;
