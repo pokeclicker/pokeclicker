ec0d9456199347678316c3132a30812c7a77b37e
Delbaere Thibaut <delbaere_thibaut@hotmail.be>
2024-09-17 04:43:38 +0200

    give the ice stone at rare item to cristal onyx
    see PR

--- src/modules/pokemons/PokemonList.ts
+++ src/modules/pokemons/PokemonList.ts
@@ -4136,7 +4136,7 @@ export const pokemonList = createPokemonArray(
             'specialDefense': 45,
             'speed': 70,
         },
-        'heldItem': { type: ItemType.underground, id: 'Hard Stone' },
+        'heldItem': { type: ItemType.underground, id: 'Ice Stone' },
     },
     {
         'id': 95.02,
