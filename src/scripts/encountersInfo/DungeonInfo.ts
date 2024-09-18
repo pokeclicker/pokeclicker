e1402a69626ffb7a3ec97b5ef07c4c3252369495
Delbaere Thibaut <delbaere_thibaut@hotmail.be>
2024-09-18 02:53:01 +0200

    works but not happy

--- src/scripts/encountersInfo/DungeonInfo.ts
+++ src/scripts/encountersInfo/DungeonInfo.ts
@@ -4,17 +4,25 @@ class DungeonInfo {
     }
 
     public static pokemonList = ko.pureComputed(() => {
-        return DungeonInfo.getPokemonList();
+        return DungeonInfo.isShadow() ? DungeonInfo.getShadowPokemonList() : DungeonInfo.getPokemonList();
     });
 
     public static itemList = ko.pureComputed(() => {
         return DungeonInfo.getItemList();
     });
 
+    private static isShadow() {
+        return player.town.dungeon?.enemyList.some((enemy) => {
+            return enemy instanceof DungeonTrainer && enemy.getTeam().filter(p => p.shadow == GameConstants.ShadowStatus.Shadow).length !== 0;
+        }) || player.town.dungeon?.bossList.some((enemy) => {
+            return !(enemy instanceof DungeonBossPokemon) && enemy.getTeam().filter(p => p.shadow == GameConstants.ShadowStatus.Shadow).length !== 0;
+        });
+    }
+
     private static getPokemonList() {
         const pokemonArray =
             player.town.dungeon?.enemyList
-                ?.filter((enemy) => {
+                .filter((enemy) => {
                     if (!enemy.hasOwnProperty('name')) {
                         if (typeof enemy === 'string') {
                             return true;
@@ -29,32 +37,67 @@ class DungeonInfo {
                     if (typeof enemy === 'string') {
                         return {id: PokemonHelper.getPokemonByName((<PokemonNameType>enemy)).id, name: enemy, type: 'normal', requirement: NullRequirement};
                     } else if (enemy.hasOwnProperty('pokemon')) {
-                        return {id: PokemonHelper.getPokemonByName((<DetailedPokemon>enemy).pokemon).id, name: (<DetailedPokemon>enemy).pokemon, type: 'normal', requirement: (<DetailedPokemon>enemy).options?.requirement};
+                        return {id: PokemonHelper.getPokemonByName((<DetailedPokemon>enemy).pokemon).id, name: (<DetailedPokemon>enemy).pokemon, type: 'normal', requirement: (<DetailedPokemon>enemy).options?.requirement, shadow: false};
                     }
                 })
                 .sort((a, b) => a.id - b.id);
         const bossArray =
             player.town.dungeon?.bossList
-                ?.filter((enemy) => {
+                .filter((enemy) => {
                     if (enemy instanceof DungeonBossPokemon) {
                         return !((<DungeonBossPokemon>enemy).options?.hide ? ((<DungeonBossPokemon>enemy).options?.requirement ? !(<DungeonBossPokemon>enemy).options?.requirement.isCompleted() : (<DungeonBossPokemon>enemy).options?.hide) : false);
                     } else {
                         return false;
                     }
                 })
-                .map((enemy) => ({id: PokemonHelper.getPokemonByName((<DungeonBossPokemon>enemy).name).id, name: (<DungeonBossPokemon>enemy).name, type: 'boss', requirement: (<DungeonBossPokemon>enemy).options?.requirement}))
+                .map((enemy) => {
+                    return {id: PokemonHelper.getPokemonByName((<DungeonBossPokemon>enemy).name).id, name: (<DungeonBossPokemon>enemy).name, type: 'boss', requirement: (<DungeonBossPokemon>enemy).options?.requirement, shadow: false};
+                })
                 .sort((a, b) => a.id - b.id);
         const mimicsArray =
             player.town.dungeon?.mimicList
-                ?.filter(enemy => App.game.party.alreadyCaughtPokemonByName(enemy))
+                .filter((enemy) => {
+                    return App.game.party.alreadyCaughtPokemonByName(enemy)
+                })
                 .map((enemy) => {
-                    return {id: PokemonHelper.getPokemonByName(enemy).id, name: enemy, type: 'mimic', requirement: NullRequirement};
+                    return {id: PokemonHelper.getPokemonByName(enemy).id, name: enemy, type: 'mimic', requirement: NullRequirement, shadow: false};
                 })
                 .sort((a, b) => a.id - b.id);
         return {
             pokemon: {category: 'Encounters', data: (pokemonArray ?? [])},
             boss: {category: 'Boss', data: (bossArray ?? [])},
             mimics: {category: 'Mimics', data: (mimicsArray ?? [])},
+        };
+    }
+
+    private static getShadowPokemonList() {
+        const pokemonArray =
+            player.town.dungeon?.enemyList
+                .filter((enemy) => {
+                    return enemy instanceof DungeonTrainer && enemy.getTeam().filter(p => p.shadow == GameConstants.ShadowStatus.Shadow).length !== 0 && !(enemy.options?.requirement && !enemy.options.requirement.isCompleted());
+                })
+                .map((enemy) => {
+                    return enemy.getTeam().filter(p => p.shadow == GameConstants.ShadowStatus.Shadow);
+                })
+                .map((pokemon) => {
+                    return {id: PokemonHelper.getPokemonByName(pokemon[0].name).id, name: pokemon[0].name, type: 'normal', requirement: pokemon[0].requirements, shadow: true};
+                })
+                .sort((a, b) => a.id - b.id);
+        const bossArray =
+            player.town.dungeon?.bossList
+                .filter((enemy) => {
+                    return !(enemy instanceof DungeonBossPokemon) && enemy.getTeam().filter(p => p.shadow == GameConstants.ShadowStatus.Shadow).length !== 0 && !(enemy.options?.hide ? (enemy.options?.requirement ? !enemy.options?.requirement.isCompleted() : enemy.options?.hide) : false);
+                })
+                .map((enemy) => {
+                    return enemy.getTeam().filter(p => p.shadow == GameConstants.ShadowStatus.Shadow);
+                })
+                .map((pokemon) => {
+                    return {id: PokemonHelper.getPokemonByName(pokemon[0].name).id, name: pokemon[0].name, type: 'boss', requirement: pokemon[0].requirements, shadow: true};
+                })
+                .sort((a, b) => a.id - b.id);
+        return {
+            pokemon: {category: 'Encounters', data: (pokemonArray ?? [])},
+            boss: {category: 'Boss', data: (bossArray ?? [])},
         };
     }
 
--- src/scripts/encountersInfo/RouteInfo.ts
+++ src/scripts/encountersInfo/RouteInfo.ts
@@ -14,28 +14,26 @@ class RouteInfo {
             [...new Set(pokemonList.land)].forEach(pokemonName => {
                 pokemonArray.push({id: PokemonHelper.getPokemonByName(pokemonName).id, name: pokemonName, type: 'land'});
             });
-
             if (App.game.keyItems.hasKeyItem(KeyItemType.Super_rod) || pokemonList.land.length == 0) {
                 [...new Set(pokemonList.water)].forEach(pokemonName => {
                     pokemonArray.push({id: PokemonHelper.getPokemonByName(pokemonName).id, name: pokemonName, type: 'water', fishing: pokemonList.land.length != 0});
                 });
             }
-
             [...new Set(pokemonList.headbutt)].forEach(pokemonName => {
                 pokemonArray.push({id: PokemonHelper.getPokemonByName(pokemonName).id, name: pokemonName, type: 'headbutt'});
             });
-
             pokemonList.special.filter(p => p.isAvailable()).forEach(special => {
                 [...new Set(special.pokemon)].forEach(pokemonName => {
                     pokemonArray.push({id: PokemonHelper.getPokemonByName(pokemonName).id, name: pokemonName, type: 'special', requirement: special.req});
                 });
             });
-
             pokemonArray.sort((a, b) => a.id - b.id);
         }
         const roamerArray =
             RoamingPokemonList.getSubRegionalGroupRoamers(player.region, RoamingPokemonList.findGroup(player.region, player.subregion))
-                ?.map((roamer) => ({id: roamer.pokemon.id, name: roamer.pokemonName, type: 'roamer', requirement: roamer.unlockRequirement}))
+                ?.map((roamer) => {
+                    return {id: roamer.pokemon.id, name: roamer.pokemonName, type: 'roamer', requirement: roamer.unlockRequirement}
+                })
                 .sort((a, b) => a.id - b.id);
         return {
             pokemons: {category: 'Encounters', data: (pokemonArray ?? [])},
