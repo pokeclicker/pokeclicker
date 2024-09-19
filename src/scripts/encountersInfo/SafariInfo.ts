04795534fe13de4f14a318a0444c19c7197a1a86
Delbaere Thibaut <delbaere_thibaut@hotmail.be>
2024-09-19 05:30:35 +0200

    Update SafariInfo.ts
    remove the distinction between items and pokemon from items in safari

--- src/scripts/encountersInfo/SafariInfo.ts
+++ src/scripts/encountersInfo/SafariInfo.ts
@@ -10,15 +10,9 @@ class SafariInfo {
     private static getItemList() {
         const itemsArray =
             SafariItemController.list[player.region]
-                ?.filter((item) => (PokemonHelper.getPokemonByName(item.item.id).name == 'MissingNo.'))
                 .map((item) => ({item: item.item.id, type: 'item', requirement: item.requirement}));
-        const pokemonsArray =
-            SafariItemController.list[player.region]
-                ?.filter((item) => (PokemonHelper.getPokemonByName(item.item.id).name != 'MissingNo.'))
-                .map((item) => ({item: item.item.id, type: 'pokemon', requirement: item.requirement}));
         return {
             items: {category: 'Items', data: (itemsArray ?? [])},
-            pokemons: {category: 'Pokémons', data: (pokemonsArray ?? [])},
         };
     }
 }
