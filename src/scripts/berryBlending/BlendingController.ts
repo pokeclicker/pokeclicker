fb968b9f4b24ca61cb8182c43da2e6bad7cd54c4
osherwott <114451054+osherwott@users.noreply.github.com>
2024-09-19 20:17:49 -0500

    better buying

--- src/scripts/berryBlending/BlendingController.ts
+++ src/scripts/berryBlending/BlendingController.ts
@@ -8,4 +8,20 @@ class BlendingController {
     public static getPokeblockList() {
         return Object.values(ItemList).filter((i) => i instanceof PokeBlock);
     }
+
+    public static amountInput = () => $('#berryBlenderModal').find('input[name="recipeAmount"]');
+
+    public static resetAmount() {
+        this.amountInput().val(1).change();
+    }
+
+    public static increaseAmount(n: number) {
+        const newVal = (parseInt(this.amountInput().val().toString(), 10) || 0) + n;
+        this.amountInput().val(newVal > 1 ? newVal : 1).change();
+    }
+
+    public static multiplyAmount(n: number) {
+        const newVal = (parseInt(this.amountInput().val().toString(), 10) || 0) * n;
+        this.amountInput().val(newVal > 1 ? newVal : 1).change();
+    }
 }
