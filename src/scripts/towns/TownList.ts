3bd3fbb4629a604e526a0547429e2532392e91ba
ely1210 <elymac2000@gmail.com>
2024-10-01 02:59:52 +0000

    Forgot double quotes weren't allowed, single quote

--- src/scripts/towns/TownList.ts
+++ src/scripts/towns/TownList.ts
@@ -321,7 +321,7 @@ const CeruleanKantoBerryMaster = new KantoBerryMasterNPC('Berry Master', [
 ]);
 const CeruleanFarmApprentice = new NPC('Farm Apprentice', [
     'Buying all these water eggs with Quest Points sure is expensive....but one day when I was tending to my passho berries, a squirtle suddenly appeared!'
-    ,"Maybe I could save even MORE Quest Points on eggs if I keep mutating different berries.",
+    ,'Maybe I could save even MORE Quest Points on eggs if I keep mutating different berries.',
 ], {image: 'assets/images/npcs/Bug Catcher.png'});
 
 const CeruleanSuperNerd = new NPC('Super Nerd Jovan', [
