8e74ed2d1f39bc6c7accecebf5ff31a462024968
osherwott <114451054+osherwott@users.noreply.github.com>
2024-10-15 17:46:23 -0500

    pokeblock description cleanup

--- src/modules/items/PokeBlock.ts
+++ src/modules/items/PokeBlock.ts
@@ -1,6 +1,5 @@
 import { PokeBlockColor, Currency } from '../GameConstants';
 import ContestType from '../enums/ContestType';
-import FlavorType from '../enums/FlavorType';
 import Item from './Item';
 
 export default class PokeBlock extends Item {
@@ -14,8 +13,8 @@ export default class PokeBlock extends Item {
         currency: Currency = Currency.money,
         contestType?: ContestType,
         canUse?: (pokemon: any) => boolean,
+        description: string = `A ${PokeBlockColor[color]} Pokéblock`,
         displayName: string = `${PokeBlockColor[color]} Pokéblock`,
-        description: string = `A ${FlavorType[contestType]} Pokéblock that boosts the Appeal of ${ContestType[contestType]} Pokémon`,
     ) {
         super(`PokeBlock_${PokeBlockColor[color]}`, basePrice, currency, undefined, displayName, description, 'pokeblock');
         this.type = color;
