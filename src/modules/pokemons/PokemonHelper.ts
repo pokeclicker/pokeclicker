c0484dc46e42af2a477d796fd385653db532d91e
osherwott <114451054+osherwott@users.noreply.github.com>
2024-10-15 17:02:01 -0500

    rest of merging

--- src/modules/pokemons/PokemonHelper.ts
+++ src/modules/pokemons/PokemonHelper.ts
@@ -16,13 +16,7 @@ import MegaEvolveRequirement from '../requirements/MegaEvolveRequirement';
 import type MegaStoneItem from '../items/MegaStoneItem';
 import { ItemList } from '../items/ItemList';
 import Settings from '../settings/Settings';
-import type { EvoData } from './evolutions/Base';
 import ContestType from '../enums/ContestType';
-
-// TODO remove when PokemonLocations is ported to modules
-declare class PokemonLocations {
-    public static getPokemonPrevolution(pokemonName: PokemonNameType, maxRegion?: Region): Array<EvoData>;
-}
 
 // TODO remove when Dungeon is ported to modules
 declare class Dungeon {
