bd0f0df6d0cabf94a3b1d15041dd705ddf0da905
RedzepR <143606926+RedzepR@users.noreply.github.com>
2024-11-05 15:36:01 +0100

    eslint

--- src/modules/pokemons/PokemonList.ts
+++ src/modules/pokemons/PokemonList.ts
@@ -24,7 +24,7 @@ import BerryType from '../enums/BerryType';
 import ItemType from '../enums/ItemType';
 import PokemonType from '../enums/PokemonType';
 import {
-    Region, StoneType, Genders, MaxIDPerRegion, MegaStoneType, MAX_EGG_CYCLES, AchievementOption,
+    Region, StoneType, Genders, MaxIDPerRegion, MegaStoneType, MAX_EGG_CYCLES,
 } from '../GameConstants';
 import BagItem from '../interfaces/BagItem';
 import {
