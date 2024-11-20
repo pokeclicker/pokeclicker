6f73cb7aec9d9e83f8c826b92bd29e302fbbebc1
jaaslet <jonas_bms@hotmail.com>
2024-11-20 09:56:19 +0100

    Fixed quest

--- src/scripts/quests/questTypes/TrainSpecificPokemonQuest.ts
+++ src/scripts/quests/questTypes/TrainSpecificPokemonQuest.ts
@@ -17,4 +17,14 @@ class TrainSpecificPokemonQuest extends Quest implements QuestInterface {
         }
         return `Train ${this.pokemon.name} to ${this.amount} attack.`;
     }
+
+    override createProgressObservables() {
+        super.createProgressObservables();
+
+        this.focusSub?.dispose();
+        // We don't want the attack to go up by a lot, each time the pokemon is breed
+        this.focusSub = this._focus.subscribe?.((newValue) => {
+            this.focusValue = newValue;
+        });
+    }
 }
