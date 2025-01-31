f86c7b6de0baeb937d49663ccf522710df172a6b
osherwott <114451054+osherwott@users.noreply.github.com>
2025-01-31 10:06:53 -0600

    don't include forms for evolution assistant

--- src/scripts/towns/AssistantNPC.ts
+++ src/scripts/towns/AssistantNPC.ts
@@ -16,9 +16,18 @@ class AssistantNPC extends NPC {
         const pokemonList = () => {
             switch (this.assistantType) {
                 case 'baby':
-                    return App.game.party.caughtPokemon.filter(p => pokemonBabyPrevolutionMap[p.name] && player.highestRegion() >= pokemonMap[pokemonBabyPrevolutionMap[p.name]].nativeRegion && !App.game.party.caughtPokemon.some(e => e.name === pokemonBabyPrevolutionMap[p.name])).sort((a,b) => a.id - b.id);
+                    return App.game.party.caughtPokemon.filter(p =>
+                        pokemonBabyPrevolutionMap[p.name] &&
+                        player.highestRegion() >= pokemonMap[pokemonBabyPrevolutionMap[p.name]].nativeRegion &&
+                        !App.game.party.caughtPokemon.some(e => e.name === pokemonBabyPrevolutionMap[p.name])
+                    ).sort((a,b) => a.id - b.id);
                 case 'evolution':
-                    return App.game.party.caughtPokemon.filter(p => p.evolutions?.some(e => e.trigger !== EvoTrigger.NONE && !App.game.party.caughtPokemon.some(p => p.name === e.evolvedPokemon) && player.highestRegion() >= pokemonMap[e.evolvedPokemon].nativeRegion && !e.restrictions.some((r) => r instanceof MegaEvolveRequirement))).sort((a,b) => a.id - b.id);
+                    return App.game.party.caughtPokemon.filter(p => p.evolutions?.some(e =>
+                        e.trigger !== EvoTrigger.NONE &&
+                        !App.game.party.caughtPokemon.some(p => p.name === e.evolvedPokemon) &&
+                        player.highestRegion() >= pokemonMap[e.evolvedPokemon].nativeRegion &&
+                        Math.floor(pokemonMap[e.basePokemon].id) != Math.floor(pokemonMap[e.evolvedPokemon].id)
+                    )).sort((a,b) => a.id - b.id);
                 default:
                     return ['Pikachu'];
             }
