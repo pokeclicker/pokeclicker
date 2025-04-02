64e16b4819de3fe5142cbbbb3989448f84070e76
AevitasDragonkin <aevitas.dragonkin@gmail.com>
2025-04-02 15:56:12 +0200

    Button to switch between list and grid view

--- src/modules/settings/index.ts
+++ src/modules/settings/index.ts
@@ -161,6 +161,12 @@ Settings.add(new Setting<string>('undergroundTreasureDisplayGrouping', 'Grouping
     ],
     'type'));
 Settings.add(new BooleanSetting('undergroundTreasureDisplayShowLocked', 'Show locked items', false));
+Settings.add(new Setting<string>('undergroundTreasureViewMode', 'Underground Treasure View Mode',
+    [
+        new SettingOption('List', 'list-view'),
+        new SettingOption('Grid', 'grid-view'),
+    ],
+    'list-view'));
 
 Settings.add(new BooleanSetting('showUndergroundModule', 'Show Underground module on main screen', true));
 Settings.add(new BooleanSetting('enableUndergroundModuleMineControls', 'Enable Underground module mine controls', true));
