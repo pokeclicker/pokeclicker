ad93bb5d80591a23cb753694bb92c80d394d9693
AevitasDragonkin <aevitas.dragonkin@gmail.com>
2025-04-09 22:26:08 +0200

    Added base override cost

--- src/modules/weather/WeatherOverride.ts
+++ src/modules/weather/WeatherOverride.ts
@@ -1,4 +1,7 @@
 import { Feature } from '../DataStore/common/Feature';
+import WeatherType from './WeatherType';
+import Item from '../items/Item';
+import { ItemList } from '../items/ItemList';
 
 export class WeatherOverride implements Feature {
     name = 'Weather Override';
@@ -6,6 +9,52 @@ export class WeatherOverride implements Feature {
 
     defaults: Record<string, any> = {
 
+    };
+
+    public static weatherCost: { [weather in WeatherType]?: Array<{ item: Item, amount: number }> } = {
+        [WeatherType.Clear]: [
+            { item: ItemList.Damp_rock_fragment, amount: 25 },
+            { item: ItemList.Heat_rock_fragment, amount: 25 },
+            { item: ItemList.Icy_rock_fragment, amount: 25 },
+            { item: ItemList.Smooth_rock_fragment, amount: 25 },
+        ],
+        [WeatherType.Overcast]: [
+            { item: ItemList.Damp_rock_fragment, amount: 50 },
+            { item: ItemList.Heat_rock_fragment, amount: 50 },
+        ],
+        [WeatherType.Rain]: [
+            { item: ItemList.Damp_rock_fragment, amount: 100 },
+        ],
+        [WeatherType.Thunderstorm]: [
+            { item: ItemList.Damp_rock_fragment, amount: 65 },
+            { item: ItemList.Heat_rock_fragment, amount: 35 },
+        ],
+        [WeatherType.Snow]: [
+            { item: ItemList.Damp_rock_fragment, amount: 50 },
+            { item: ItemList.Icy_rock_fragment, amount: 50 },
+        ],
+        [WeatherType.Hail]: [
+            { item: ItemList.Heat_rock_fragment, amount: 35 },
+            { item: ItemList.Icy_rock_fragment, amount: 65 },
+        ],
+        [WeatherType.Blizzard]: [
+            { item: ItemList.Icy_rock_fragment, amount: 100 },
+        ],
+        [WeatherType.Harsh_Sunlight]: [
+            { item: ItemList.Heat_rock_fragment, amount: 100 },
+        ],
+        [WeatherType.Sandstorm]: [
+            { item: ItemList.Smooth_rock_fragment, amount: 100 },
+        ],
+        [WeatherType.Fog]: [
+            { item: ItemList.Damp_rock_fragment, amount: 50 },
+            { item: ItemList.Heat_rock_fragment, amount: 25 },
+            { item: ItemList.Icy_rock_fragment, amount: 25 },
+        ],
+        [WeatherType.Windy]: [
+            { item: ItemList.Damp_rock_fragment, amount: 35 },
+            { item: ItemList.Heat_rock_fragment, amount: 65 },
+        ],
     };
 
     canAccess(): boolean {
