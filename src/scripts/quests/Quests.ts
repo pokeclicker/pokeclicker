4c663f0262bbb2e0780f300c82f22e83dca6c3ff
hippolythe guiffant <hippolythe.guiffant@epitech.eu>
2025-06-02 21:30:23 +0200

    feedback from AevitasDragonki taken into account

--- src/scripts/quests/Quests.ts
+++ src/scripts/quests/Quests.ts
@@ -18,6 +18,7 @@ class Quests implements Saveable {
     public freeRefresh = ko.observable(false);
     public questList: KnockoutObservableArray<Quest> = ko.observableArray();
     public questLines: KnockoutObservableArray<QuestLine> = ko.observableArray();
+    public selectedQuestName = ko.observable('');
     public level: KnockoutComputed<number> = ko.pureComputed((): number => {
         return this.xpToLevel(this.xp());
     });
@@ -41,7 +42,7 @@ class Quests implements Saveable {
         return list.sort(Quests.questCompareBy);
     });
 
-    constructor() {}
+    constructor() { }
 
     static questCompareBy(quest1, quest2): number {
         if (Quests.getQuestSortStatus(quest1) < Quests.getQuestSortStatus(quest2)) {
@@ -78,7 +79,7 @@ class Quests implements Saveable {
     }
 
     public beginQuest(index: number) {
-        const quest  = this.questList()[index];
+        const quest = this.questList()[index];
         // Check if we can start a new quest, and the requested quest isn't started or completed
         if (this.canStartNewQuest() && quest && !quest.inProgress() && !quest.isCompleted()) {
             quest.begin();
@@ -95,7 +96,7 @@ class Quests implements Saveable {
 
     public quitQuest(index: number, shouldConfirm = false) {
         // Check if we can quit this quest
-        const quest  = this.questList()[index];
+        const quest = this.questList()[index];
         if (quest && quest.inProgress()) {
             quest.quit(shouldConfirm);
         } else {
@@ -108,7 +109,7 @@ class Quests implements Saveable {
 
     public claimQuest(index: number) {
         // Check if we can claim this quest
-        const quest  = this.questList()[index];
+        const quest = this.questList()[index];
         if (quest && quest.isCompleted() && !quest.claimed()) {
             quest.claim();
             if (player.highestRegion() >= GameConstants.Region.kalos && App.game.party.alreadyCaughtPokemonByName('Medicham') && !player.hasMegaStone(GameConstants.MegaStoneType.Medichamite)) {
@@ -219,25 +220,33 @@ class Quests implements Saveable {
 
     public getQuestDetails(questName: QuestLineNameType) {
         type DetailsQuest = {
+            taskIndex: number;
             taskDescription: string;
             isCompleted: boolean;
             isCurrentTask: boolean;
+            questStatut: number;
         };
 
         const quest = this.getQuestLine(questName);
+
+        if (!quest) {
+            return;
+        }
+
         const tasks = quest.quests();
-        const returnObject = new Array<DetailsQuest>();
+        const returnObject = tasks.map(task => {
+            const detailsQuest: DetailsQuest = {
+                taskIndex: task.index,
+                taskDescription:
+                    (task.index - 1 <= quest.curQuest() || quest.state() === QuestLineState.ended) && quest.state() !== 0
+                        ? task.description
+                        : '???',
+                isCompleted: task.index - 1 < quest.curQuest() || quest.state() === QuestLineState.ended,
+                isCurrentTask: task.index - 1 === quest.curQuest() && quest.state() !== QuestLineState.ended,
+                questStatut: quest.state(),
+            };
 
-        tasks.forEach(task => {
-            if (task.customDescription !== undefined) {
-                const detailsQuest: DetailsQuest = {
-                    taskDescription: task.index - 1 <= quest.curQuest() || quest.state() === QuestLineState.ended ? task.customDescription : 'Continue to progress through the quest to unlock the next tasks.',
-                    isCompleted: task.index - 1 < quest.curQuest() || quest.state() === QuestLineState.ended ? true : false,
-                    isCurrentTask: task.index - 1 === quest.curQuest() && quest.state() !== QuestLineState.ended,
-                };
-
-                returnObject.push(detailsQuest);
-            }
+            return detailsQuest;
         });
 
         return returnObject;
@@ -319,7 +328,7 @@ class Quests implements Saveable {
     public questProgressTooltip() {
         const level = this.level();
         const xp = this.xp();
-        return {title : `${(xp - this.levelToXP(level)).toLocaleString('en-US')} / ${(this.levelToXP(level + 1) - this.levelToXP(level)).toLocaleString('en-US')}`, trigger : 'hover' };
+        return { title: `${(xp - this.levelToXP(level)).toLocaleString('en-US')} / ${(this.levelToXP(level + 1) - this.levelToXP(level)).toLocaleString('en-US')}`, trigger: 'hover' };
     }
 
     public isDailyQuestsUnlocked() {
