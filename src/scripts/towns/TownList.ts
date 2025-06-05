83be3ee0c72a74f8aa7e4c9fba0b89a442c5c33a
ely1210 <elymac2000@gmail.com>
2025-06-05 12:07:47 +1000

    new(npc): New Farm NPC (#5570)
    * git commit -m "New Farm NPC for suggestion #5568"
    
    * git commit -m "Changing capitalisation of QP"
    
    * Forgot double quotes weren't allowed, single quote
    
    * I can't do grammar: fixed
    
    * Capital B Berries is cringe
    
    * language
    
    ---------
    
    Co-authored-by: CypherX <672420+CypherX@users.noreply.github.com>

--- src/scripts/towns/TownList.ts
+++ src/scripts/towns/TownList.ts
@@ -319,6 +319,10 @@ const CeruleanKantoBerryMaster = new KantoBerryMasterNPC('Berry Master', [
     'Bah! You younglings have no appreciation of the art of Berry farming!',
     'Come back when you are ready to learn!',
 ]);
+const CeruleanFarmApprentice = new NPC('Farm Apprentice', [
+    'One day I was tending to my Passho berries and a Squirtle suddenly appeared! If only I knew about this before spending all of my Quest Points on Water Eggs..',
+    'If I try mutating different berries maybe I can catch new Pokémon while saving my Quest Points!',
+], {image: 'assets/images/npcs/Bug Catcher.png'});
 
 const CeruleanSuperNerd = new NPC('Super Nerd Jovan', [
     'In my spare time I like to play this kickass browser game. It takes ages to get all the best stuff.',
@@ -937,7 +941,7 @@ TownList['Cerulean City'] = new Town(
     [CeruleanCityShop, new ShardTraderShop(GameConstants.ShardTraderLocations['Cerulean City']), new MoveToDungeon(dungeonList['Cerulean Cave'])],
     {
         requirements: [new RouteKillRequirement(10, GameConstants.Region.kanto, 4)],
-        npcs: [CeruleanKantoBerryMaster, CeruleanSuperNerd, Mewtwo1, Mewtwo2, DetectiveRaichu],
+        npcs: [CeruleanKantoBerryMaster, CeruleanFarmApprentice, CeruleanSuperNerd, Mewtwo1, Mewtwo2, DetectiveRaichu],
     }
 );
 TownList['Bill\'s House'] = new Town(
