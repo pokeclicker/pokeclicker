ccf275f37f6d28ded0756b325e41fc80351e4ca3
CypherX <672420+CypherX@users.noreply.github.com>
2025-07-04 13:25:43 -0400

    1,000,000 -> 100,000 defeats
    1,000,000 defeats without clicking while one shotting would take 69 days

--- src/scripts/achievements/AchievementHandler.ts
+++ src/scripts/achievements/AchievementHandler.ts
@@ -682,8 +682,8 @@ class AchievementHandler {
 
         AchievementHandler.addSecretAchievement(
             'Smell Ya Later!',
-            'Defeat Champion Blue 1,000,000 times.',
-            new ClearGymRequirement(1e6, GameConstants.getGymIndex('Champion Blue')),
+            'Defeat Champion Blue 100,000 times.',
+            new ClearGymRequirement(1e5, GameConstants.getGymIndex('Champion Blue')),
             'Blue is my favorite color'
         );
 
