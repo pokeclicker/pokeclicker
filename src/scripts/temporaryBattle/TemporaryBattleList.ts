d07c3e6534bff07d1442c97ab605912d8b0dacc0
RedzepR <143606926+RedzepR@users.noreply.github.com>
2025-07-08 19:39:54 +0200

    Add a Questline based on Deoxys Movie. Unlocks 2 new alt forms

--- src/scripts/temporaryBattle/TemporaryBattleList.ts
+++ src/scripts/temporaryBattle/TemporaryBattleList.ts
@@ -2131,6 +2131,52 @@ TemporaryBattleList['Ghetsis 2'] = new TemporaryBattle(
         imageName: 'Ghetsis',
     }
 );
+TemporaryBattleList['Destiny Deoxys Rayquaza'] = new TemporaryBattle(
+    'Destiny Deoxys Rayquaza',
+    [
+        new GymPokemon('Deoxys', 47_276_622, 70),
+        new GymPokemon('Rayquaza', 57_782_538, 70),
+    ],
+    '<i>Rayquaza and Deoxys fled seperate ways.</i>',
+    [new QuestLineStepCompletedRequirement('Destiny Deoxys', 1)],
+    undefined,
+    {
+        displayName: 'Deoxys & Rayquaza',
+        imageName: 'specialNPCs/Transparent',
+        hideTrainer: true,
+    }
+);
+TemporaryBattleList['Destiny Deoxys Army'] = new TemporaryBattle(
+    'Destiny Deoxys Army',
+    [
+        new GymPokemon('Deoxys (Clone)', 11_766_624, 70),
+        new GymPokemon('Deoxys (Clone)', 11_766_624, 70),
+        new GymPokemon('Deoxys (Clone)', 11_766_624, 70),
+        new GymPokemon('Deoxys (Clone)', 11_766_624, 70),
+        new GymPokemon('Deoxys (Clone)', 11_766_624, 70),
+        new GymPokemon('Deoxys', 58_833_120, 70),
+    ],
+    '<i>The Deoxys are scattering.</i>',
+    [new QuestLineStepCompletedRequirement('Destiny Deoxys', 4)],
+    undefined,
+    {
+        displayName: 'Deoxys Army',
+        imageName: '../pokemon/386',
+        finalPokemonImage: 'specialNPCs/Transparent',
+    }
+);
+TemporaryBattleList['Destiny Rayquaza'] = new TemporaryBattle(
+    'Destiny Rayquaza',
+    [new GymPokemon('Rayquaza', 127_079_520, 70)],
+    '<i>Rayquaza calmed down.</i>',
+    [new QuestLineStepCompletedRequirement('Destiny Deoxys', 7)],
+    undefined,
+    {
+        displayName: 'Rayquaza',
+        imageName: '../pokemon/384',
+        hideTrainer: true,
+    }
+);
 TemporaryBattleList['Hugh 5'] = new TemporaryBattle(
     'Hugh 5',
     [
