ff15d6145a7e9c83313a85785b6008f562386b2f
osherwott <114451054+osherwott@users.noreply.github.com>
2025-11-08 22:01:49 -0600

    Update InContestRequirement.ts

--- src/modules/requirements/InContestRequirement.ts
+++ src/modules/requirements/InContestRequirement.ts
@@ -1,9 +1,10 @@
+import ContestRank from '../enums/ContestRank';
 import ContestType from '../enums/ContestType';
 import { AchievementOption } from '../GameConstants';
 import Requirement from './Requirement';
 
 export default class InContestRequirement extends Requirement {
-    constructor(public contestType: ContestType, option = AchievementOption.more) {
+    constructor(public contestType?: ContestType, public contestRank?: ContestRank, public availableInBalancedType = true, option = AchievementOption.more) {
         super(1, option);
     }
 
