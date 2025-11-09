c4872f88e795807df80465a52c123aa0398cf288
osherwott <114451054+osherwott@users.noreply.github.com>
2025-11-08 22:01:24 -0600

    Create InContestTypeRequirement.ts

---
+++ src/modules/requirements/InContestTypeRequirement.ts
@@ -0,0 +1,23 @@
+import ContestType from '../enums/ContestType';
+import { AchievementOption } from '../GameConstants';
+import Requirement from './Requirement';
+
+export default class InContestTypeRequirement extends Requirement {
+    constructor(public contestType: ContestType, public availableInBalancedType = true, option = AchievementOption.more) {
+        super(1, option);
+    }
+
+    public getProgress() {
+        if (this.availableInBalancedType && this.contestType != ContestType.Balanced) {
+            return Number(ContestRunner?.type() === this.contestType) || Number(ContestRunner?.type() === ContestType.Balanced);
+        }
+        return Number(ContestRunner?.type() === this.contestType);
+    }
+
+    public hint(): string {
+        if (this.availableInBalancedType && this.contestType != ContestType.Balanced) {
+            return `You must be in a ${ContestType[this.contestType]} or Balanced contest.`;
+        }
+        return `You must be in a ${ContestType[this.contestType]} contest.`;
+    }
+}
