80d10a48508eda59c23bb2e20d9475cef81e7b9c
osherwott <114451054+osherwott@users.noreply.github.com>
2025-11-09 00:35:51 -0600

    Update ContestBattlePokemon.ts

--- src/scripts/contest/ContestBattlePokemon.ts
+++ src/scripts/contest/ContestBattlePokemon.ts
@@ -1,11 +1,21 @@
 /// <reference path="../../declarations/GameHelper.d.ts" />
 
 class ContestBattlePokemon extends BattlePokemon {
+    status: KnockoutObservable<number>;
+    dance: KnockoutObservableArray<number>;
+    danceHearts: KnockoutObservable<number> = ko.observable(0);
+    usableMoves: contestMove[];
+
     constructor(
         public contestTypes: ContestType[],
         public nickname: string,
+        dance: number[],
+        moves: ContestType[],
         ...args: ConstructorParameters<typeof BattlePokemon>
     ) {
         super(...args);
+        this.status = ko.observable(ContestOpponentStatus.Waiting);
+        this.dance = ko.observableArray(dance);
+        this.usableMoves = moves.map(m => Object({ moveType: m, pp: ko.observable(1)}) as contestMove);
     }
 }
