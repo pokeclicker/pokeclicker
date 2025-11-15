fc8ca6fec567cf75181729ffedd289eab80ba185
Ultima <69112975+Ultima1990@users.noreply.github.com>
2025-11-15 19:08:01 +0100

    spaces oopsies

--- src/scripts/Update.ts
+++ src/scripts/Update.ts
@@ -2959,7 +2959,7 @@ class Update implements Saveable {
             if (saveData.badgeCase[17]) {
                 Update.startQuestLine(saveData, 'Team Rocket Again');
             }
-            
+
             // Add Parfum Palace Garden dungeon
             saveData.statistics.dungeonsCleared = Update.moveIndex(saveData.statistics.dungeonsCleared, 126);
         },
