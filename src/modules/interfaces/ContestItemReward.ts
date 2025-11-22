212451d4af757439a181b5a4d36dab78b993ad77
osherwott <114451054+osherwott@users.noreply.github.com>
2025-11-22 17:46:16 -0600

    interfaces

---
+++ src/modules/interfaces/ContestItemReward.ts
@@ -0,0 +1,12 @@
+import { ItemNameType } from '../items/ItemNameType';
+import MultiRequirement from '../requirements/MultiRequirement';
+import OneFromManyRequirement from '../requirements/OneFromManyRequirement';
+import Requirement from '../requirements/Requirement';
+
+export default interface ContestItemReward {
+    item: ItemNameType,
+    amount: KnockoutObservable<number>,
+    amountLimit?: number,
+    chance?: number,
+    requirement?: MultiRequirement | OneFromManyRequirement | Requirement,
+}
