8c2f700a0bf22ed225ce99335bebfe21f2e556ed
Martius RegisCoaxans <reiiky91300@gmail.com>
2025-12-17 00:31:45 +0100

    Typo

--- src/scripts/towns/TownList.ts
+++ src/scripts/towns/TownList.ts
@@ -7187,7 +7187,7 @@ const HapuHope = new NPC('Hapu', [
 });
 
 const AlolanAstronomer = new NPC('Alolan Astronomer', [
-    'I have been studing Cosmoem and noticed that it needs the light from either the sun or the moon to reach its fullest potential.',
+    'I have been studying Cosmoem and noticed that it needs the light from either the sun or the moon to reach its fullest potential.',
     'When exposed to the light of the sun and leveling up, it will transform into Solgaleo. And it will turn into Lunala with the light of the moon.',
 ]);
 
