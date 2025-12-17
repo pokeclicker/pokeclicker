41880396368011612e68851a04088ee84c1a89c7
osherwott <114451054+osherwott@users.noreply.github.com>
2025-12-16 23:38:36 -0600

    Update TownList.ts

--- src/scripts/towns/TownList.ts
+++ src/scripts/towns/TownList.ts
@@ -2412,8 +2412,6 @@ const HoennFluteMaster = new GemMasterShop(GameConstants.GemShops.HoennFluteMast
 const HoennStoneSalesman = new GemMasterShop(GameConstants.GemShops.HoennStoneSalesman, 'Stone Salesman', [new TemporaryBattleRequirement('Hoenn Stone Salesman')], true);
 
 const SpectacularContestHall = new ContestHall([ContestRank.Spectacular], [ContestType.Cool, ContestType.Beautiful, ContestType.Cute, ContestType.Smart, ContestType.Tough, ContestType.Balanced]);
-const SuperContests = new ContestHall([ContestRank['Super Normal'], ContestRank['Super Great'], ContestRank['Super Ultra'], ContestRank['Super Master'], ContestRank.Practice], undefined, undefined, 'Sinnoh Contests');
-const HoennContests = new ContestHall([ContestRank.Normal, ContestRank.Super, ContestRank.Hyper, ContestRank.Master], undefined, undefined, 'Hoenn Contests');
 
 //Hoenn NPCs
 
@@ -3484,7 +3482,7 @@ TownList['Lilycove City'] = new Town(
     'Lilycove City',
     GameConstants.Region.hoenn,
     GameConstants.HoennSubRegions.Hoenn,
-    [new ContestHall([ContestRank.Master]), SpectacularContestHall, HoennContests, SuperContests, DepartmentStoreShop, HoennContestShop, PokeblockShop],
+    [new ContestHall([ContestRank.Master]), SpectacularContestHall, DepartmentStoreShop, HoennContestShop, PokeblockShop],
     {
         requirements: [new RouteKillRequirement(10, GameConstants.Region.hoenn, 121)],
     }
@@ -4202,7 +4200,7 @@ const SecretBerryMaster = new BerryMasterShop(GameConstants.BerryTraderLocations
     ItemList.Mulch_Shovel,
 ], 'Secret Berry Shop');
 
-// const SuperContests = new ContestHall([ContestRank['Super Normal'], ContestRank['Super Great'], ContestRank['Super Ultra'], ContestRank['Super Master'], ContestRank.Practice], undefined, undefined, 'Sinnoh Contests');
+const SuperContests = new ContestHall([ContestRank['Super Normal'], ContestRank['Super Great'], ContestRank['Super Ultra'], ContestRank['Super Master'], ContestRank.Practice], undefined, undefined, 'Sinnoh Contests');
 const BrilliantShiningContestHall = new ContestHall([ContestRank['Brilliant Shining']], [ContestType.Balanced]);
 
 //Sinnoh NPCs
