4f49d08205558ebb5747768f3e80bfb07b82e081
rocketguy2 <85809879+rocketguy2@users.noreply.github.com>
2026-04-28 20:59:34 +0100

    Renamed "DropRate" to "Pickup" on the farm
    Name in multiplier menu remains unaltered.

--- src/scripts/farming/Farming.ts
+++ src/scripts/farming/Farming.ts
@@ -54,7 +54,7 @@ class Farming implements Feature {
         this.externalAuras[AuraType.Roaming] = ko.pureComputed<number>(() => this.multiplyPlotAuras(AuraType.Roaming));
         this.externalAuras[AuraType.Ev] = ko.pureComputed<number>(() => this.multiplyPlotAuras(AuraType.Ev));
         this.externalAuras[AuraType.Xp] = ko.pureComputed<number>(() => this.multiplyPlotAuras(AuraType.Xp));
-        this.externalAuras[AuraType.DropRate] = ko.pureComputed<number>(() => this.multiplyPlotAuras(AuraType.DropRate));
+        this.externalAuras[AuraType.Pickup] = ko.pureComputed<number>(() => this.multiplyPlotAuras(AuraType.Pickup));
 
         const multiplierSource = 'Farm Aura';
         this.multiplier.addBonus('shiny', () => this.externalAuras[AuraType.Shiny](), multiplierSource);
@@ -62,7 +62,7 @@ class Farming implements Feature {
         this.multiplier.addBonus('roaming', () => this.externalAuras[AuraType.Roaming](), multiplierSource);
         this.multiplier.addBonus('ev', () => this.externalAuras[AuraType.Ev](), multiplierSource);
         this.multiplier.addBonus('exp', () => this.externalAuras[AuraType.Xp](), multiplierSource);
-        this.multiplier.addBonus('rareItemDropRate', () => this.externalAuras[AuraType.DropRate](), multiplierSource);
+        this.multiplier.addBonus('rareItemDropRate', () => this.externalAuras[AuraType.Pickup](), multiplierSource);
 
         this.highestUnlockedBerry = ko.pureComputed(() => {
             for (let i = GameHelper.enumLength(BerryType) - 2; i >= 0; i--) {
@@ -732,7 +732,7 @@ class Farming implements Feature {
                 'This Berry has a disagreeable "green" flavor and scent typical of vegetables. It is rich in health-promoting fiber.',
                 'It has a tendency to expand into nearby plots, and attracts Pokémon that are holding items.',
             ],
-            new Aura(AuraType.DropRate, [1.01, 1.02, 1.03]),
+            new Aura(AuraType.Pickup, [1.01, 1.02, 1.03]),
             ['Bulbasaur', 'Chikorita', 'Treecko', 'Turtwig', 'Snivy', 'Chespin', 'Rowlet', 'Grookey', 'Sprigatito']
         );
 
