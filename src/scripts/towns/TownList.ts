5aab910cb6c2059a13d031babd000c89569dd2f3
RedzepR <143606926+RedzepR@users.noreply.github.com>
2026-09-03 21:38:42 +0200

    add the shady fence to celadon

--- src/scripts/towns/TownList.ts
+++ src/scripts/towns/TownList.ts
@@ -1071,7 +1071,7 @@ TownList['Celadon City'] = new Town(
     'Celadon City',
     GameConstants.Region.kanto,
     GameConstants.KantoSubRegions.Kanto,
-    [CeladonDepartmentStoreShop, CeladonCityShop, new MoveToDungeon(dungeonList['Rocket Game Corner'])],
+    [CeladonDepartmentStoreShop, CeladonCityShop, new GenericTraderShop('PirateFence', 'Shady Fence', undefined, [new ItemOwnedRequirement('Relic_gold', 1)]), new MoveToDungeon(dungeonList['Rocket Game Corner'])],
     {
         requirements: [new RouteKillRequirement(10, GameConstants.Region.kanto, 7)],
         npcs: [BigSpender, RichBoy, CandyMan, EggHuntErika],
